09cfdc5883136f1be0c833d3243ab0fb43e6f899
Fabrizio Caruso <Fabrizio_Caruso@hotmail.com>
2017-06-14 11:50:35 +0200

    Minor stuff

--- display_macros.h
+++ display_macros.h
@@ -51,120 +51,16 @@ typedef struct ImageStruct Image;
 
 //
 
-#if ( defined(__C64__) || defined(__C128__)) && defined(C64_HARDWARE_SPRITE_VERSION)
-	#define GET_SCREEN_SIZE(x,y) {screensize(x,y); POKE(53248u+21,PEEK(53248u+12) & 4);}	
-#else
-	#define GET_SCREEN_SIZE(x,y) {screensize(x,y);}
-#endif
+
+#define GET_SCREEN_SIZE(x,y) {screensize(x,y);}
+
 
 #define DRAW_BROKEN_WALL(ghost) {SET_TEXT_COLOR(((ghost)->_imagePtr)->_color); gotoxy(((ghost)->_x),((ghost)->_y)); cputc('X'); SET_TEXT_COLOR(TEXT_COLOR);};
 
 
-#if ( defined(__C64__) || defined(__C128__)) && defined(C64_HARDWARE_SPRITE_VERSION)
-	#define DRAW(ghost) \
-	{ \
-		short x = (ghost)->_x; \
-		short y = (ghost)->_y; \
-		if((((ghost)->_imagePtr)->_imageData) == 'O') \
-		{ \
-			POKE(1024  +((ghost)->_y)*40+((ghost)->_x),15); \
-			POKE(55296u+((ghost)->_y)*40+((ghost)->_x),(((ghost)->_imagePtr)->_color)); \
-		} \
-		else if((((ghost)->_imagePtr)->_imageData) == '*') \
-		{ \
-			POKE(2042,13); \
-			POKE(53248u+21,PEEK(53248u+21) | 4);\
-			if(x*8+16>255) \
-			{ \
-				POKE(53248u+4,x*8+16-256); \
-				POKE(53248u+16,PEEK(53248u+16) | 4); \
-			} \
-			else \
-			{ \
-				POKE(53248u+4,x*8+16); \
-				POKE(53248u+16,PEEK(53248u+16) & 255-4); \
-			} \
-			POKE(53248u+5,y*8+36); \
-		} \
-		else if((((ghost)->_imagePtr)->_imageData) == 'P') \
-		{ \
-			POKE(2041,15); \
-			POKE(53248u+21,PEEK(53248u+21) | 2);\
-			if(x*8+16>255) \
-			{ \
-				POKE(53248u+2,x*8+16-256); \
-				POKE(53248u+16,PEEK(53248u+16) | 2); \
-			} \
-			else \
-			{ \
-				POKE(53248u+2,x*8+16); \
-				POKE(53248u+16,PEEK(53248u+16) & 255-2); \
-			} \
-			POKE(53248u+3,y*8+36); \
-		} \
-		else if((((ghost)->_imagePtr)->_imageData) == '+') \
-		{ \
-			POKE(53248u+21,PEEK(53248u+21) | 1); \
-			POKE(2040,14); \
-			if(x*8+16>255) \
-			{ \
-				POKE(53248u+0,x*8+16-256); \
-				POKE(53248u+16,PEEK(53248u+16) | 1); \
-			} \
-			else \
-			{ \
-				POKE(53248u+0,x*8+16); \
-				POKE(53248u+16,PEEK(53248u+16) & 255-1); \
-			} \
-			POKE(53248u+1,y*8+36); \
-		} \
-		else \
-		{ \
-			SET_TEXT_COLOR(((ghost)->_imagePtr)->_color); \
-			gotoxy(x,y); \
-			cputc(((ghost)->_imagePtr)->_imageData); \
-			SET_TEXT_COLOR(TEXT_COLOR); \
-		} \
-	}
-#else
-	#define DRAW(ghost) {SET_TEXT_COLOR(((ghost)->_imagePtr)->_color); gotoxy(((ghost)->_x),((ghost)->_y)); cputc(((ghost)->_imagePtr)->_imageData); SET_TEXT_COLOR(TEXT_COLOR);};
-#endif
+#define DRAW(ghost) {SET_TEXT_COLOR(((ghost)->_imagePtr)->_color); gotoxy(((ghost)->_x),((ghost)->_y)); cputc(((ghost)->_imagePtr)->_imageData); SET_TEXT_COLOR(TEXT_COLOR);};
 
-#if ( defined(__C64__) || defined(__C128__)) && defined(C64_HARDWARE_SPRITE_VERSION)
-	#define DELETE_CHARACTER(ghost) \
-	{ \
-		if((((ghost)->_imagePtr)->_imageData) == 'P') \
-		{ \
-			POKE(53248u+21,PEEK(53248u+21) & 255-2);\
-		} \
-		else \
-		{ \
-			gotoxy(((ghost)->_x),((ghost)->_y)); \
-			cputc(' '); \
-		} \
-	}
-#else
-	#define DELETE_CHARACTER(ghost) {gotoxy(((ghost)->_x),((ghost)->_y)); cputc(' ');};
-#endif
-
-
-// C64 and C128 possible implementation
-// #define DRAW(ghost) \
-	// { \
-	// POKE(1024  +((ghost)->_y)*40+((ghost)->_x),(((ghost)->_imagePtr)->_imageData & 127)); \
-	// POKE(55296u+((ghost)->_y)*40+((ghost)->_x),(((ghost)->_imagePtr)->_color)); \
-	// };
-
-// CONIO character version	
-// #define DRAW(ghost) {SET_TEXT_COLOR(((ghost)->_imagePtr)->_color); gotoxy(((ghost)->_x),((ghost)->_y)); cputc(((ghost)->_imagePtr)->_imageData); SET_TEXT_COLOR(TEXT_COLOR);};
-
-// CONIO multi-character version
-//#define DRAW(ghost) {SET_TEXT_COLOR(((ghost)->_imagePtr)->_color); gotoxy((((ghost)->_x)-1),((ghost)->_y)); cputc('('); cputc(((ghost)->_imagePtr)->_imageData); cputc(')'); SET_TEXT_COLOR(TEXT_COLOR);};
-
-// #define DELETE_CHARACTER(ghost) {gotoxy(((ghost)->_x),((ghost)->_y)); cputc(' ');};
-
-// CONIO multi-character version
-//#define DELETE_CHARACTER(ghost) {gotoxy((((ghost)->_x)-1),((ghost)->_y)); cputs("   ");};
+#define DELETE_CHARACTER(ghost) {gotoxy(((ghost)->_x),((ghost)->_y)); cputc(' ');};
 
 
 // TODO: Implement color conversion

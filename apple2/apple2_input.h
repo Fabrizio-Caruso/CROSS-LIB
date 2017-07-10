342fb0f46261fb4f2539816d285d9e67e8a81922
fabriziocaruso <Fabrizio_Caruso@hotmail.com>
2017-07-10 20:50:01 +0200

    sleep implementation in Apple2

--- apple2/apple2_input.h
+++ apple2/apple2_input.h
@@ -8,34 +8,6 @@ unsigned char GET_CHAR()
 	unsigned char polledValue = PEEK(0xC000);
 	POKE(0xC010,0);
 	return PEEK(0xC010)-128;	
-	// switch(polledValue)
-	// {
-		// case 193:
-			// POKE(0xC010,0);
-			// return 'A';
-		// break;
-		// case 215:
-			// POKE(0xC010,0);
-			// return 'W';
-		// break;
-		// case 211:
-			// POKE(0xC010,0);
-			// return 'S';
-		// break;
-		// case 196:
-			// POKE(0xC010,0);
-			// return 'D';
-		// break;
-		// case 160:
-			// POKE(0xC010,0);
-			// return ' ';
-		// break;
-		// default:
-			// POKE(0xC010,0);
-		// return '\0';
-		// break;
-	// }
-	// return '\0';
 }
 
 #endif // _APPLE2_INPUT

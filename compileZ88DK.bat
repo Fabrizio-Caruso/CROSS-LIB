54d4d4af378a86c5737faec5b9ace5d9dab600b5
Fabrizio Caruso <Fabrizio_Caruso@hotmail.com>
2017-07-31 21:25:42 +0200

    Progress on CPC ANSI-CONIO version

--- compileZ88DK.bat
+++ compileZ88DK.bat
@@ -96,7 +96,7 @@ cd %mypath%\
 @REM del %deliverables%\msx.prg
 
 @REM 
-zcc +cpc -vn -DDEBUG_CHARACTERS -D__CPC__ -DAMALLOC -lmalloc -lndos -create-app -o %deliverables%\cpc.prg %mypath%\display_macros.c %mypath%\powerUps.c %mypath%\enemy.c %mypath%\invincible_enemy.c %mypath%\level.c %mypath%\character.c %mypath%\text.c %mypath%\missile.c %mypath%\strategy.c %mypath%\input.c %mypath%\main.c
+zcc +cpc -clib=ansi -vn -DDEBUG_CHARACTERS -D__CPC__ -DAMALLOC -lmalloc -lndos -create-app -o %deliverables%\cpc.prg %mypath%\display_macros.c %mypath%\powerUps.c %mypath%\enemy.c %mypath%\invincible_enemy.c %mypath%\level.c %mypath%\character.c %mypath%\text.c %mypath%\missile.c %mypath%\strategy.c %mypath%\input.c %mypath%\main.c
 @REM del %deliverables%\cpc.prg 
 @REM del %deliverables%\cpc.cpc
 cd %mypath%\tools\2cdt
--- patch/z88dk_conio_patch.h
+++ patch/z88dk_conio_patch.h
@@ -34,7 +34,7 @@
 #ifndef _Z88DK_CONIO_PATCH
 #define _Z88DK_CONIO_PATCH
 
-	//#include <conio.h>
+	#include <conio.h>
 
 	//define gotoxy(a,b)     printf("\x16%c%c",a+32,b+32)
 	

33a862ed04b10d67c8c8176dbdc243fe27d9a4f1
fabriziocaruso <Fabrizio_Caruso@hotmail.com>
2017-08-21 20:06:54 +0200

    TOCK sound in minimal Vic 20 + 8k

--- minimal/vic20/vic20_sounds.h
+++ minimal/vic20/vic20_sounds.h
@@ -1,22 +1,17 @@
 #ifndef _VIC_SOUNDS
 #define _VIC_SOUNDS
-
-	void EXPLOSION_SOUND(void);
+	void _ping_sound(unsigned char freq);
+	#define PING_SOUND() _ping_sound(0xF0);	
+	#define TOCK_SOUND() _ping_sound(0x08);
 	
+	void _explosion_sound(unsigned char freq, unsigned char vol);
+	#define SHOOT_SOUND() _explosion_sound(0xF0, 0x0A);
+	#define EXPLOSION_SOUND() _explosion_sound(0x80, 0x08);
 	
-	void PING_SOUND(void);	
-	
-	
-	void SHOOT_SOUND(void);	
-	
-	// TODO: Save more memory to implement them
-	#define TICK_SOUND()
-	#define TOCK_SOUND()
-	// void TICK_SOUND();	
-	
-	// void TOCK_SOUND();		
-	
-
 	void ZAP_SOUND();	
+	
+	// TODO: Save more memory to implement it
+	#define TICK_SOUND()
+	// void TICK_SOUND();	
 	
 #endif // _VIC_SOUNDS

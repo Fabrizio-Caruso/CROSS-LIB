e2011319e189f04e0b851b539ee446a4d2f605b2
fabriziocaruso <Fabrizio_Caruso@hotmail.com>
2017-08-22 20:01:47 +0200

    Initial ATARI sound effects

--- atari/atari_sounds.h
+++ atari/atari_sounds.h
@@ -41,22 +41,50 @@
 
 
 	#include<atari.h>
-
-	
+	#include <peekpoke.h>
+		
+		// POKE AUDCTL with zero and POKE 53775,3 	
 	#define EXPLOSION_SOUND() \
 	{ \
+		unsigned short i; \
+		POKEY_WRITE.audctl = 0x00; \
+		POKE(53775u,3); \
 		POKEY_WRITE.audf1 = 0x40; \
-		POKEY_WRITE.audc1 = 0x80; \
+		POKEY_WRITE.audc1 = 0x0F; \
+		for(i=300;i>0;--i) \
+		{ \
+			POKEY_WRITE.audc1 = i/20;\
+		} \
 	};
 	
+	//		POKEY_WRITE.audf2 = 0xA0; \
+	//	POKEY_WRITE.audc2 = 0x2F; \
 	
 	#define PING_SOUND() \
 	{ \
+		unsigned short i; \
+		POKEY_WRITE.audctl = 0x00; \
+		POKE(53775u,3); \
+		POKEY_WRITE.audf1 = 0xF0; \
+		POKEY_WRITE.audc1 = 0x0F; \
+		for(i=700;i>0;--i) \
+		{ \
+		} \
+		POKEY_WRITE.audc1 = 0x00; \
 	};	
 	
 	
 	#define SHOOT_SOUND() \
 	{ \
+		unsigned short i; \
+		POKEY_WRITE.audctl = 0x00; \
+		POKE(53775u,3); \
+		POKEY_WRITE.audf1 = 0xD0; \
+		POKEY_WRITE.audc1 = 0x0F; \
+		for(i=150;i>0;--i) \
+		{ \
+			POKEY_WRITE.audc1 = i/10;\
+		} \
 	};	
 	
 	
@@ -71,6 +99,18 @@
 
 	#define ZAP_SOUND() \
 	{ \
+		unsigned short i; \
+		unsigned short j; \
+		POKEY_WRITE.audctl = 0x00; \
+		POKE(53775u,3); \
+		POKEY_WRITE.audf1 = 0x40; \
+		POKEY_WRITE.audc1 = 0x0F; \
+		for(i=0;i<100;++i) \
+		{ \
+			for(j=0;j<50;++j) { } \
+			POKEY_WRITE.audf1 = 100+i; \
+		} \
+		POKEY_WRITE.audc1 = 0x00; \
 	};		
 
 

18195bae98c961fa3c2c75469179553bc791515f
Fabrizio Caruso <Fabrizio_Caruso@hotmail.com>
2017-08-23 13:11:13 +0200

    compile options for minimal version

--- minimal/compile_minimal_msx.bat
+++ minimal/compile_minimal_msx.bat
@@ -1,13 +1,13 @@
-@REM 
-set cc65path="C:\Retro\DEV\cc65-snapshot-win32"
-@REM 
-set mypath="C:\Users\Brizio\Documents\GitHub\PortableChase\minimal"
-@REM 
-set configpath="C:\Users\Brizio\Documents\GitHub\PortableChase\minimal\experiments"
+@REM set cc65path="C:\Retro\DEV\cc65-snapshot-win32"
+@REM set mypath="C:\Users\Brizio\Documents\GitHub\PortableChase\minimal"
+@REM set configpath="C:\Users\Brizio\Documents\GitHub\PortableChase\minimal\experiments"
 
-@REM set cc65path="D:\personal\cc65-snapshot-win32"
-@REM set configpath="D:\Userfiles\fcaruso\Documents\GitHub\ASCII-CHASE\cfg"
-@REM set mypath="d:\Userfiles\fcaruso\Documents\GitHub\ASCII-CHASE"
+@REM 
+set cc65path="D:\personal\cc65-snapshot-win32"
+@REM 
+set configpath="D:\Userfiles\fcaruso\Documents\GitHub\ASCII-CHASE\minimal\cfg"
+@REM 
+set mypath="d:\Userfiles\fcaruso\Documents\GitHub\ASCII-CHASE\minimal"
 
 
 set Deliverables=%mypath%\deliverables
@@ -31,9 +31,9 @@ cd %mypath%\
 
 @REM CPC HALF-WORKINg
 @REM -DDEBUG_CHARACTERS
-zcc +msx  -create-app -vn  -D__MSX__ -lndos -create-app -o %deliverables%\MINIMAL_msx_experimental.prg %mypath%\display_macros.c %mypath%\enemy.c %mypath%\invincible_enemy.c %mypath%\level.c %mypath%\character.c %mypath%\text.c %mypath%\missile.c %mypath%\strategy.c %mypath%\input_macros.c %mypath%\main.c
+zcc +msx  -create-app -vn  -D__MSX__ -lndos -create-app -o %deliverables%\MINIMAL_msx_16k_experimental.prg %mypath%\display_macros.c %mypath%\enemy.c %mypath%\invincible_enemy.c %mypath%\level.c %mypath%\character.c %mypath%\text.c %mypath%\missile.c %mypath%\strategy.c %mypath%\input_macros.c %mypath%\main.c
 @REM 
-del %deliverables%\MINIMAL_msx_experimental.prg 
+del %deliverables%\MINIMAL_msx_16k_experimental.prg 
 @REM del %deliverables%\cpc.cpc
 
 

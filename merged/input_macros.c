15df791020ec8d0b91811ea9363be2b107a0968f
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2017-09-08 00:21:35 +0200

    Turn-based Aquarius prototype

--- merged/input_macros.c
+++ merged/input_macros.c
@@ -508,7 +508,7 @@ extern unsigned char guns;
 	#elif defined(__VG5K__) 
 		void MOVE_PLAYER(void) {movePlayerByKeyboard(getk());}	
 	#elif defined(__AQUARIUS__) 
-		void MOVE_PLAYER(void) {movePlayerByKeyboard(GET_CHAR());}
+		void MOVE_PLAYER(void) {movePlayerByKeyboard(getch());} // TODO: this is wrong (turn-based)
 	#elif defined(__ZX81__) 
 		void MOVE_PLAYER(void) {movePlayerByKeyboard(getk());}		
 	#elif defined(__APPLE2__) || defined(__APPLE2ENH__)

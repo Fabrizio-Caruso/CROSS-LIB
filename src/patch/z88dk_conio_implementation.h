829f0a030f536d835605d249187c0a7d501cb519
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2017-12-12 16:23:48 +0100

    Fix ZX81/ZX80/Lambda compile issue on recent Z88DK versions

--- src/patch/z88dk_conio_implementation.h
+++ src/patch/z88dk_conio_implementation.h
@@ -41,6 +41,7 @@
 			#define gotoxy(x,y) printf("\x16%c%c",x+1,y+1); 
 		#endif
 	#elif defined(__ZX81__)	|| defined(__ZX80__) || defined(__LAMBDA__)
+		#undef gotoxy
 		#define gotoxy(x,y) zx_setcursorpos(y-1+1,x)
 	#elif defined(__ENTERPRISE__) || defined(__MTX__)
 		#define gotoxy(x,y) printf("\x16%c%c",x+1,y+1); 

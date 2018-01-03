61ad3db6b8d7e9e3078e59ad37a361a99d5c7de3
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-01-03 15:20:25 +0100

    Improve error.c

--- experiments/error.c
+++ experiments/error.c
@@ -9,23 +9,30 @@ typedef struct ParentStruct Parent;
 struct ChildStruct
 {
 	Parent parent;
-        void (*method)(void);	
+    
+	void (*method)(void);	
+	int b;
 };
 
 typedef struct ChildStruct Child;
 
-void foo()
+void foo(void)
 {
 }
 
 int main()
 {
 	Child c;
-	c.method = &foo;
 
 	Parent *pPtr = (Parent *) &c;
 
+	c.method = &foo;
+
 	pPtr->a = 42;
+	
+	c.b = 43;
+	
+	c.method();
 
 	return 0;
 }

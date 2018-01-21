e27df762e323dec9074e90416e3cea4d87a1b63b
U-HP_Quad\Brizio <fabrizio_caruso@hotmail.com>
2018-01-21 21:20:48 +0100

    rand experiments

---
+++ experiments/coco_rand.c
@@ -0,0 +1,26 @@
+#include <coco.h>
+
+int main()
+{
+	unsigned char i;
+	unsigned char j;
+	
+	for(j=0; j<10;++j)
+	{
+		for(i=0; i<10; ++i)
+		{
+			if(rand()>16000)
+			{
+				printf("%d: YES\n",i);
+			}
+			else
+			{
+				printf("%d: NO\n",i);
+			}
+		}
+		sleep(1);
+		printf("\n");
+	}
+	printf("hello world\n");
+	return 0;
+}

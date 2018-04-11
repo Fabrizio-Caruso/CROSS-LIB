4087fcc9be28d8c57a23c493d9ca81b1bb24cf6c
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-04-11 23:23:54 +0200

    Fix light NC100 graphical glitches

--- src/z88dk_sprites/z88dk_sprites_graphics.c
+++ src/z88dk_sprites/z88dk_sprites_graphics.c
@@ -113,12 +113,12 @@ extern unsigned char sprites[];
 
 #define _DRAW_VERTICAL_WALL(x,y) \
 { \
-	putsprite(spr_and,x*SPRITE_X_STEP,y*SPRITE_Y_STEP,sprites + (unsigned char *) (_VERTICAL_BAR*(2+SPRITE_Y_SIZE)) ); \
+	putsprite(spr_or,x*SPRITE_X_STEP,y*SPRITE_Y_STEP,sprites + (unsigned char *) (_VERTICAL_BAR*(2+SPRITE_Y_SIZE)) ); \
 }
  
 #define _DRAW_HORIZONTAL_WALL(x,y) \
 { \
-	putsprite(spr_and,x*SPRITE_X_STEP,y*SPRITE_Y_STEP,sprites + (unsigned char *) (_HORIZONTAL_BAR*(2+SPRITE_Y_SIZE)) ); \
+	putsprite(spr_or,x*SPRITE_X_STEP,y*SPRITE_Y_STEP,sprites + (unsigned char *) (_HORIZONTAL_BAR*(2+SPRITE_Y_SIZE)) ); \
 }
 
 #define _DRAW_BROKEN_WALL(x,y) \

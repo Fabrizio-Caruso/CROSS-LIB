3d00b20938cad0a1d4f442f060ebb5dec02be72e
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-05-22 11:17:42 +0200

    Remove source files for nocart

--- tools/nocart/src/patchroms.bat
+++
@@ -1,4 +0,0 @@
-set PATH=c:\tools;%PATH%
-pasmo --bin patchos.asm os.rom
-pasmo --bin patchbasic.asm basic.rom
-pasmo --bin patchdos.asm amsdos.rom
--- tools/nocart/src/patchroms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm os.rom
-rm basic.rom
-rm amsdos.rom
-pasmo --bin patchos.asm os.rom
-pasmo --bin patchbasic.asm basic.rom
-pasmo --bin patchdos.asm amsdos.rom

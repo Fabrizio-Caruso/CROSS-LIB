54409dae8014915f958313ce72c0064390f7e7f6
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-06-29 19:38:29 +0200

    Default graphics settings

---
+++ src/cross_lib/display/default_graphics_settings.h
@@ -0,0 +1,72 @@
+/* --------------------------------------------------------------------------------------- */ 
+// 
+// CROSS CHASE by Fabrizio Caruso
+//
+// Fabrizio_Caruso@hotmail.com
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from
+// the use of this software.
+
+// Permission is granted to anyone to use this software for non-commercial applications, 
+// subject to the following restrictions:
+
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software in
+// a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+
+// 2. Altered source versions must be plainly marked as such, and must not
+// be misrepresented as being the original software.
+
+// 3. This notice may not be removed or altered from any source distribution.
+/* --------------------------------------------------------------------------------------- */ 
+
+#ifndef _DEFAULT_GRAPHICS_SETTINGS
+#define _DEFAULT_GRAPHICS_SETTINGS
+
+
+#define _PLAYER '*'
+
+// RED
+#define _BOMB 'X'
+
+// WHITE
+#define _GHOST 'o'
+
+#define _DEAD_GHOST '#'
+
+// BLUE
+#define _GUN '!'
+
+// YELLOW
+#define _SKULL '+'
+#define _CONFUSE '+'
+#define _ZOMBIE 'O'	
+
+#define _EXTRA_LIFE ('*')
+#define _EXTRA_POINTS '$'
+
+// GREEN
+#define _POWERUP  'S';
+
+// CYAN
+#define _INVINCIBILITY 'V'
+#define _BULLET '.'
+
+#define _LEFT_HORIZONTAL_MISSILE '>'
+
+#define _RIGHT_HORIZONTAL_MISSILE '<'
+
+#define _ROCKET ('^')
+
+
+#define _VERTICAL_BRICK ('|')
+#define _HORIZONTAL_BRICK '-'
+#define _BROKEN_WALL _BOMB	
+#define _SPACE ' '
+
+
+
+#endif // _DEFAULT_GRAPHICS_SETTINGS
+
--- src/cross_lib/display/graphics.h
+++ src/cross_lib/display/graphics.h
@@ -1,7 +1,9 @@
 #  if defined(__C64__)
 	#include "cc65/c64/c64_memory_mapped_settings.h"
-#elif defined(__CBM__)
+#elif defined(__CBM__) && defined(MEMORY_MAPPED)
 	#include "cc65/petscii_memory_mapped_settings.h"
 #elif defined(__CMOC__) && !defined(__WINCMOC__)
 	#include "cmoc/cmoc_memory_mapped_settings.h"
+#else
+	#include "default_graphics_settings.h"
 #endif 

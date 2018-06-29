3c38ec567de83cad9b05b91a25bfffd1e873d2b6
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-06-29 07:08:56 +0200

    Restore c64 redefined chars

--- src/cross_lib/display/display_macros.h
+++ src/cross_lib/display/display_macros.h
@@ -38,8 +38,10 @@
 	// #include "cmoc/cmoc_memory_mapped_settings.h"
 // #endif
 
-	
-#  if defined(__CBM__)
+
+#  if defined(__C64__)
+	#include "cc65/c64/c64_memory_mapped_settings.h"
+#elif defined(__CBM__)
 	#include "cc65/petscii_memory_mapped_settings.h"
 #elif defined(__CMOC__) && !defined(__WINCMOC__)
 	#include "cmoc/cmoc_memory_mapped_settings.h"

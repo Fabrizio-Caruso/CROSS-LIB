ee29f8453e3d1da9855fd67ac363e5bdb9f881f5
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-07-04 17:59:52 +0200

    Add support for C16 16k light version with UDG

--- src/cross_lib/display/graphics_settings.h
+++ src/cross_lib/display/graphics_settings.h
@@ -1,7 +1,12 @@
+
 #  if defined(__C64__)
 	#include "cc65/c64/c64_memory_mapped_settings.h"
 #elif defined(__VIC20__) && defined(VIC20_EXP_3K)
 	#include "cc65/vic20/vic20_exp_3k_settings.h"	
+#elif defined(__VIC20__) && defined(CONIO)
+	#include "cc65/vic20/vic20_exp_16k_settings.h"
+#elif defined(__C16__) && defined(CONIO)
+	#include "cc65/c264/c264_redefined_chars_settings.h"
 #elif defined(__CBM__) && defined(MEMORY_MAPPED)
 	#include "cc65/petscii_memory_mapped_settings.h"
 #elif defined(__CMOC__) && !defined(__WINCMOC__)
@@ -10,8 +15,6 @@
 	#include "cc65/atari/atari_mode1_redefined_chars_settings.h"
 #elif defined(__ATMOS__)
 	#include "cc65/atmos/atmos_redefined_characters_settings.h"
-#elif defined(__VIC20__) && defined(CONIO)
-	#include "cc65/vic20/vic20_exp_16k_settings.h"
 #else
 	#include "default_graphics_settings.h"
 #endif 

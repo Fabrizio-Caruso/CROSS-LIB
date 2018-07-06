a67dede3398b90887684617ce6a6277829a8ccac
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-07-06 17:56:27 +0200

    Simplify DRAW_STAT cases

--- src/cross_lib/display/display_macros.h
+++ src/cross_lib/display/display_macros.h
@@ -197,16 +197,8 @@ extern Image GHOST_IMAGE;
 	#define CPC_YELLOW 2 
 	#define CPC_RED 3
 	#define CPC_BLACK 4
-	#define DRAW_STAT_CHARACTER(x,y,image) \
-		cpc_PrintGphStrStdXY(CPC_YELLOW,image,x*2,y*8);gotoxy(x+1,y); cputc(':')
-
-#elif defined(__ZX81__) || defined(__ZX80__) || defined(__LAMBDA__)
-	#define DRAW_STAT_CHARACTER(x,y,image) \
-		zx_setcursorpos(y, x); cputc(image);cputc(':')
-
-#elif (defined(__ATARI__) || defined(__ATARIXL__)) && defined(ATARI_MODE1)
-	#define DRAW_STAT_CHARACTER(x,y,image) \
-		gotoxy(x,0); cputc(image)
+	#define DRAW_STAT_CHARACTER(x,y,image) 
+	//	cpc_PrintGphStrStdXY(CPC_YELLOW,image,x*2,y*8);gotoxy(x+1,y); cputc(':')
 
 #elif (defined(__CMOC__) && !defined(__WINCMOC__)) \
 	|| defined(__TRS80__) || defined(__EG2K__) \
--- src/cross_lib/text/text_macros.c
+++ src/cross_lib/text/text_macros.c
@@ -30,41 +30,5 @@
 	#endif
 
 #endif
-/*
-#if !defined(NO_COLOR) && !defined(NO_MESSAGE) //&& !defined(NO_TEXT_COLOR)
-	void printCenteredMessageOnRow(unsigned char row, char *Text)
-	{
-		PRINT(((XSize - strlen(Text))>>1), row, Text);	
-	}
-	
-	#if defined(__ATMOS__)
-		#include <peekpoke.h>
-		void printCenteredMessageOnRowWithCol(unsigned char row, unsigned char col, char *Text)
-		{
-			POKE(0xBB80+3+(row+Y_OFFSET)*40,16);POKE(0xBB80+3+1+(row+Y_OFFSET)*40,col);
-			printCenteredMessageOnRow(row, Text);						
-			POKE(0xBB80+35+(row+Y_OFFSET)*40,16);POKE(0xBB80+35+1+(row+Y_OFFSET)*40,3);			
-		}			
-	#else
-		
-		void printCenteredMessageOnRowWithCol(unsigned char row, unsigned char col, char *Text)
-		{
-			SET_TEXT_COLOR(col);
-			printCenteredMessageOnRow(row, Text);
-		}		
-	#endif
-#else
-    #if !defined(NO_MESSAGE)
-		void printCenteredMessageOnRow(unsigned char row, char *Text)
-		{
-			PRINT((unsigned char)(((unsigned char) XSize - strlen (Text))>>1), row, Text);	
-		}
-	#else
-		
-	#endif
-	#define printCenteredMessageOnRowWithCol(row, col, Text) \
-		printCenteredMessageOnRow(row, Text)
-#endif
-*/
 
 

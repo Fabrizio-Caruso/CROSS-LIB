4cb6eab8af55764065e087c0c1366746b68b0a3f
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-07-07 16:26:46 +0200

    Experiments with sprites (still broken)

--- src/cross_lib/display/z88dk/z88dk_sprites/z88dk_sprites_graphics.h
+++ src/cross_lib/display/z88dk/z88dk_sprites/z88dk_sprites_graphics.h
@@ -17,17 +17,3 @@
 	putsprite(spr_and,x*SPRITE_X_STEP,y*SPRITE_Y_STEP,sprites + (unsigned char *) (_SPACE*(2+SPRITE_Y_SIZE)) ); \
 }
 
-#define _DRAW_VERTICAL_WALL(x,y) \
-{ \
-	putsprite(spr_or,x*SPRITE_X_STEP,y*SPRITE_Y_STEP,sprites + (unsigned char *) (_VERTICAL_BAR*(2+SPRITE_Y_SIZE)) ); \
-}
- 
-#define _DRAW_HORIZONTAL_WALL(x,y) \
-{ \
-	putsprite(spr_or,x*SPRITE_X_STEP,y*SPRITE_Y_STEP,sprites + (unsigned char *) (_HORIZONTAL_BAR*(2+SPRITE_Y_SIZE)) ); \
-}
-
-#define _DRAW_BROKEN_WALL(x,y) \
-{ \
-}
-

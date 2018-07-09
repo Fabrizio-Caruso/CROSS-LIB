d748d64775bb50b57fdcee0aeddc9fc513a840ce
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-07-09 22:43:59 +0200

    Optimize memory_mapped

--- src/cross_lib/display/memory_mapped/memory_mapped_graphics.h
+++ src/cross_lib/display/memory_mapped/memory_mapped_graphics.h
@@ -38,7 +38,7 @@
 	do \
 	{ \
 		DISPLAY_POKE(loc(x,y), image->_imageData); \
-		DISPLAY_POKE((unsigned short) ((unsigned short) (COLOR_ADDR+x+X_OFFSET) +(unsigned short)(y+Y_OFFSET)*(XSize+X_OFFSET)),image->_color); \
+		DISPLAY_POKE((unsigned short) ((unsigned short) (X_OFFSET+COLOR_ADDR+x) +(unsigned short)(Y_OFFSET+y)*(X_OFFSET+XSize)),image->_color); \
 	} \
 	while(0)
 

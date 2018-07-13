88a1aeda8bf2b6b364ebde52230f4d4354486a4a
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-07-14 00:27:27 +0200

    Experiments with Supervision

--- src/cross_lib/display/cc65/supervision/supervision_graphics.h
+++ src/cross_lib/display/cc65/supervision/supervision_graphics.h
@@ -4,22 +4,16 @@
 
 #include <supervision.h>
 
-
-//#define EXTRA_INVISIBLE 4
-
-
-// TODO: Remove BOGUS graphics data (image->_imageData points to the real data and is not the data itself) 
-
-// 		SV_VIDEO[2*x+2*(XSize+EXTRA_INVISIBLE)*(k+8*y)]= image->_imageData; 
-//		SV_VIDEO[2*x+2*(XSize+EXTRA_INVISIBLE)*(k+8*y)+1]= image->_imageData; 
+// 		SV_VIDEO[2*(x)+48*k+48*8*(y)]=byte2wordmsb(redefine_map[(unsigned char) image->_imageData].bitmap); 
+//		SV_VIDEO[2*(x)+48*k+48*8*(y)+1]=byte2wordlsb(redefine_map[(unsigned char) image->_imageData].bitmap); 
 
 #define _DRAW(x,y,image) \
 { \
 	unsigned short k; \
 	for(k=0;k<8;++k) \
 	{ \
-		SV_VIDEO[2*(x)+48*k+48*8*(y)]=image->_imageData; \
-		SV_VIDEO[2*(x)+48*k+48*8*(y)+1]=image->_imageData; \
+		SV_VIDEO[2*(x)+48*k+48*8*(y)] = image->_imageData; \
+		SV_VIDEO[2*(x)+48*k+48*8*(y)+1] = image->_imageData; \
 	} \
 }
 
--- src/cross_lib/display/graphics.c
+++ src/cross_lib/display/graphics.c
@@ -47,6 +47,65 @@ extern Image HORIZONTAL_BRICK_IMAGE;
 
 	extern unsigned char sprites[];
 	
+#elif defined(SUPERVISION)
+
+	// struct redefine_struct
+	// {
+	   // unsigned char ascii;
+	   // unsigned char bitmap[8];
+	// } ;
+
+
+	// struct redefine_struct redefine_map[] =
+	// {
+		// {_PLAYER_DOWN, { 24, 36, 24,102,153, 24, 36,102}},
+		// {_PLAYER_UP, { 24, 60, 24,102,153, 24, 36,102}},
+		// {_PLAYER_RIGHT, { 24, 52, 25,118,152, 24, 20, 20}},	
+		// {_PLAYER_LEFT, { 24, 44,152,110, 25, 24, 40, 40}},
+		// {_GHOST, {129,126,165,129,129,189,129,126}},
+		// {_SKULL, { 60, 66,165,129, 90, 36, 36, 60}},
+		// {_GUN, {  0,128,126,200,248,192,128,  0}},
+		// {_POWERUP, {  0, 60, 54,223,231,122, 36, 24}},
+		// {_BULLET, {  0,  0,  8, 56, 28, 16,  0,  0}},
+		// {_BOMB, { 60, 66,165,153,153,165, 66, 60}},
+		// {_VERTICAL_BRICK, { 24, 24, 24, 48, 24, 12, 24, 24}},
+		// {_HORIZONTAL_BRICK, {  0,  0,  0,255,  0,  0,  0,  0}},	
+		// #if defined(FULL_GAME)
+			// {_RIGHT_HORIZONTAL_MISSILE, {  0,  0, 15,252,252, 15,  0,  0}},
+			// {_LEFT_HORIZONTAL_MISSILE, {  0,  0,240, 63, 63,240,  0,  0}},	
+			// {_ROCKET, { 24, 60, 60, 60,126, 90, 66, 66}},
+			// {_INVINCIBILITY, { 24, 36, 24,  0,153,  0, 36,102}},
+		// #endif
+	// };
+
+
+	// unsigned char byte2wordlsb(unsigned char oneByte)
+	// {
+		// unsigned lsb = 0;
+		// unsigned char i;
+		
+		// for(i=0;i<4;++i)
+		// {
+			// lsb += oneByte&(1<<(i*2));
+			// lsb += (oneByte&(1<<(i*2+1)));
+		// }
+		// return lsb;
+	// }
+
+	// unsigned char byte2wordmsb(unsigned char oneByte)
+	// {
+		// unsigned msb = 0;
+		// unsigned char i;
+		
+		// for(i=4;i<8;++i)
+		// {
+			// msb += oneByte&(1<<(i*2));
+			// msb += (oneByte&(1<<(i*2)+1));
+		// }
+		// return msb;
+	// }
+
+
 #endif
 
 #ifndef X_OFFSET
@@ -63,7 +122,7 @@ extern Image HORIZONTAL_BRICK_IMAGE;
 
 	void _draw_stat(unsigned char x, unsigned char y, Image * image) 
 	{
-		_DRAW(X_OFFSET+x,y,image);
+		_DRAW((X_OFFSET+x),(y),image);
 	}
 
 	void _draw(unsigned char x, unsigned char y, Image * image) 

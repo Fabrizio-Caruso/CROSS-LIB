1dbfbbc3342c07b18c6ce3f08e21c41c567b3936
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-07-23 14:12:03 +0200

    Re-factor msx udg code

--- src/cross_lib/display/init_graphics/z88dk/msx/msx_redefined_chars_init_graphics.c
+++ src/cross_lib/display/init_graphics/z88dk/msx/msx_redefined_chars_init_graphics.c
@@ -43,65 +43,112 @@ void set_udg_colors(void)
 	}
 }
 
-void set_udg_images(void)
+struct redefine_struct
 {
-	static const char player_down[8] =      _PLAYER_DOWN_UDG;
-	static const char player_up[8] =        _PLAYER_UP_UDG;
-	static const char player_right[8] =     _PLAYER_RIGHT_UDG;	
-	static const char player_left[8] =      _PLAYER_LEFT_UDG;
-	static const char ghost[8] =            _GHOST_UDG;
-	static const char skull[8] =            _SKULL_UDG;
-	static const char gun[8] =              _GUN_UDG;
-	static const char powerUp[8] =          _POWERUP_UDG;
-	static const char bullet[8] =           _BULLET_UDG;
-	static const char bomb[8] =             _BOMB_UDG;
+   unsigned char ascii;
+   unsigned char bitmap[8];
+} ;
+
+
+struct redefine_struct redefine_map[] =
+{
+	{_PLAYER_DOWN, _PLAYER_DOWN_UDG},
+	{_PLAYER_UP, _PLAYER_UP_UDG},
+	{_PLAYER_RIGHT, _PLAYER_RIGHT_UDG},	
+	{_PLAYER_LEFT, _PLAYER_LEFT_UDG},
+	{_GHOST, _GHOST_UDG},
+	{_BOMB, _BOMB_UDG},
 	
-	static const char rocket[8] =           _ROCKET_UDG;	
-	static const char missile_right[8] =    _RIGHT_HORIZONTAL_MISSILE_UDG;
-	static const char missile_left[8] =     _LEFT_HORIZONTAL_MISSILE_UDG;	
-	static const char invincibility[8] =    _INVINCIBILITY_UDG;
-	
-	static const char dead_ghost[8] =       _DEAD_GHOST_UDG;
-	
-	static const char vertical_brick[8] =   _VERTICAL_BRICK_UDG;
-	static const char horizontal_brick[8] = _HORIZONTAL_BRICK_UDG;	
-	static const char extra_points[8]     = _EXTRA_POINTS_UDG;
-		
-	#if !defined(ANIMATE_PLAYER)
-		redefine(_PLAYER,player_down);
-	#else
-		redefine(_PLAYER_DOWN,player_down);
-		redefine(_PLAYER_UP,player_up);	
-		redefine(_PLAYER_LEFT,player_left);	
-		redefine(_PLAYER_RIGHT, player_right);	
+	#if defined(FULL_GAME)
+	{_RIGHT_HORIZONTAL_MISSILE, _RIGHT_HORIZONTAL_MISSILE_UDG},
+	{_LEFT_HORIZONTAL_MISSILE, _LEFT_HORIZONTAL_MISSILE_UDG},
 	#endif
 	
-	redefine(_BOMB, bomb);
-	redefine(_GUN, gun);
-	redefine(_POWERUP, powerUp);		
-	redefine(_SUPER, powerUp);
-		
-	redefine(_GHOST, ghost);
-	redefine(_ZOMBIE, ghost);
-	redefine(_SKULL, skull);	
-	redefine(_BULLET, bullet);	
-		
-	redefine(_EXTRA_LIFE,player_down);	
-	redefine(_INVINCIBILITY,invincibility);	
+	#if !defined(TINY_GAME)
+	{_SKULL, _SKULL_UDG},
+	{_GUN, _GUN_UDG},
+	{_POWERUP, _POWERUP_UDG},
+	{_BULLET, _BULLET_UDG},
+	{_ROCKET, _ROCKET_UDG},
+	{_INVINCIBILITY, _INVINCIBILITY_UDG},
+	{_VERTICAL_BRICK, _VERTICAL_BRICK_UDG},
+	{_HORIZONTAL_BRICK, _HORIZONTAL_BRICK_UDG},		
+	#endif	
 	
 	#if !defined(NO_DEAD_GHOST)
-		redefine(_DEAD_GHOST,dead_ghost);	
-	#endif	
+	{_DEAD_GHOST, _DEAD_GHOST_UDG},
+	#endif
+	
+	{_EXTRA_POINTS, _EXTRA_POINTS_UDG}
+};
+
+
+void set_udg_images(void)
+{
+	unsigned char i;
+	
+	for (i = 0; i < sizeof(redefine_map) / sizeof(*redefine_map); ++i)
+	{
+	   redefine(redefine_map[i].ascii, redefine_map[i].bitmap);
+	}
+	
+	// static const char player_down[8] =      _PLAYER_DOWN_UDG;
+	// static const char player_up[8] =        _PLAYER_UP_UDG;
+	// static const char player_right[8] =     _PLAYER_RIGHT_UDG;	
+	// static const char player_left[8] =      _PLAYER_LEFT_UDG;
+	// static const char ghost[8] =            _GHOST_UDG;
+	// static const char skull[8] =            _SKULL_UDG;
+	// static const char gun[8] =              _GUN_UDG;
+	// static const char powerUp[8] =          _POWERUP_UDG;
+	// static const char bullet[8] =           _BULLET_UDG;
+	// static const char bomb[8] =             _BOMB_UDG;
+	
+	// static const char rocket[8] =           _ROCKET_UDG;	
+	// static const char missile_right[8] =    _RIGHT_HORIZONTAL_MISSILE_UDG;
+	// static const char missile_left[8] =     _LEFT_HORIZONTAL_MISSILE_UDG;	
+	// static const char invincibility[8] =    _INVINCIBILITY_UDG;
+	
+	// static const char dead_ghost[8] =       _DEAD_GHOST_UDG;
+	
+	// static const char vertical_brick[8] =   _VERTICAL_BRICK_UDG;
+	// static const char horizontal_brick[8] = _HORIZONTAL_BRICK_UDG;	
+	// static const char extra_points[8]     = _EXTRA_POINTS_UDG;
 		
-	redefine(_VERTICAL_BRICK,vertical_brick);	
-	redefine(_HORIZONTAL_BRICK,horizontal_brick);		
+	// #if !defined(ANIMATE_PLAYER)
+		// redefine(_PLAYER,player_down);
+	// #else
+		// redefine(_PLAYER_DOWN,player_down);
+		// redefine(_PLAYER_UP,player_up);	
+		// redefine(_PLAYER_LEFT,player_left);	
+		// redefine(_PLAYER_RIGHT, player_right);	
+	// #endif
+	
+	// redefine(_BOMB, bomb);
+	// redefine(_GUN, gun);
+	// redefine(_POWERUP, powerUp);		
+	// redefine(_SUPER, powerUp);
 		
-	redefine(_FREEZE, powerUp);
+	// redefine(_GHOST, ghost);
+	// redefine(_ZOMBIE, ghost);
+	// redefine(_SKULL, skull);	
+	// redefine(_BULLET, bullet);	
 		
-	redefine(_LEFT_HORIZONTAL_MISSILE,missile_left);	
-	redefine(_RIGHT_HORIZONTAL_MISSILE,missile_right);	
-	redefine(_ROCKET, rocket);
-	redefine(_EXTRA_POINTS, extra_points);
+	// redefine(_EXTRA_LIFE,player_down);	
+	// redefine(_INVINCIBILITY,invincibility);	
+	
+	// #if !defined(NO_DEAD_GHOST)
+		// redefine(_DEAD_GHOST,dead_ghost);	
+	// #endif	
+		
+	// redefine(_VERTICAL_BRICK,vertical_brick);	
+	// redefine(_HORIZONTAL_BRICK,horizontal_brick);		
+		
+	// redefine(_FREEZE, powerUp);
+		
+	// redefine(_LEFT_HORIZONTAL_MISSILE,missile_left);	
+	// redefine(_RIGHT_HORIZONTAL_MISSILE,missile_right);	
+	// redefine(_ROCKET, rocket);
+	// redefine(_EXTRA_POINTS, extra_points);
 	
 }
 
--- src/cross_lib/display/init_graphics/z88dk/spectrum/spectrum_redefined_chars_init_graphics.c
+++ src/cross_lib/display/init_graphics/z88dk/spectrum/spectrum_redefined_chars_init_graphics.c
@@ -7,62 +7,55 @@
 
 #include "../../../graphics_data/8x8_chars.h"
 
-#if defined(ANIMATE_PLAYER)
-	extern char udg[];
-#endif
 
-#if defined(ANIMATE_PLAYER)
+extern char udg[];
+
 struct redefine_struct
 {
    unsigned char ascii;
    unsigned char bitmap[8];
 } ;
-#endif
 
 
-#if defined(ANIMATE_PLAYER)
-	struct redefine_struct redefine_map[] =
-	{
-		{_PLAYER_DOWN, _PLAYER_DOWN_UDG},
-		{_PLAYER_UP, _PLAYER_UP_UDG},
-		{_PLAYER_RIGHT, _PLAYER_RIGHT_UDG},	
-		{_PLAYER_LEFT, _PLAYER_LEFT_UDG},
-		{_GHOST, _GHOST_UDG},
-		{_BOMB, _BOMB_UDG},
-		
-		#if defined(FULL_GAME)
-		{_RIGHT_HORIZONTAL_MISSILE, _RIGHT_HORIZONTAL_MISSILE_UDG},
-		{_LEFT_HORIZONTAL_MISSILE, _LEFT_HORIZONTAL_MISSILE_UDG},
-		#endif
-		
-		#if !defined(TINY_GAME)
-		{_SKULL, _SKULL_UDG},
-		{_GUN, _GUN_UDG},
-		{_POWERUP, _POWERUP_UDG},
-		{_BULLET, _BULLET_UDG},
-		{_ROCKET, _ROCKET_UDG},
-		{_INVINCIBILITY, _INVINCIBILITY_UDG},
-		{_VERTICAL_BRICK, _VERTICAL_BRICK_UDG},
-		{_HORIZONTAL_BRICK, _HORIZONTAL_BRICK_UDG}		
-		#endif
-	};
-#endif
+struct redefine_struct redefine_map[] =
+{
+	{_PLAYER_DOWN, _PLAYER_DOWN_UDG},
+	{_PLAYER_UP, _PLAYER_UP_UDG},
+	{_PLAYER_RIGHT, _PLAYER_RIGHT_UDG},	
+	{_PLAYER_LEFT, _PLAYER_LEFT_UDG},
+	{_GHOST, _GHOST_UDG},
+	{_BOMB, _BOMB_UDG},
+	
+	#if defined(FULL_GAME)
+	{_RIGHT_HORIZONTAL_MISSILE, _RIGHT_HORIZONTAL_MISSILE_UDG},
+	{_LEFT_HORIZONTAL_MISSILE, _LEFT_HORIZONTAL_MISSILE_UDG},
+	#endif
+	
+	#if !defined(TINY_GAME)
+	{_SKULL, _SKULL_UDG},
+	{_GUN, _GUN_UDG},
+	{_POWERUP, _POWERUP_UDG},
+	{_BULLET, _BULLET_UDG},
+	{_ROCKET, _ROCKET_UDG},
+	{_INVINCIBILITY, _INVINCIBILITY_UDG},
+	{_VERTICAL_BRICK, _VERTICAL_BRICK_UDG},
+	{_HORIZONTAL_BRICK, _HORIZONTAL_BRICK_UDG}		
+	#endif
+};
 
 
 void INIT_GRAPHICS(void)
 {
-	#if defined(ANIMATE_PLAYER)
-		unsigned char i;
-		for (i = 0; i < sizeof(redefine_map) / sizeof(*redefine_map); ++i)
-		{
-		   memcpy(udg + (redefine_map[i].ascii - 32)*8, redefine_map[i].bitmap, 8);
-		}
-	#endif
+	unsigned char i;
+	for (i = 0; i < sizeof(redefine_map) / sizeof(*redefine_map); ++i)
+	{
+	   memcpy(udg + (redefine_map[i].ascii - 32)*8, redefine_map[i].bitmap, 8);
+	}
 
 	clg();
 	zx_border(0);
 	zx_colour(PAPER_BLACK|INK_WHITE);
 	textbackground(COLOR_BLACK);
-
 }
+
 

89b8227cab7530dd15ffb5448cd0b298ad336f62
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-07-24 17:58:23 +0200

    Make udg uniform by using maps across all targets

--- src/cross_lib/display/init_graphics/cc65/atari/atari_mode1_redefined_chars_init_graphics.c
+++ src/cross_lib/display/init_graphics/cc65/atari/atari_mode1_redefined_chars_init_graphics.c
@@ -36,40 +36,41 @@
 #include "../../../graphics_data/8x8_chars.h"
 
 #define GRAPHICS_MODE (1+16)
-	
 
-struct redefine_struct
-{
-   unsigned char ascii;
-   unsigned char bitmap[8];
-} ;
+#include "../../../redefine_characters/udg_map.h"	
+
+// struct redefine_struct
+// {
+   // unsigned char ascii;
+   // unsigned char bitmap[8];
+// } ;
 
 
-struct redefine_struct redefine_map[] =
-{
-	{_PLAYER_DOWN_OFFSET, _PLAYER_DOWN_UDG},
-	{_PLAYER_UP_OFFSET, _PLAYER_UP_UDG},
-	{_PLAYER_RIGHT_OFFSET, _PLAYER_RIGHT_UDG},	
-	{_PLAYER_LEFT_OFFSET, _PLAYER_LEFT_UDG},
-	{_GHOST_OFFSET, _GHOST_UDG},
-	{_BOMB_OFFSET, _BOMB_UDG},
+// struct redefine_struct redefine_map[] =
+// {
+	// {_PLAYER_DOWN_OFFSET, _PLAYER_DOWN_UDG},
+	// {_PLAYER_UP_OFFSET, _PLAYER_UP_UDG},
+	// {_PLAYER_RIGHT_OFFSET, _PLAYER_RIGHT_UDG},	
+	// {_PLAYER_LEFT_OFFSET, _PLAYER_LEFT_UDG},
+	// {_GHOST_OFFSET, _GHOST_UDG},
+	// {_BOMB_OFFSET, _BOMB_UDG},
 	
-	#if defined(FULL_GAME)
-	{_RIGHT_HORIZONTAL_MISSILE_OFFSET, _RIGHT_HORIZONTAL_MISSILE_UDG},
-	{_LEFT_HORIZONTAL_MISSILE_OFFSET, _LEFT_HORIZONTAL_MISSILE_UDG},
-	#endif
+	// #if defined(FULL_GAME)
+	// {_RIGHT_HORIZONTAL_MISSILE_OFFSET, _RIGHT_HORIZONTAL_MISSILE_UDG},
+	// {_LEFT_HORIZONTAL_MISSILE_OFFSET, _LEFT_HORIZONTAL_MISSILE_UDG},
+	// #endif
 	
-	#if !defined(TINY_GAME)
-	{_SKULL_OFFSET, _SKULL_UDG},
-	{_GUN_OFFSET, _GUN_UDG},
-	{_POWERUP_OFFSET, _POWERUP_UDG},
-	{_BULLET_OFFSET, _BULLET_UDG},
-	{_ROCKET_OFFSET, _ROCKET_UDG},
-	{_INVINCIBILITY_OFFSET, _INVINCIBILITY_UDG},
-	{_VERTICAL_BRICK_OFFSET, _VERTICAL_BRICK_UDG},
-	{_HORIZONTAL_BRICK_OFFSET, _HORIZONTAL_BRICK_UDG}		
-	#endif
-};
+	// #if !defined(TINY_GAME)
+	// {_SKULL_OFFSET, _SKULL_UDG},
+	// {_GUN_OFFSET, _GUN_UDG},
+	// {_POWERUP_OFFSET, _POWERUP_UDG},
+	// {_BULLET_OFFSET, _BULLET_UDG},
+	// {_ROCKET_OFFSET, _ROCKET_UDG},
+	// {_INVINCIBILITY_OFFSET, _INVINCIBILITY_UDG},
+	// {_VERTICAL_BRICK_OFFSET, _VERTICAL_BRICK_UDG},
+	// {_HORIZONTAL_BRICK_OFFSET, _HORIZONTAL_BRICK_UDG}		
+	// #endif
+// };
 
 
 
--- src/cross_lib/display/init_graphics/cc65/atmos/atmos_redefined_characters_init_graphics.c
+++ src/cross_lib/display/init_graphics/cc65/atmos/atmos_redefined_characters_init_graphics.c
@@ -31,42 +31,44 @@ void init_colors(void)
 	}		
 }
 
-struct redefine_struct
-{
-   unsigned char ascii;
-   unsigned char bitmap[8];
-} ;
+#include "../../../redefine_characters/udg_map.h"
+
+// struct redefine_struct
+// {
+   // unsigned char ascii;
+   // unsigned char bitmap[8];
+// } ;
 
 
-struct redefine_struct redefine_map[] =
-{
-	{_PLAYER_DOWN_OFFSET, _PLAYER_DOWN_UDG},
-	{_PLAYER_UP_OFFSET, _PLAYER_UP_UDG},
-	{_PLAYER_RIGHT_OFFSET, _PLAYER_RIGHT_UDG},	
-	{_PLAYER_LEFT_OFFSET, _PLAYER_LEFT_UDG},
-	{_GHOST_OFFSET, _GHOST_UDG},
-	{_BOMB_OFFSET, _BOMB_UDG},
+// struct redefine_struct redefine_map[] =
+// {
+	// {_PLAYER_DOWN_OFFSET, _PLAYER_DOWN_UDG},
+	// {_PLAYER_UP_OFFSET, _PLAYER_UP_UDG},
+	// {_PLAYER_RIGHT_OFFSET, _PLAYER_RIGHT_UDG},	
+	// {_PLAYER_LEFT_OFFSET, _PLAYER_LEFT_UDG},
+	// {_GHOST_OFFSET, _GHOST_UDG},
+	// {_BOMB_OFFSET, _BOMB_UDG},
 	
-	#if defined(FULL_GAME)
-	{_RIGHT_HORIZONTAL_MISSILE_OFFSET, _RIGHT_HORIZONTAL_MISSILE_UDG},
-	{_LEFT_HORIZONTAL_MISSILE_OFFSET, _LEFT_HORIZONTAL_MISSILE_UDG},
-	#endif
+	// #if defined(FULL_GAME)
+	// {_RIGHT_HORIZONTAL_MISSILE_OFFSET, _RIGHT_HORIZONTAL_MISSILE_UDG},
+	// {_LEFT_HORIZONTAL_MISSILE_OFFSET, _LEFT_HORIZONTAL_MISSILE_UDG},
+	// #endif
 	
-	#if !defined(TINY_GAME)
-	{_SKULL_OFFSET, _SKULL_UDG},
-	{_GUN_OFFSET, _GUN_UDG},
-	{_POWERUP_OFFSET, _POWERUP_UDG},
-	{_BULLET_OFFSET, _BULLET_UDG},
-	{_ROCKET_OFFSET, _ROCKET_UDG},
-	{_INVINCIBILITY_OFFSET, _INVINCIBILITY_UDG},
-	{_VERTICAL_BRICK_OFFSET, _VERTICAL_BRICK_UDG},
-	{_HORIZONTAL_BRICK_OFFSET, _HORIZONTAL_BRICK_UDG},	
-	#endif
+	// #if !defined(TINY_GAME)
+	// {_SKULL_OFFSET, _SKULL_UDG},
+	// {_GUN_OFFSET, _GUN_UDG},
+	// {_POWERUP_OFFSET, _POWERUP_UDG},
+	// {_BULLET_OFFSET, _BULLET_UDG},
+	// {_ROCKET_OFFSET, _ROCKET_UDG},
+	// {_INVINCIBILITY_OFFSET, _INVINCIBILITY_UDG},
+	// {_VERTICAL_BRICK_OFFSET, _VERTICAL_BRICK_UDG},
+	// {_HORIZONTAL_BRICK_OFFSET, _HORIZONTAL_BRICK_UDG},	
+	// #endif
 	
-	#if !defined(NO_DEAD_GHOST)
-	{_DEAD_GHOST_OFFSET, _DEAD_GHOST_UDG}
-	#endif
-};
+	// #if !defined(NO_DEAD_GHOST)
+	// {_DEAD_GHOST_OFFSET, _DEAD_GHOST_UDG}
+	// #endif
+// };
 
 
 

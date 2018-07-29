afbf729857f7eaa6176c91a07117500a6f6d327a
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-07-29 13:43:42 +0200

    Fix noise in psg sound

--- src/cross_lib/sound/z88dk/psg/psg_sounds.c
+++ src/cross_lib/sound/z88dk/psg/psg_sounds.c
@@ -79,81 +79,71 @@ Sel	Pulse
 
 #define ENV_WAVE 13
 		
-	void ZAP_SOUND(void) 
+
+void ZAP_SOUND(void) 
+{
+	unsigned char i;
+	unsigned char j;
+	
+	set_psg(A_VOLUME,15);
+	set_psg(A_PERIOD_LOW,255);
+	
+	set_psg(A_PERIOD_HI, 15);
+	
+	set_psg(CONTROL, 0xFF  - 0x01);
+	
+	for(i=0;i<16;i++)
 	{
-		unsigned char control;
-		unsigned char i;
-		unsigned char j;
-		
-		set_psg(A_VOLUME,15);
-		
-		set_psg(A_PERIOD_LOW,255);
-		set_psg(A_PERIOD_HI, 15);
-		
-		control = get_psg(CONTROL);
-		set_psg(CONTROL, get_psg(CONTROL) & (0xFF  - 0x01));
-		
-		for(i=0;i<16;i++)
-		{
-			set_psg(A_PERIOD_HI,15-i);		
-			for(j=0;j<150;++j)
-			{
-			}
-		}
-		
-		set_psg(CONTROL, control);
-
-		set_psg(A_VOLUME,0);		
-	}
-
-	
-	void _ping_sound(unsigned char freq)
-	{ 
-		unsigned char control;
-		unsigned char i;
-		
-		set_psg(A_VOLUME,15);
-		set_psg(A_PERIOD_LOW,255);
-		set_psg(A_PERIOD_HI, 15 - (freq>>4));		
-
-		control = get_psg(CONTROL);		
-		set_psg(CONTROL, control & (0xFF - 0x01));
-		
-		for(i=0;i<220;++i)
+		set_psg(A_PERIOD_HI,15-i);		
+		for(j=0;j<150;++j)
 		{
 		}
-		
-		set_psg(CONTROL, control);		
-		set_psg(A_VOLUME,0);		
-	}			
-	
-
-	void _explosion_sound(unsigned char freq)
-	{ 
-		unsigned char control;
-		unsigned char i;
-		unsigned char j;
-		
-		set_psg(NOISE,15);
-		set_psg(A_VOLUME,15);
-		
-		set_psg(A_PERIOD_LOW,0);
-		set_psg(A_PERIOD_HI, 15 - (freq>>4));
-		
-		control = get_psg(CONTROL);
-		set_psg(CONTROL, get_psg(CONTROL) & (0xFF - 0x08));
-		
-		for(i=0;i<13;i++)
-		{
-			set_psg(A_VOLUME,15-i);		
-			for(j=0;j<253;++j)
-			{
-			}		
-		}
-		// for(j=0;j<250;++j)
-			// {
-			// }			
-		set_psg(CONTROL, control);
-		set_psg(A_VOLUME,0);		
 	}
 	
+	set_psg(A_VOLUME,0);		
+}
+
+
+void _ping_sound(unsigned char freq)
+{ 
+	unsigned char i;
+	
+	set_psg(A_VOLUME,15);
+	set_psg(A_PERIOD_LOW,255);
+	
+	set_psg(A_PERIOD_HI, 15 - (freq>>4));		
+
+	set_psg(CONTROL, 0xFF - 0x01);
+	
+	for(i=0;i<220;++i)
+	{
+	}
+		
+	set_psg(A_VOLUME,0);
+}			
+
+
+void _explosion_sound(unsigned char freq)
+{ 
+	unsigned char i;
+	unsigned char j;
+	
+	set_psg(NOISE,15);
+	set_psg(A_VOLUME,15);
+	
+	set_psg(A_PERIOD_LOW,0);
+	set_psg(A_PERIOD_HI, 15 - (freq>>4));
+	
+	set_psg(CONTROL, 0xFF - 0x08);
+	
+	for(i=0;i<13;i++)
+	{
+		set_psg(A_VOLUME,15-i);		
+		for(j=0;j<253;++j)
+		{
+		}		
+	}
+
+	set_psg(A_VOLUME,0);		
+}
+

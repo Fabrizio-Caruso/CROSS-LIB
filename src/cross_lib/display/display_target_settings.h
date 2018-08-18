e90085648390dbdc6df46bd07543aa9da3523ea8
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-08-18 17:40:39 +0200

    Color for PC6001 in screen mode 2

--- src/cross_lib/display/display_target_settings.h
+++ src/cross_lib/display/display_target_settings.h
@@ -179,7 +179,7 @@
 
 
 #if defined(__GAL__) || defined(__GAMATE__) \
-    || defined(__PC6001__) || defined(__ATARI5200__) || defined(__CREATIVISION__) \
+    || (defined(__PC6001__) && !(FORCE_SCREEN_MODE==2)) || defined(__ATARI5200__) || defined(__CREATIVISION__) \
 	|| defined(__WINCMOC__) || defined(__CMOC__) || defined(__OSIC1P__) || defined(__MC1000__) \
 	|| defined(__LAMBDA__) || defined(__MSX__) \
 	|| defined(__VZ__) || defined(__VG5k__) \

38b9fb50c0265c1e7c108ee878adb881bbd757de
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-08-23 13:36:53 +0200

    Initial graphics for the nes target

--- src/cross_lib/display/image_settings/nes_settings.h
+++ src/cross_lib/display/image_settings/nes_settings.h
@@ -34,17 +34,17 @@
 #include "../display/display_macros.h"
 
 #define _POWERUP (char) (0x7A-0x20)
-#define _GHOST (char) (0x76-0x20)
+#define _GHOST (char) 35
 #define _BOMB (char) (0x78-0x20)
 #define _GUN (char) (0x7B-0x20)
 
-#define _PLAYER_DOWN '\''
-#define _PLAYER_UP ((char) 0x76 - 6 - 0x20)
-#define _PLAYER_RIGHT ((char) 0x76 - 2 - 0x20)
-#define _PLAYER_LEFT ((char) 0x76 - 4 -0x20)
+#define _PLAYER_DOWN 252
+#define _PLAYER_UP 255
+#define _PLAYER_RIGHT 254
+#define _PLAYER_LEFT 253
 	
 
-#define _BULLET (char) (0x7C-0x20)
+#define _BULLET '.'
 #define _ROCKET ('^'-0x40)
 #define _EXTRA_POINTS '*'
 #define _LEFT_HORIZONTAL_MISSILE '>'

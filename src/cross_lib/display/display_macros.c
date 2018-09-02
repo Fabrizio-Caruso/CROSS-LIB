6379ca672d64410094b486ceea45a86145c8c475
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-09-03 01:01:05 +0200

    Improve pc8086 - Fix buffer overflow

--- src/cross_lib/display/display_macros.c
+++ src/cross_lib/display/display_macros.c
@@ -128,7 +128,7 @@ Image BOMB_IMAGE;
 	extern unsigned char sprites[];
 
 #elif defined(BUFFERED)
-	unsigned char video_buffer[YSize][XSize];
+	unsigned char video_buffer[YSize+Y_OFFSET][XSize];
 	
 	void display_all(void)
 	{

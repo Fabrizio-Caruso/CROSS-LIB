c865b017fc0c31a120c17302c170aeb9250dcd99
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-09-20 15:31:04 +0200

    More Z88 experiments

---
+++ src/cross_lib/display/init_graphics/z88dk/z88/z88_init_graphics.c
@@ -0,0 +1,72 @@
+	#include <dor.h>
+
+	// #define HELP1   "A pointless demo application made with z88dk"
+	// #define HELP2   "Simply loops, points out menu selections and dies"
+	// #define HELP3   "..eventually..see source for details"
+	// #define HELP4   ""
+	// #define HELP5   "v0.1 - 9.4.99 (djm)"
+	// #define HELP6   ""
+
+	#pragma -no-expandz88
+	
+	#if !defined(TINY_GAME)
+		#define BAD_PAGES 2
+	#else
+		#define BAD_PAGES 1
+	#endif 
+	#pragma -reqpag=BAD_PAGES
+	
+	#pragma printf = ""
+	
+	// /* Redirect OZ required callbacks to our functions */
+	// #pragma redirect handlecmds=_handlecmds
+	// #pragma redirect redrawscreen=_redrawscreen
+	
+	#define APP_INFO "by Fabrizio Caruso"
+	#define APP_KEY  'X'
+	#define APP_NAME "XChase"
+	#define APP_TYPE AT_Good
+	
+
+	// #define TOPIC1   "Action"
+	// #define TOPIC1ATTR      TP_Help
+	// #define TOPIC1HELP1     "Some help for the topic"
+	// #define TOPIC1HELP2     "And another line of help"
+
+	// #define TOPIC1_1         "Eat"
+	// #define TOPIC1_1KEY      "AE"
+	// #define TOPIC1_1CODE     $81
+	// #define TOPIC1_1ATTR     MN_Help
+
+	// #define TOPIC1_1HELP1    "Are you hungry?"
+
+	// #define TOPIC1_2         "Drink"
+	// #define TOPIC1_2KEY      "AD"
+	// #define TOPIC1_2CODE     $82
+	// #define TOPIC1_2ATTR     MN_Help
+	// #define TOPIC1_2HELP1    "Why yes, if you're offering, mine'd a"
+	// #define TOPIC1_2HELP2    "pint of something nice"
+
+	// #define TOPIC1_3         "Sleep"
+	// #define TOPIC1_3KEY      "AS"
+	// #define TOPIC1_3CODE     $83
+	// #define TOPIC1_3ATTR     MN_Help
+	// #define TOPIC1_3HELP1    "Yawn...."
+
+	// #define TOPIC2           "Commands"
+	// #define TOPIC2ATTR       TP_Help
+	// #define TOPIC2HELP1      "From this menu you can quit this"
+	// #define TOPIC2HELP2      "Completely useless application"
+
+	// #define TOPIC2_1         "Quit"
+	// #define TOPIC2_1KEY      "CQ"
+	// #define TOPIC2_1CODE     $84
+	// #define TOPIC2_1ATTR     MN_Help
+	// #define TOPIC2_1HELP1    "Go on, leave me now!"
+
+
+	#include <application.h>
+	
+void INIT_GRAPHICS(void)
+{
+}

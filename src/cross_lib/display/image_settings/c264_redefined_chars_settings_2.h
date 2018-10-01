b9c8c67865d6875f6a80c823c6ec43a3f326ed97
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-10-01 08:55:07 +0200

    Some udg on full c16 16k

--- src/cross_lib/display/image_settings/c264_redefined_chars_settings_2.h
+++ src/cross_lib/display/image_settings/c264_redefined_chars_settings_2.h
@@ -1,17 +1,17 @@
 
-#define UDG_BASE_FACTOR 15
+//#define UDG_BASE_FACTOR 15
 
 // BLUE
 #define _PLAYER 0x3B
-#define _PLAYER_DOWN 0x40
-#define _PLAYER_UP 0x3C
-#define _PLAYER_RIGHT 0x3D 
-#define _PLAYER_LEFT 0x3E
+#define _PLAYER_DOWN 0x67
+#define _PLAYER_UP 0x40
+#define _PLAYER_RIGHT 0x69
+#define _PLAYER_LEFT 0x68
 
 #define _GUN 0x3F
 
 // YELLOW
-#define _SKULL 0x27
+#define _SKULL 0x65
 #define _VERTICAL_BRICK 0x26
 #define _HORIZONTAL_BRICK 0x2B
 #define _EXTRA_LIFE _PLAYER_DOWN
@@ -21,7 +21,7 @@
 #define _POWERUP  0x2C
 
 // RED
-#define _BOMB 0x29
+#define _BOMB 0x66
 
 // CYAN
 #define _INVINCIBILITY 0x21
@@ -32,7 +32,7 @@
 #define _BROKEN_BRICK _BOMB
 
 // WHITE
-#define _GHOST 0x28
+#define _GHOST 0x64
 
 #define _DEAD_GHOST _GHOST
 
@@ -40,6 +40,6 @@
 
 #define _RIGHT_HORIZONTAL_MISSILE 0x2E
 
-#define _ROCKET 0x2F
+#define _ROCKET 0x5E
 
 #define _SPACE ' '
--- src/cross_lib/display/init_graphics/cc65/c264/c16_16k_redefined_chars_init_graphics.c
+++ src/cross_lib/display/init_graphics/cc65/c264/c16_16k_redefined_chars_init_graphics.c
@@ -11,18 +11,11 @@
 
 // #include "../../../redefine_characters/udg_map.h"
 
-//#define UDG_BASE_FACTOR 15
+#define UDG_BASE_FACTOR 15
 
 void INIT_GRAPHICS(void)
 {
-	// unsigned short i;
-	
 
-	// POKE(1177,62); // disable switch to RAM in PEEK
-	// for(i=0;i<1023;++i)
-	// {
-		// POKE(UDG_BASE_FACTOR*1024+i,PEEK((unsigned long) (54272ul+(unsigned long) i)));
-	// }
 	// POKE(1177,63); // re-enable switch to RAM in PEEK
 	POKE(65299ul,(PEEK(65299ul)&3)|((15)*4)); // change character base address to 28th Kbyte
 	POKE(65298ul,PEEK(65298ul)&251); // make graphics chip get characters from RAM

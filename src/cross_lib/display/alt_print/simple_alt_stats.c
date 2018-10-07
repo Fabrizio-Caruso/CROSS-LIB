c6f41140d931b01052951879d42e01d62a14a08b
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-10-07 19:50:32 +0200

    simple alt stats for all targets

--- src/cross_lib/display/alt_print/simple_alt_stats.c
+++ src/cross_lib/display/alt_print/simple_alt_stats.c
@@ -1,18 +1,29 @@
 #include "../display_macros.h"
 
+#include "../graphics_mode/memory_mapped_graphics.h"
+
 #if defined(ALT_DISPLAY_STATS) || defined(ALT_HISCORE)
-	#include <peekpoke.h>
+
+	#if !defined(CC65)
+		#define POKE(addr,val)     (*(unsigned char*) (addr) = (val))
+		#define POKEW(addr,val)    (*(unsigned*) (addr) = (val))
+		#define PEEK(addr)         (*(unsigned char*) (addr))
+		#define PEEKW(addr) (*(unsigned*) (addr))
+	#else
+		#include <peekpoke.h>	
+	#endif
+
 	void _displayShort(unsigned short value)
 	{ 
 		unsigned char i; 
 
 		for(i=1;i<6;++i) 
 		{ 
-			value -= POKE(7686-i,(unsigned char) ((value)%10)); 
+			value -= POKE(BASE_ADDR+6-i,(unsigned char) ((value)%10)); 
 			value/=10; 
-			POKE(7686-i,PEEK(7686-i)+48); 
+			POKE(BASE_ADDR+6-i,PEEK(BASE_ADDR+6-i)+48); 
 		} 
-		POKE(7686,48); 	
+		POKE(BASE_ADDR+6,48); 	
 	}
 
 	void PRINT(unsigned char x, unsigned char y, char * str)

18127d247c04f6b54961010a6bfbc6c7d9390981
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2018-10-07 13:03:55 +0200

    Optmize freeze effect

--- src/chase/item.c
+++ src/chase/item.c
@@ -194,11 +194,9 @@ extern Item extraPoints;
 
 	void _freezeEffect(void)
 	{
-		unsigned char i;
-		for(i=0;i<3;++i)
-		{
-			_commonPowerUpEffect();
-		}	
+		_commonPowerUpEffect();
+		_commonPowerUpEffect();
+		_commonPowerUpEffect();
 	}
 
 	void freezeEffect(void)

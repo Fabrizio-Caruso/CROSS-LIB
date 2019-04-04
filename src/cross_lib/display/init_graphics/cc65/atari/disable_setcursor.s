d0ebc5f11c3a989e06dc7b14d9405558b9c88f43
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-04-04 23:01:03 +0200

    Fix mul40 in Atari

--- src/cross_lib/display/init_graphics/cc65/atari/disable_setcursor.s
+++ src/cross_lib/display/init_graphics/cc65/atari/disable_setcursor.s
@@ -4,7 +4,7 @@
 ; cursor handling, internal function
 
         .include "atari.inc"
-        .import cursor,mul40
+        .import cursor,_mul40
         .export setcursor
 
 .proc   setcursor
@@ -14,7 +14,7 @@
         sta     (OLDADR),y
 
         lda     ROWCRS
-        jsr     mul40
+        jsr     _mul40
         clc
         adc     SAVMSC          ; add start of screen memory
         sta     OLDADR

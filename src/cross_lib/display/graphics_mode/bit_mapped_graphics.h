51d16b96994cbe3287c87b93b488b95dd1edc0aa
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-04-12 01:07:32 +0200

    Use loops inside bitmap display macros

--- src/cross_lib/display/graphics_mode/bit_mapped_graphics.h
+++ src/cross_lib/display/graphics_mode/bit_mapped_graphics.h
@@ -12,28 +12,31 @@ extern unsigned char udgs[];
 
 #define __DRAW(x,y,image) \
 { \
-	SV_VIDEO[(x)+(XSize)*8*(y)]           = udgs[(8*(unsigned char)(image)->_imageData)]; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+1*(XSize)] = udgs[(8*(unsigned char)(image)->_imageData)+1]; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+2*(XSize)] = udgs[(8*(unsigned char)(image)->_imageData)+2]; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+3*(XSize)] = udgs[(8*(unsigned char)(image)->_imageData)+3]; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+4*(XSize)] = udgs[(8*(unsigned char)(image)->_imageData)+4]; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+5*(XSize)] = udgs[(8*(unsigned char)(image)->_imageData)+5]; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+6*(XSize)] = udgs[(8*(unsigned char)(image)->_imageData)+6]; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+7*(XSize)] = udgs[(8*(unsigned char)(image)->_imageData)+7]; \
+	unsigned char i; \
+	unsigned short base = (x)+(XSize)*8*(y); \
+	unsigned short delta = 0; \
+	unsigned char offset = (8*(unsigned char)(image)->_imageData) ; \
+	\
+	for(i=0;i<8;++i) \
+	{ \
+		SV_VIDEO[base+delta]  = udgs[offset+i]; \
+		delta+=XSize; \
+	} \
 }
+
 
 #define __DELETE(x,y) \
 { \
-	SV_VIDEO[(x)+(XSize)*8*(y)] = 0; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+1*(XSize)] = 0; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+2*(XSize)] = 0; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+3*(XSize)] = 0; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+4*(XSize)] = 0; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+5*(XSize)] = 0; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+6*(XSize)] = 0; \
-	SV_VIDEO[(x)+(XSize)*8*(y)+7*(XSize)] = 0; \
+	unsigned char i; \
+	unsigned short base = (x)+(XSize)*8*(y); \
+	unsigned short delta = 0; \
+	\
+	for(i=0;i<8;++i) \
+	{ \
+		SV_VIDEO[(unsigned short) base+delta] = 0; \
+		delta+=XSize; \
+	} \
 }
-
 
 #endif // _MO5_BIT_MAPPED_GRAPHICS
 

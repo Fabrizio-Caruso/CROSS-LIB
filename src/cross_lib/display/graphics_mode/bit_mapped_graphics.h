250d6e8b9344b8635b5bc87dd231959f92df36fc
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-04-12 01:15:08 +0200

    Optimize XSize=40 in bitmap macros

--- src/cross_lib/display/graphics_mode/bit_mapped_graphics.h
+++ src/cross_lib/display/graphics_mode/bit_mapped_graphics.h
@@ -14,14 +14,15 @@ extern unsigned char udgs[];
 { \
 	unsigned char i; \
 	unsigned short base = (x)+(XSize)*8*(y); \
-	unsigned short delta = 0; \
+	unsigned char delta = 0; \
 	unsigned char offset = (8*(unsigned char)(image)->_imageData) ; \
 	\
-	for(i=0;i<8;++i) \
+	for(i=0;i<7;++i) \
 	{ \
 		SV_VIDEO[base+delta]  = udgs[offset+i]; \
 		delta+=XSize; \
 	} \
+	SV_VIDEO[base+(XSize)*7] = udgs[offset+7]; \
 }
 
 
@@ -29,13 +30,14 @@ extern unsigned char udgs[];
 { \
 	unsigned char i; \
 	unsigned short base = (x)+(XSize)*8*(y); \
-	unsigned short delta = 0; \
+	unsigned char delta = 0; \
 	\
-	for(i=0;i<8;++i) \
+	for(i=0;i<7;++i) \
 	{ \
 		SV_VIDEO[(unsigned short) base+delta] = 0; \
 		delta+=XSize; \
 	} \
+	SV_VIDEO[base+(XSize)*7] = 0; \
 }
 
 #endif // _MO5_BIT_MAPPED_GRAPHICS

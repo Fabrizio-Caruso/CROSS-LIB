0fcc98db496ddae6455fe938ae4d373e5dd3ef1f
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-04-13 10:02:36 +0200

    Score added in Mo5 version

--- src/cross_lib/display/alt_print/mo5_alt_print.c
+++ src/cross_lib/display/alt_print/mo5_alt_print.c
@@ -29,37 +29,41 @@ void print_05u0(unsigned char x, unsigned char y, unsigned short val)
 		val/=10;
 	}
 	
+	gotoxy(x,y);
 	for(i=0;i<6;++i)
 	{
-		_DISPLAY(x+i,y, (unsigned char) (digits[5-i])+48);
+		PUTCH((unsigned char) (digits[5-i])+48);
 	}
 }	
 
 void print_02u(unsigned char x, unsigned char y, unsigned short val)
 {
-	_DISPLAY(x,y, ((unsigned char) val)/10+48);
-	_DISPLAY(1+x,y, ((unsigned char) val)%10+48);
+	gotoxy(x,y);
+	PUTCH(((unsigned char) val)/10+48);
+	PUTCH(((unsigned char) val)%10+48);
 }	
 
 
 void print_u(unsigned char x, unsigned char y, unsigned short val)
 {
-	_DISPLAY(x,y, (unsigned char) (val+48));
+	gotoxy(x,y);
+	PUTCH((unsigned char) (val+48));	
 }
 
 
 void PRINTF(unsigned char x, unsigned char y, char * str, unsigned short val)
 {
-	// if(strlen(str)==5)
-	// {	
-		// print_05u0(x,y,val);
-	// }
-	// else if(strlen(str)==4)
-	// {
-		// print_02u(x,y,val);		
-	// }
-	// else
-	// {
-		// print_u(x,y,val);		
-	// }
+	if(strlen(str)==5)
+	{	
+		print_05u0(x,y,val);
+	}
+	else if(strlen(str)==4)
+	{
+		print_02u(x,y,val);		
+	}
+	else
+	{
+		print_u(x,y,val);		
+	}
+	SWITCH_COLOR_BANK_OFF();	
 }

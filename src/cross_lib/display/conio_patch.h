e835d4b573b9c03e5ab48499ef46b17db9ec72b7
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-04-13 17:46:11 +0200

    Implement colors in Mo5 (bug: inverted foreground)

--- src/cross_lib/display/conio_patch.h
+++ src/cross_lib/display/conio_patch.h
@@ -168,6 +168,17 @@
 #endif
 
 #if !defined(NO_COLOR)
+	#if defined(__MO5__)
+		#define COLOR_BLACK 0
+		#define COLOR_RED 1
+		#define COLOR_GREEN 2
+		#define COLOR_YELLOW 3		
+		#define COLOR_BLUE 4
+		#define COLOR_MAGENTA 5
+		#define COLOR_CYAN 6
+		#define COLOR_WHITE 7		
+	#endif
+
 	#ifndef COLOR_BLACK
 		#if defined(Z88DK)
 			#define COLOR_BLUE BLUE
--- src/cross_lib/display/graphics_mode/bit_mapped_graphics.h
+++ src/cross_lib/display/graphics_mode/bit_mapped_graphics.h
@@ -10,35 +10,78 @@
 
 extern unsigned char udgs[];
 
-#define __DRAW(x,y,image) \
-{ \
-	unsigned char i; \
-	unsigned short base = (x)+(XSize)*8*(y); \
-	unsigned char delta = 0; \
-	unsigned char offset = (8*(unsigned char)(image)->_imageData) ; \
-	\
-	for(i=0;i<7;++i) \
+#if defined(NO_COLOR)
+	#define __DRAW(x,y,image) \
 	{ \
-		SV_VIDEO[base+delta]  = udgs[offset+i]; \
-		delta+=XSize; \
-	} \
-	SV_VIDEO[base+(XSize)*7] = udgs[offset+7]; \
-}
+		unsigned char i; \
+		unsigned short base = (x)+(XSize)*8*(y); \
+		unsigned char delta = 0; \
+		unsigned char offset = (8*(unsigned char)(image)->_imageData) ; \
+		\
+		for(i=0;i<7;++i) \
+		{ \
+			SV_VIDEO[base+delta]  = udgs[offset+i]; \
+			delta+=XSize; \
+		} \
+		SV_VIDEO[base+(XSize)*7] = udgs[offset+7]; \
+	}
 
 
-#define __DELETE(x,y) \
-{ \
-	unsigned char i; \
-	unsigned short base = (x)+(XSize)*8*(y); \
-	unsigned char delta = 0; \
-	\
-	for(i=0;i<7;++i) \
+	#define __DELETE(x,y) \
 	{ \
-		SV_VIDEO[(unsigned short) base+delta] = 0; \
-		delta+=XSize; \
-	} \
-	SV_VIDEO[base+(XSize)*7] = 0; \
-}
+		unsigned char i; \
+		unsigned short base = (x)+(XSize)*8*(y); \
+		unsigned char delta = 0; \
+		\
+		for(i=0;i<7;++i) \
+		{ \
+			SV_VIDEO[(unsigned short) base+delta] = 0; \
+			delta+=XSize; \
+		} \
+		SV_VIDEO[base+(XSize)*7] = 0; \
+	}
+#elif defined(__MO5__)
+	#include "conio_patch.h"
+	#define __DRAW(x,y,image) \
+	{ \
+		unsigned char i; \
+		unsigned short base = (x)+(XSize)*8*(y); \
+		unsigned char delta = 0; \
+		unsigned char offset = (8*(unsigned char)(image)->_imageData) ; \
+		\
+		for(i=0;i<7;++i) \
+		{ \
+			SV_VIDEO[base+delta]  = udgs[offset+i]; \
+			delta+=XSize; \
+		} \
+		SV_VIDEO[base+(XSize)*7] = udgs[offset+7]; \
+		\
+		delta = 0; \
+		SWITCH_COLOR_BANK_ON(); \
+		for(i=0;i<7;++i) \
+		{ \
+			SV_VIDEO[base+delta]  = (image)->_color; \
+			delta+=XSize; \
+		} \
+		SV_VIDEO[base+(XSize)*7] = (image)->_color; \
+		SWITCH_COLOR_BANK_OFF(); \
+	}
 
+
+	#define __DELETE(x,y) \
+	{ \
+		unsigned char i; \
+		unsigned short base = (x)+(XSize)*8*(y); \
+		unsigned char delta = 0; \
+		\
+		for(i=0;i<7;++i) \
+		{ \
+			SV_VIDEO[(unsigned short) base+delta] = 0; \
+			delta+=XSize; \
+		} \
+		SV_VIDEO[base+(XSize)*7] = 0; \
+	}	
+#endif
+	
 #endif // _MO5_BIT_MAPPED_GRAPHICS
 

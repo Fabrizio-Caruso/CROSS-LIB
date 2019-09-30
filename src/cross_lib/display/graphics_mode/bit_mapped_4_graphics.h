68a6f8c2626831c5f5f2c4f1a6095f85fa1d537e
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-09-30 22:37:21 +0200

    tabs clean up

--- src/cross_lib/display/graphics_mode/bit_mapped_4_graphics.h
+++ src/cross_lib/display/graphics_mode/bit_mapped_4_graphics.h
@@ -3,10 +3,10 @@
 #define _BIT_MAPPED_4_GRAPHICS
 
 // TODO: Make this more general 
-#if defined(__SUPERVISION__)	
-	#define EXTRA_PADDING 8
+#if defined(__SUPERVISION__)    
+    #define EXTRA_PADDING 8
 #else
-	#define EXTRA_PADDING 0
+    #define EXTRA_PADDING 0
 #endif
 
 #define BYTES_PER_LINE ((XSize)*2+EXTRA_PADDING)
@@ -19,16 +19,16 @@ unsigned char right_map_one_to_two(unsigned char n);
 #if defined(NO_COLOR)
     #define __DRAW(x,y,image) \
     { \
-		unsigned char k; \
-		unsigned short base = (x)+(XSize)*8*(y); \
-		unsigned char delta = 0; \
-		unsigned short offset = (8*(unsigned char)(image)->_imageData) ; \
+        unsigned char k; \
+        unsigned short base = (x)+(XSize)*8*(y); \
+        unsigned char delta = 0; \
+        unsigned short offset = (8*(unsigned char)(image)->_imageData) ; \
         \
         for(k=0;k<8;++k) \
         { \
-			SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)]    = left_map_one_to_two(udgs[offset+k]); \
+            SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)]    = left_map_one_to_two(udgs[offset+k]); \
             SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)+1]  = right_map_one_to_two(udgs[offset+k]); \
-			delta+=XSize; \
+            delta+=XSize; \
         } \
     }
 

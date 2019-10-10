b040dcfb29580fee55da81fe0b37ccfd8a0429ad
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-10-10 12:46:24 +0200

    Improve Atari Lynx speed

--- src/chase/speed_game_settings.h
+++ src/chase/speed_game_settings.h
@@ -195,7 +195,7 @@
 		#define GAME_SLOW_DOWN 1200
 	#elif defined(__ATARI_LYNX__)
 		#define SLOW_DOWN 
-		#define GAME_SLOW_DOWN 2500U
+		#define GAME_SLOW_DOWN 4000U
 	#elif defined(__SVI__) && !defined(MSX_MODE0)
 		#define SLOW_DOWN
 		#define GAME_SLOW_DOWN 1200	
--- src/cross_lib/display/graphics_mode/tgi_graphics.h
+++ src/cross_lib/display/graphics_mode/tgi_graphics.h
@@ -8,22 +8,19 @@
 
 
 #define __DRAW(x,y,image) \
-	{ \
-	char chString[2]; \
-	chString[0] = image->_imageData; \
-	chString[1]='\0'; \
-	tgi_setcolor(image->_color); \
-	tgi_outtextxy((x*8),(y*8),chString); \
-	}
-	
+{ \
+    char chString[2]; \
+    chString[0] = image->_imageData; \
+    chString[1]='\0'; \
+    tgi_setcolor(image->_color); \
+    tgi_outtextxy((x*8),(y*8),chString); \
+}
+    
 
 #define __DELETE(x,y) \
-	{ \
-	char chString[2]; \
-	chString[0] = ' '; \
-	chString[1]='\0'; \
-	tgi_setbgcolor(_BG_COLOR); \
-	tgi_outtextxy((x*8),(y*8),chString); \
-	}    
-	
+{ \
+    tgi_setbgcolor(_BG_COLOR); \
+    tgi_outtextxy((x*8),(y*8)," "); \
+}    
+    
 #endif // _TGI_GRAPHICS

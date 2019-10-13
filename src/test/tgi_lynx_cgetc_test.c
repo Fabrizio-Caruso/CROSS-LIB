355e3f85212248b00b5f6587eef76250c7981338
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-10-13 18:39:01 +0200

    More lynx tests

--- src/test/tgi_lynx_cgetc_test.c
+++ src/test/tgi_lynx_cgetc_test.c
@@ -1,5 +1,6 @@
 
 #include <tgi.h>
+#include <conio.h>
 
 #define CLI() asm("\tcli")
 
@@ -18,11 +19,18 @@ int main(void)
     tgi_setbgcolor(TGI_COLOR_GREY);
 
     tgi_setcolor(TGI_COLOR_RED);
-    tgi_outtextxy(8*6,8,"** ##"); 
-    
-    tgi_setcolor(TGI_COLOR_WHITE);
-    tgi_outtextxy(8,8,"** ##"); 
 
+    tgi_outtextxy(8,8*0,"step 0"); 
+    
+    cgetc();   
+    tgi_outtextxy(8,8*1,"step 1"); 
+
+    cgetc();   
+    tgi_outtextxy(8,8*2,"step 2"); 
+    
+    cgetc();   
+    tgi_outtextxy(8,8*3,"step 3"); 
+    
     while(1){};
     return 0;
 }

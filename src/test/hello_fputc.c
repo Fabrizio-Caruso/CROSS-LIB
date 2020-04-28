e67d338fb6a08240240b60a02fc0a60fbecc124d
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-04-28 23:41:30 +0200

    Aquarius test with stack config

---
+++ src/test/hello_fputc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+    fputc_cons('h');
+    fputc_cons('e');
+    fputc_cons('l');
+    fputc_cons('l');
+    fputc_cons('o');
+    while(1){};
+    return 0;
+}
+

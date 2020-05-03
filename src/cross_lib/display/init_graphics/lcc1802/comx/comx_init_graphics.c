134d02b1e43594e98ce1c9559ab845aec8ac9c79
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-05-03 23:57:05 +0200

    Initialize video for COMX-35

--- src/cross_lib/display/init_graphics/lcc1802/comx/comx_init_graphics.c
+++ src/cross_lib/display/init_graphics/lcc1802/comx/comx_init_graphics.c
@@ -10,8 +10,8 @@ void vidchar(int vidmem, int character){ //write character to vidmem location in
 
 void INIT_GRAPHICS(void)
 {
-	// asm(" ldiReg R8,0xF800\n"
-	    // " sex R8\n"
-	    // " out 7\n"
-	    // " sex R2\n");
+	asm(" ldiReg R8,0xF800\n"
+	    " sex R8\n"
+	    " out 7\n"
+	    " sex R2\n");
 }

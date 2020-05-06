2cc2359191847d359d7574fc1926148e005ff208
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-05-07 00:57:07 +0200

    Simplest case

---
+++ src/test/comx_simplest_class.c
@@ -0,0 +1,50 @@
+
+void setvideobase(unsigned int vidmem){
+    asm( //vidmem pointer is R12
+        " sex R12\n"
+        " out 7\n"
+        " sex R2\n");
+}
+
+
+void vidstrcpy(unsigned int vidmem,char * text){ //write to video memory
+	asm(
+	"$$cpy:\n"
+	" lda R13 ;pick up input pointer\n"
+	" b1  $	;wait til video is quiet\n"
+	" str R12 ;move the byte\n"
+	" inc R12 ;++\n"
+	" bnz $$cpy\n");
+}
+
+
+struct ClassStruct
+{
+    int data;
+    void (*method_t)(void);
+};
+
+typedef struct ClassStruct class_t;
+
+unsigned int vidmem=0xf800;
+    
+void _print_42(void)
+{
+    vidstrcpy(vidmem+40, "42");
+}
+
+
+int main()
+{
+    class_t foo;
+
+    setvideobase(vidmem);
+
+    foo.method_t = _print_42;
+    
+    foo.method_t();
+    
+
+
+    return 0;
+}
---
+++ src/test/gcc_simplest_class.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+
+struct ClassStruct
+{
+    int data;
+    void (*method_t)(void);
+};
+typedef struct ClassStruct class_t;
+
+
+void _print_42(void)
+{
+    printf("42\n");
+}
+
+
+
+int main()
+{
+    class_t foo;
+   
+    foo.method_t = _print_42;
+
+    foo.method_t();
+
+    return EXIT_SUCCESS;
+}

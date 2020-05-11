d80f179bb35e924296d643997027a63ea4fa9056
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-05-11 15:11:53 +0200

    Faster linear congruential generator

--- src/cross_lib/rand/rand.c
+++ src/cross_lib/rand/rand.c
@@ -1,7 +1,18 @@
-static unsigned long int next = 1;
 
+
+#if defined(__COMX__) || defined(__PECOM__)
+static unsigned int next = 1;
+int rand(void) // RAND_MAX assumed to be 32767
+{
+    next = next * 115 + 125;
+    return next;
+}
+
+#else
+static unsigned long int next = 1;
 int rand(void) // RAND_MAX assumed to be 32767
 {
     next = next * 1103515245 + 12345;
-    return (unsigned int)(next/65536) % 32768;
+    return (unsigned int)(next>>16)&0x7FFF;
 }
+#endif

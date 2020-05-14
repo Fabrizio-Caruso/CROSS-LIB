fb9177ca842c91af4c5aa4024231c3cb18dc71db
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-05-15 01:09:45 +0200

    new hello world for comx

--- src/test/hwcomx.c
+++ src/test/hwcomx.c
@@ -288,30 +288,26 @@ void monochrome(unsigned char mono){
 		" sex R2\n");
 }
 
-void initrand(){
-	asm(
-	" ldireg R8, 0x407e\n"	
-    "$$loop:\n"
+unsigned short int rand(){
+	asm( 
+	" ldireg R8, 0x42be\n"		// COMX BASIC RND seed value
+	" lda R8\n"	
+	" bnz $$rand1\n"			// Do rand if high byte of seed value != 0
+    "$$zerolow:\n"
+	" glo R0\n"					
+	" bz $$zerolow\n"			// Wait until R0.0 != 0
+	" phi R15\n"				// Use R0.0 as low byte of seed value
+    "$$zerohigh:\n"
 	" ghi R0\n"
-	" bz $$loop\n"
-	" str R8\n"
-	" inc R8\n"
-	" glo R0\n"
-	" str R8\n");
-}
-
-/*
-unsigned short int rand(unsigned char bits){
-	asm( // LFSR is shifted 'bits' times stored in R12.0
-	" ldireg R8, 0x407e\n"	
-	" lda R8\n"	
-	" phi R15\n"	
+	" bnz $$rand2\n"			// If R0.1 != 0 use as high byte of seed value
+	" br $$zerohigh\n"			// Wait until R0.1 != 0
+   "$$rand1:\n"
+	" phi R15\n"	// R15.1 = high byte of seed value
 	" ldn 8\n"	
-	" plo R15\n"	// R15&D = low byte of seed value
-    "$$loop:\n"
-	" glo R15\n"
+   "$$rand2:\n"
+	" plo R15\n"	// R15.0 = low byte of seed value
 	" ani 1\n"
-	" str R2\n"		// Stack = bit 0 seed value, rest is 0
+	" str R2\n"		// Stack = bit 0 of seed value, rest is 0
 	" glo R15\n"	// D = low byte of seed value
 	" shr\n"	    
 	" shr\n"		
@@ -332,64 +328,16 @@ unsigned short int rand(unsigned char bits){
 	" shr\n"		// DF = new input bit
 	" ghi R15\n"
 	" shrc\n"
-	" phi R15\n"	// R15.1 >> 1, MSB = DF, new input bit, DF = LSB
 	" dec R8\n"
-	" str R8\n"		// Store high byte new seed value
+	" str R8\n"		// Store high byte new seed value, 
+					// R15.1 >> 1, MSB = DF, new input bit, DF = LSB
+	" ani 0x7f\n"
+	" phi R15\n"	// R15.1 = seed value & 0x7f, allowing only postivie numbers
 	" glo R15\n"
 	" shrc\n"
 	" plo R15\n"	// R15.0 >> 1, MSB = DF, DF = LSB
 	" inc R8\n"
 	" str R8\n"		// Store low byte new seed value
-	" dec R12\n"
-	" glo R12\n"
-	" bnz $$loop\n"
-	" Cretn\n");
-	return 0; //this statement will never be executed but it keeps the compiler happy
-}
-*/
-
-unsigned short int _rand_aux(unsigned char bits){
-	asm( // LFSR is shifted 'bits' times stored in R12.0
-	" ldireg R8, 0x407e\n"	
-	" lda R8\n"	
-	" phi R15\n"	
-	" ldn 8\n"	
-	" plo R15\n"	// R15&D = low byte of seed value
-    "$$loop:\n"
-	" glo R15\n"
-	" ani 1\n"
-	" str R2\n"		// Stack = bit 0 seed value, rest is 0
-	" glo R15\n"	// D = low byte of seed value
-	" shr\n"	    
-	" shr\n"		
-	" xor\n"		// D bit 0 = bit 0 XOR bit 2
-	" plo R9\n"
-	" ani 1\n"
-	" str R2\n"		// Stack = bit 0 XOR bit 2
-	" glo R9\n"
-	" shr\n"		
-	" xor\n"		// D bit 0 = bit 0/2 XOR bit 3
-	" plo R9\n"
-	" ani 1\n"
-	" str R2\n"		// Stack = bit 0/2 XOR bit 3
-	" glo R9\n"
-	" shr\n"		
-	" shr\n"		
-	" xor\n"		// D bit 0 = bit 0/2/3  XOR bit 5
-	" shr\n"		// DF = new input bit
-	" ghi R15\n"
-	" shrc\n"
-	" phi R15\n"	// R15.1 >> 1, MSB = DF, new input bit, DF = LSB
-	" dec R8\n"
-	" str R8\n"		// Store high byte new seed value
-	" glo R15\n"
-	" shrc\n"
-	" plo R15\n"	// R15.0 >> 1, MSB = DF, DF = LSB
-	" inc R8\n"
-	" str R8\n"		// Store low byte new seed value
-	" dec R12\n"
-	" glo R12\n"
-	" bnz $$loop\n"
 	" Cretn\n");
 	return 0; //this statement will never be executed but it keeps the compiler happy
 }
@@ -499,9 +447,8 @@ void main(){
     printf("0,20");
 	gotoxy(10,10);
     printf("10,10");
-	initrand();
 	while(1)
-		printf(" %i",_rand_aux(1)&0x7fff);
+		printf(" %i",rand());
 }
 
 #include <nstdlib.c>

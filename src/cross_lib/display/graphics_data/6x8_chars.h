6a0ec26bfe2699dbd7c33e009546e47099a3a134
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-06-14 11:21:13 +0200

    refactor comx init to use shared graphics data

--- src/cross_lib/display/graphics_data/6x8_chars.h
+++ src/cross_lib/display/graphics_data/6x8_chars.h
@@ -1,27 +1,35 @@
 #ifndef _6X8_CHARS
 #define _6X8_CHARS
 
-	#define _PLAYER_UDG                   {12,18,12,51,45,12,18,51}
-	#define _PLAYER_DOWN_UDG              {12,18,12,51,45,12,18,51}
-	#define _PLAYER_UP_UDG                {12,30,12,51,45,12,18,51}
-	#define _PLAYER_LEFT_UDG              {12,22,12,62,13,12,20,20}
-	#define _PLAYER_RIGHT_UDG             {12,26,12,31,44,12,10,10}
+#if defined(NINTH_CHAR_LINE)
+    #define NINTH_EMPTY_LINE 0
+    #define NINTH_VERTICAL_BRICK_LINE 12
+#else
+    #define NINTH_EMPTY_LINE
+    #define NINTH_VERTICAL_BRICK_LINE 
+#endif
 
-	#define _GHOST_UDG                    {33,30,33,51,33,45,33,30}
-	#define _SKULL_UDG                    {30,33,51,33,33,18,18,12}
-	#define _GUN_UDG                      { 0,32,31,40,56,32, 0, 0}
-	#define _POWERUP_UDG                  { 0,30,51,55,51,26,18,12}
-	#define _BULLET_UDG                   { 0, 0, 4,28,14, 8, 0, 0}
-	#define _BOMB_UDG                     {30,33,51,45,45,51,33,30}
+	#define _PLAYER_UDG                   {12,18,12,51,45,12,18,51, NINTH_EMPTY_LINE}
+	#define _PLAYER_DOWN_UDG              {12,18,12,51,45,12,18,51, NINTH_EMPTY_LINE}
+	#define _PLAYER_UP_UDG                {12,30,12,51,45,12,18,51, NINTH_EMPTY_LINE}
+	#define _PLAYER_LEFT_UDG              {12,22,12,62,13,12,20,20, NINTH_EMPTY_LINE}
+	#define _PLAYER_RIGHT_UDG             {12,26,12,31,44,12,10,10, NINTH_EMPTY_LINE}
 
-	#define _DEAD_GHOST_UDG               {18,30,33,51,33,45,51,30}
+	#define _GHOST_UDG                    {33,30,33,51,33,45,33,30, NINTH_EMPTY_LINE}
+	#define _SKULL_UDG                    {30,33,51,33,33,18,18,12, NINTH_EMPTY_LINE}
+	#define _GUN_UDG                      { 0,32,31,40,56,32, 0, 0, NINTH_EMPTY_LINE}
+	#define _POWERUP_UDG                  { 0,30,51,55,51,26,18,12, NINTH_EMPTY_LINE}
+	#define _BULLET_UDG                   { 0, 0, 4,28,14, 8, 0, 0, NINTH_EMPTY_LINE}
+	#define _BOMB_UDG                     {30,33,51,45,45,51,33,30, NINTH_EMPTY_LINE}
+
+	#define _DEAD_GHOST_UDG               {18,30,33,51,33,45,51,30, NINTH_EMPTY_LINE}
 	
-	#define _ROCKET_UDG                   {12,30,30,30,63,45,33,33}
-	#define _LEFT_HORIZONTAL_MISSILE_UDG { 0, 0,56,31,31,56, 0, 0}
-	#define _RIGHT_HORIZONTAL_MISSILE_UDG  { 0, 0, 7,62,62, 7, 0, 0}
-	#define _INVINCIBILITY_UDG            {12,18,12,51, 0,12, 0,51}
-	#define _VERTICAL_BRICK_UDG           {12,12,28,12,12,14,12,12}
-	#define _HORIZONTAL_BRICK_UDG         { 0, 0, 0,63,63,00,00,00}
+	#define _ROCKET_UDG                   {12,30,30,30,63,45,33,33, NINTH_EMPTY_LINE}
+	#define _LEFT_HORIZONTAL_MISSILE_UDG { 0, 0,56,31,31,56, 0, 0, NINTH_EMPTY_LINE}
+	#define _RIGHT_HORIZONTAL_MISSILE_UDG  { 0, 0, 7,62,62, 7, 0, 0, NINTH_EMPTY_LINE}
+	#define _INVINCIBILITY_UDG            {12,18,12,51, 0,12, 0,51, NINTH_EMPTY_LINE}
+	#define _VERTICAL_BRICK_UDG           {12,12,28,12,12,14,12,12, NINTH_VERTICAL_BRICK_LINE}
+	#define _HORIZONTAL_BRICK_UDG         { 0, 0, 0,63,63,00,00,00, NINTH_EMPTY_LINE}
 	#define _EXTRA_POINTS_UDG             
 
 #endif // _6x8_CHARS
--- src/cross_lib/display/init_graphics/lcc1802/comx/comx_init_graphics.c
+++ src/cross_lib/display/init_graphics/lcc1802/comx/comx_init_graphics.c
@@ -1,69 +1,46 @@
 #include <devkit/video/vis_video.h>
 #include <devkit/system/system.h>
 
+#include "6x8_chars.h"
+
 #if defined(__CIDELSA__)
-#include <devkit/video/cidelsa_char.h>
+    #include <devkit/video/cidelsa_char.h>
 #endif
 
 
 #if defined(__COMX__) || defined(__PECOM__) || defined(REDEFINED_CHARS)
-    void redefine_char(const unsigned char * shapelocation, int color)
+    void redefine_char(unsigned char ch, const unsigned char * shapelocation, int color)
     {
         unsigned char colored_shape[9];
         unsigned char i;
         
         for(i=0;i<9;++i)
         {
-           colored_shape[i]=shapelocation[i+1]+color*64;
+           colored_shape[i]=shapelocation[i]+color*64;
         }
-        shapechar(colored_shape, *shapelocation, 1);
+        shapechar(colored_shape, ch, 1);
     }
 
-    #define __PLAYER_DOWN_UDG              12,18,12,51,45,12,18,51
-    #define __PLAYER_UP_UDG                12,30,12,51,45,12,18,51
-    #define __PLAYER_LEFT_UDG              12,22,12,62,13,12,20,20
-    #define __PLAYER_RIGHT_UDG             12,26,12,31,44,12,10,10
 
-
-    #define __GHOST_UDG                    33,30,33,51,33,45,33,30
-    #define __SKULL_UDG                    30,33,51,33,33,18,18,12
-    #define __GUN_UDG                       0,32,31,40,56,32, 0, 0
-    #define __POWERUP_UDG                   0,30,51,55,51,26,18,12
-    #define __BULLET_UDG                    0, 0, 4,28,14, 8, 0, 0
-    #define __BOMB_UDG                     30,33,51,45,45,51,33,30
-    #define __INVINCIBILITY_UDG            12,18,12,51, 0,12, 0,51
-
-    #define __DEAD_GHOST_UDG               18,30,33,51,33,45,51,30
-
-    #define __ROCKET_UDG                   12,30,30,30,63,45,33,33
-    #define __LEFT_HORIZONTAL_MISSILE_UDG   0, 0,56,31,31,56, 0, 0
-    #define __RIGHT_HORIZONTAL_MISSILE_UDG  0, 0, 7,62,62, 7, 0, 0
-    #define __VERTICAL_BRICK_UDG           12,12,28,12,12,14,12,12
-    #define __HORIZONTAL_BRICK_UDG          0, 0, 0,63,63,00,00,00
-
-
-
-// 106 - 124
-    const unsigned char invincibility_udg[10] =  { 106 , __INVINCIBILITY_UDG, 0};
-    const unsigned char player_down[10] = { 107, __PLAYER_DOWN_UDG, 0 };
-    const unsigned char player_up[10] = { 108, __PLAYER_UP_UDG, 0 };
-    const unsigned char player_right[10] = { 109 , __PLAYER_LEFT_UDG, 0 };
-    const unsigned char player_left[10] = { 110 , __PLAYER_RIGHT_UDG, 0};
-    const unsigned char bomb[10] =  { 111 , __BOMB_UDG, 0};
-    const unsigned char ghost[10] = { 112 , __GHOST_UDG, 0 };
-    const unsigned char skull_udg[10] = { 113 , __SKULL_UDG, 0 };
-    const unsigned char power_udg[10] = { 114 , __POWERUP_UDG, 0 };
-    const unsigned char gun_udg[10] =  { 115 , __GUN_UDG, 0};
-    const unsigned char bullet_udg[10] =  { 116 , __BULLET_UDG, 0};
-    const unsigned char dead_ghost_udg[10] = {117, __DEAD_GHOST_UDG, 0};
-    const unsigned char rocket_udg[10] = { 118 , __ROCKET_UDG, 0 };
-    const unsigned char left_missile_udg[10] = { 119 , __LEFT_HORIZONTAL_MISSILE_UDG, 0 };
-    const unsigned char right_missile_udg[10] =  { 120 , __RIGHT_HORIZONTAL_MISSILE_UDG, 0};
-    const unsigned char vertical_brick_udg[10] =  { 121 , __VERTICAL_BRICK_UDG, 12 };
-    const unsigned char horizontal_brick_udg[10] = {122, __HORIZONTAL_BRICK_UDG, 0};
-    const unsigned char freeze_udg[10] = { 123 , __POWERUP_UDG, 0 };
-    const unsigned char super_udg[10] = { 124 , __POWERUP_UDG, 0 };
-
+    const unsigned char invincibility_udg[9] =  _INVINCIBILITY_UDG;
+    const unsigned char player_down[9] = _PLAYER_DOWN_UDG;
+    const unsigned char player_up[9] = _PLAYER_UP_UDG;
+    const unsigned char player_right[9] = _PLAYER_LEFT_UDG;
+    const unsigned char player_left[9] = _PLAYER_RIGHT_UDG;
+    const unsigned char bomb[9] =  _BOMB_UDG;
+    const unsigned char ghost[9] = _GHOST_UDG;
+    const unsigned char skull_udg[9] = _SKULL_UDG;
+    const unsigned char power_udg[9] = _POWERUP_UDG;
+    const unsigned char gun_udg[9] =  _GUN_UDG;
+    const unsigned char bullet_udg[9] =  _BULLET_UDG;
+    const unsigned char dead_ghost_udg[9] = _DEAD_GHOST_UDG;
+    const unsigned char rocket_udg[9] = _ROCKET_UDG;
+    const unsigned char left_missile_udg[9] = _LEFT_HORIZONTAL_MISSILE_UDG;
+    const unsigned char right_missile_udg[9] =  _RIGHT_HORIZONTAL_MISSILE_UDG;
+    const unsigned char vertical_brick_udg[9] =  _VERTICAL_BRICK_UDG;
+    const unsigned char horizontal_brick_udg[9] = _HORIZONTAL_BRICK_UDG;
+    const unsigned char freeze_udg[9] = _POWERUP_UDG;
+    const unsigned char super_udg[9] = _POWERUP_UDG;
 
 
 #endif
@@ -80,27 +57,27 @@ void INIT_GRAPHICS(void)
     #endif
     
     #if defined(__COMX__) || defined(__PECOM__) || defined(REDEFINED_CHARS) 
-        redefine_char(player_down, 3);
-        redefine_char(player_up, 3);
-        redefine_char(player_right, 3);
-        redefine_char(player_left, 3);
-        redefine_char(ghost, 3);
-        redefine_char(bomb, 0);
-        redefine_char(invincibility_udg, 3);
+        redefine_char(106, invincibility_udg, 3);
+        redefine_char(107, player_down, 3);
+        redefine_char(108, player_up, 3);
+        redefine_char(109, player_right, 3);
+        redefine_char(110, player_left, 3);
+        redefine_char(111, bomb, 0);
+        redefine_char(112, ghost, 3);
 
-        redefine_char(skull_udg, 2);
-        redefine_char(power_udg, 2);
-        redefine_char(gun_udg, 1);
-        redefine_char(bullet_udg, 3);
-        redefine_char(dead_ghost_udg, 0);
+        redefine_char(113, skull_udg, 2);
+        redefine_char(114, power_udg, 2);
+        redefine_char(115, gun_udg, 1);
+        redefine_char(116, bullet_udg, 3);
+        redefine_char(117, dead_ghost_udg, 0);
 
-        redefine_char(rocket_udg, 3);
-        redefine_char(left_missile_udg, 3);
-        redefine_char(right_missile_udg, 3);
-        redefine_char(vertical_brick_udg, 2);
-        redefine_char(horizontal_brick_udg, 2);
-        redefine_char(freeze_udg, 3);
-        redefine_char(super_udg, 0);
+        redefine_char(118, rocket_udg, 3);
+        redefine_char(119, left_missile_udg, 3);
+        redefine_char(120, right_missile_udg, 3);
+        redefine_char(121, vertical_brick_udg, 2);
+        redefine_char(122, horizontal_brick_udg, 2);
+        redefine_char(123, freeze_udg, 3);
+        redefine_char(124, super_udg, 0);
     #endif
     #if defined(__COMX__) || defined(__PECOM__) || defined(__CIDELSA__)
         disableinterrupt();

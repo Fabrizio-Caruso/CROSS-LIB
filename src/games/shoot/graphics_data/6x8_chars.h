d5515d6c5472b09d4a2afccd63ad286ebb225769
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-07-11 09:58:00 +0200

    Atari and Oric Atmos versions

--- src/games/shoot/graphics_data/6x8_chars.h
+++ src/games/shoot/graphics_data/6x8_chars.h
@@ -9,28 +9,28 @@
     #define NINTH_VERTICAL_BRICK_LINE 
 #endif
 
-	#define _PLAYER_UDG                   {12,18,12,51,45,12,18,51, NINTH_EMPTY_LINE}
-	#define _PLAYER_DOWN_UDG              {12,18,12,51,45,12,18,51, NINTH_EMPTY_LINE}
-	#define _PLAYER_UP_UDG                {12,30,12,51,45,12,18,51, NINTH_EMPTY_LINE}
-	#define _PLAYER_LEFT_UDG              {12,22,12,62,13,12,20,20, NINTH_EMPTY_LINE}
-	#define _PLAYER_RIGHT_UDG             {12,26,12,31,44,12,10,10, NINTH_EMPTY_LINE}
+	#define _TILE_0_UDG              {12,18,12,51,45,12,18,51, NINTH_EMPTY_LINE}
+	#define _TILE_0_UDG              {12,18,12,51,45,12,18,51, NINTH_EMPTY_LINE}
+	#define _TILE_1_UDG              {12,30,12,51,45,12,18,51, NINTH_EMPTY_LINE}
+	#define _TILE_2_UDG              {12,22,12,62,13,12,20,20, NINTH_EMPTY_LINE}
+	#define _TILE_3_UDG              {12,26,12,31,44,12,10,10, NINTH_EMPTY_LINE}
 
-	#define _GHOST_UDG                    {33,30,33,51,33,45,33,30, NINTH_EMPTY_LINE}
-	#define _SKULL_UDG                    {30,33,51,33,33,18,18,12, NINTH_EMPTY_LINE}
-	#define _GUN_UDG                      { 0,32,31,40,56,32, 0, 0, NINTH_EMPTY_LINE}
-	#define _POWERUP_UDG                  { 0,30,51,55,51,26,18,12, NINTH_EMPTY_LINE}
-	#define _BULLET_UDG                   { 0, 0, 4,28,14, 8, 0, 0, NINTH_EMPTY_LINE}
-	#define _BOMB_UDG                     {30,33,51,45,45,51,33,30, NINTH_EMPTY_LINE}
+	#define _TILE_4_UDG              {33,30,33,51,33,45,33,30, NINTH_EMPTY_LINE}
+	#define _TILE_7_UDG              {30,33,51,33,33,18,18,12, NINTH_EMPTY_LINE}
+	#define _TILE_9_UDG              { 0,32,31,40,56,32, 0, 0, NINTH_EMPTY_LINE}
+	#define _TILE_8_UDG              { 0,30,51,55,51,26,18,12, NINTH_EMPTY_LINE}
+	#define _TILE_11_UDG             { 0, 0, 4,28,14, 8, 0, 0, NINTH_EMPTY_LINE}
+	#define _TILE_5_UDG              {30,33,51,45,45,51,33,30, NINTH_EMPTY_LINE}
 
-	#define _DEAD_GHOST_UDG               {18,30,33,51,33,45,51,30, NINTH_EMPTY_LINE}
+	#define _TILE_6_UDG              {18,30,33,51,33,45,51,30, NINTH_EMPTY_LINE}
 	
-	#define _ROCKET_UDG                   {12,30,30,30,63,45,33,33, NINTH_EMPTY_LINE}
-	#define _LEFT_HORIZONTAL_MISSILE_UDG { 0, 0,56,31,31,56, 0, 0, NINTH_EMPTY_LINE}
-	#define _RIGHT_HORIZONTAL_MISSILE_UDG  { 0, 0, 7,62,62, 7, 0, 0, NINTH_EMPTY_LINE}
-	#define _INVINCIBILITY_UDG            {12,18,12,51, 0,12, 0,51, NINTH_EMPTY_LINE}
-	#define _VERTICAL_BRICK_UDG           {12,12,28,12,12,14,12,12, NINTH_VERTICAL_BRICK_LINE}
-	#define _HORIZONTAL_BRICK_UDG         { 0, 0, 0,63,63,00,00,00, NINTH_EMPTY_LINE}
-	#define _EXTRA_POINTS_UDG             
+	#define _TILE_16_UDG             {12,30,30,30,63,45,33,33, NINTH_EMPTY_LINE}
+	#define _TILE_14_UDG             { 0, 0,56,31,31,56, 0, 0, NINTH_EMPTY_LINE}
+	#define _TILE_15_UDG             { 0, 0, 7,62,62, 7, 0, 0, NINTH_EMPTY_LINE}
+	#define _TILE_20_UDG             {12,18,12,51, 0,12, 0,51, NINTH_EMPTY_LINE}
+	#define _TILE_12_UDG             {12,12,28,12,12,14,12,12, NINTH_VERTICAL_BRICK_LINE}
+	#define _TILE_13_UDG             { 0, 0, 0,63,63,00,00,00, NINTH_EMPTY_LINE}
+	#define _TILE_10_UDG             
 
 #endif // _6x8_CHARS
 
--- src/games/shoot/graphics_data/8x8_chars.h
+++ src/games/shoot/graphics_data/8x8_chars.h
@@ -6,13 +6,17 @@
 #define _TILE_3_UDG     { 24, 44,152,110, 25, 24, 40, 40}
 #define _TILE_2_UDG     { 24, 52, 25,118,152, 24, 20, 20}
 
-#define _TILE_4_UDG     {129,126,165,129,129,189,129,126}
+// Ghost
+// #define _TILE_4_UDG     {129,126,165,129,129,189,129,126}
+#define _TILE_4_UDG     { 0x3c,0x7e,0xff,0x99,0x7e,0x3c,0x7e,0xdb}
 
 #define _TILE_7_UDG     { 60, 66,165,129, 90, 36, 36, 60}
 
-#define _TILE_9_UDG     {  0,128,126,200,248,192,128,  0}
+// #define _TILE_9_UDG     {  0,128,126,200,248,192,128,  0}
+#define _TILE_9_UDG     {0x81,0x42,0x18,0x34,0x2c,0x18,0x42,0x81}
 
-#define _TILE_8_UDG     {  0, 60, 54,223,231,122, 36, 24}
+// Power-up (Calm-Down)
+#define _TILE_8_UDG     {  0, 60, 54,223,231,122, 36, 24} 
 
 #define _TILE_11_UDG    {  0,  0,  8, 56, 28, 16,  0,  0}
 
@@ -26,7 +30,9 @@
 
 #define _TILE_20_UDG    { 24, 36, 24,  0,153,  0, 36,102}
 
-#define _TILE_6_UDG     {129,126,165,129,129,189,129,126}
+// Dead Ghost
+// #define _TILE_6_UDG     {129,126,165,129,129,189,129,126}
+#define _TILE_6_UDG     { 0x3c,0x7e,0xff,0x99,0x7e,0x3c,0x7e,0xdb}
 
 #define _TILE_12_UDG    { 24, 24, 24, 48, 24, 12, 24, 24}
 

c0bd1eae21a50f98ba8462cfc59e761552fabf09
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-07-20 10:04:24 +0200

    Use lists in generate assets

--- src/meta-code/generate_assets.py
+++ src/meta-code/generate_assets.py
@@ -1,20 +1,26 @@
 #!/bin/python
 
+NUMBER_OF_TILES = 1
+
+tile=[]
+
 #read tile0
-with open('tile0.txt', 'r') as myfile:
-  tile0 = myfile.read()
+for i in [0,NUMBER_OF_TILES-1]:
+    with open('tile'+str(i)+'.txt', 'r') as myfile:
+        tile.append(myfile.read())
 
 
 #input file
 fin = open("template_8x8_chars.txt", "rt")
 
 #output file to write the result to
-fout = open("8x8_chars.h", "wt")
+fout = open("./generated_assets/8x8_chars.h", "wt")
 
 #for each line in the input file
-for line in fin:
-	#read replace the string and write to output file
-	fout.write(line.replace('<tile_0>', tile0))
+for i in [0,NUMBER_OF_TILES-1]:
+    for line in fin:
+            #read replace the string and write to output file
+            fout.write(line.replace('<tile_'+str(i)+'>', tile[i]))
 #close input and output files
 fin.close()
 fout.close()

e4699ed0591cb09c77792f778395bf3fe30872a3
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-07-21 10:19:58 +0200

    Experimental generate template code

--- src/meta-code/generate_assets.py
+++ src/meta-code/generate_assets.py
@@ -2,10 +2,16 @@
 
 import sys
 
+templates = False
+
 if len(sys.argv)<2:
     game_dir = "chase"
 else:
     game_dir = sys.argv[1]
+    if len(sys.argv)>1:
+        templates = True 
+        template_name = argv[2]
+
 
 NUMBER_OF_TILES = 24
 
@@ -19,20 +25,52 @@ for i in range(NUMBER_OF_TILES):
 
 file_names = ["8x8_chars.h", "xchase.asm", "cpc_Chars8.asm"]
 
-for file_name in file_names:
 
-    fin = open("./templates/"+file_name+".template", "rt")
-    fout = open("./generated_assets/"+game_dir+"/"+file_name, "wt")
+def generate_assets():
+    for file_name in file_names:
+
+        fin = open("./templates/"+file_name+".template", "rt")
+        fout = open("./generated_assets/"+game_dir+"/"+file_name, "wt")
+
+        for line in fin:
+            newline = line
+            # print("initial line: "+newline)
+            for i in range(NUMBER_OF_TILES):
+                #read replace the string
+                newline = newline.replace('<tile_'+str(i)+'>', tile[i])
+            fout.write(newline)
+            if line != newline:
+                print("changing \n"+line+"with\n"+newline)
+        #close input and output files
+        fin.close()
+        fout.close()
+
+
+def generate_template():
+
+    fin = open("./templates/"+template_name, "rt")
+    fout = open("./templates/"+template_name+".template",  "wt")
 
     for line in fin:
         newline = line
         # print("initial line: "+newline)
         for i in range(NUMBER_OF_TILES):
             #read replace the string
-            newline = newline.replace('<tile_'+str(i)+'>', tile[i])
+            newline = newline.replace(tile[i],'<tile_'+str(i)+'>', tile[i])
         fout.write(newline)
         if line != newline:
             print("changing \n"+line+"with\n"+newline)
     #close input and output files
     fin.close()
     fout.close()
+
+
+def main():
+    if not templates:
+        generate_assets()
+    else:
+        generate_template()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()

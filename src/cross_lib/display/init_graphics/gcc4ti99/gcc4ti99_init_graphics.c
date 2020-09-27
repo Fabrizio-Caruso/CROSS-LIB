f6df68720030f4ae7faf4c3eef76a5b2ec11c819
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-09-27 20:11:02 +0200

    Experimental TI99 (broken)

---
+++ src/cross_lib/display/init_graphics/gcc4ti99/gcc4ti99_init_graphics.c
@@ -0,0 +1,18 @@
+// #include <cross_lib.h>
+
+#include "system.h"
+#include <conio.h>
+
+void INIT_GRAPHICS(void)
+{
+    set_text();
+    charsetlc();
+    textcolor(COLOR_WHITE);
+    bgcolor(COLOR_DKBLUE);
+    
+    gotoxy(2,2);
+    cprintf("Hello world\n");
+    
+    while(1){};
+}
+

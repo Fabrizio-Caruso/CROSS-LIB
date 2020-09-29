4cce25f2ba9e84eee605b5bd4d0a932ff6953047
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-09-29 11:15:31 +0200

    Init screen for TI99

--- src/cross_lib/display/init_graphics/gcc4ti99/gcc4ti99_init_graphics.c
+++ src/cross_lib/display/init_graphics/gcc4ti99/gcc4ti99_init_graphics.c
@@ -7,11 +7,8 @@ void INIT_GRAPHICS(void)
     set_text();
     charsetlc();
     textcolor(COLOR_WHITE);
-    bgcolor(COLOR_DKBLUE);
-    
-    gotoxy(2,2);
-    cprintf("Hello world\n");
-    
-    while(1){};
+    // bgcolor(COLOR_DKBLUE);
+    bgcolor(COLOR_BLACK);
+
 }
 

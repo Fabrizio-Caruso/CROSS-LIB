f042ea8ffd17f1ac22b76415734a80b95c3a227b
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-11-27 17:05:54 +0100

    Improve some alt_print for tgy and memory mapped targets

--- src/cross_lib/display/alt_print/tgi_alt_print.c
+++ src/cross_lib/display/alt_print/tgi_alt_print.c
@@ -12,21 +12,17 @@ void PRINT(uint8_t x, uint8_t y, char * str)
 void PRINTD(uint8_t x, uint8_t y, uint8_t length, uint16_t val)
 {
 	uint8_t i;
-	uint8_t digits[5];
+	uint8_t digit;
     unsigned char str[2];
     
     str[1]='\0';
 	
 	for(i=0;i<length;++i)
 	{
-		digits[i] = (uint8_t) ((val)%10);
-		val-= digits[i];
+		digit = (uint8_t) ((val)%10);
+		val-= digit;
 		val/=10;
-	}
-	
-	for(i=0;i<length;++i)
-	{
-        str[0] = (uint8_t) (digits[i])+48;
+        str[0] = (uint8_t) (digit+(uint8_t) 48u);
 		tgi_outtextxy((x+length-1-i)*8,y*8, str );
         
 	}

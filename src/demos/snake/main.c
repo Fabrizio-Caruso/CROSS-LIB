39ddaedf1cbe18dfa3260156f15c3ea3ad979e51
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-12-02 22:28:48 +0100

    Modular arithmetic

--- src/demos/snake/main.c
+++ src/demos/snake/main.c
@@ -102,27 +102,40 @@ struct SnakeBodyStruct
     uint8_t x;
     uint8_t y;
 };
+typedef struct SnakeBodyStruct SnakeBody;
+
+SnakeBody snake[MAX_SNAKE_LENGTH];
 
 enum Direction {UP, DOWN, LEFT, RIGHT};
 
 enum Direction direction;
 
-typedef struct SnakeBodyStruct SnakeBody;
-
-SnakeBody snake[MAX_SNAKE_LENGTH];
-
 uint8_t snake_length;
+
+uint8_t snake_head;
 
 Image *head_image_ptr;
 
 void draw_head(void)
 {
-    _XLIB_DRAW(snake[0].x,snake[0].y,head_image_ptr);
+    _XLIB_DRAW(snake[snake_head].x,snake[snake_head].y,head_image_ptr);
 }
 
-void draw_body(uint8_t i)
+void draw_body_part(uint8_t i)
 {
     _XLIB_DRAW(snake[i].x,snake[i].y,&BODY_IMAGE);
+}
+
+void draw_snake(void)
+{
+    uint8_t i;
+    
+    draw_head();
+
+    for(i=1;i<snake_length;++i)
+    {
+        draw_body_part((i+snake_head) % snake_length);
+    }
 }
 
 void init_snake(void)
@@ -131,26 +144,19 @@ void init_snake(void)
     uint8_t i;
     
     snake_length = INIT_SNAKE_LENGTH;
+    snake_head = 0;
     
-    for(i=0;i<INIT_SNAKE_LENGTH;++i)
+    for(i=0;i<snake_length;++i)
     {
-        snake[i].x = XSize/2+snake_length/2-i;
-        snake[i].y = YSize/2;
-        
+        snake[(i+snake_head)%snake_length].x = XSize/2+snake_length/2-i;
+        snake[(i+snake_head)%snake_length].y = YSize/2;
     }
     
     head_image_ptr = &HORIZONTAL_HEAD_IMAGE;
     
-    draw_head();
-
-    for(i=1;i<INIT_SNAKE_LENGTH;++i)
-    {
-        draw_body(i);
-    }
-    
-    
-    
+    draw_snake();
 }
+
 
 
 int main(void)

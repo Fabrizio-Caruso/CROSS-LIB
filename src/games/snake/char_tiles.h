bce0e9c58f0eace5a5a0b860433c359ca6cccd76
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2020-12-14 00:33:12 +0100

    ASCII tiles for XSnake

--- src/games/snake/char_tiles.h
+++ src/games/snake/char_tiles.h
@@ -3,30 +3,44 @@
 
 #include "cross_lib.h"
 
-// Plane
-#define _TILE_1 '-'
-#define _TILE_2 '>'
 
-// Animated plane
-#define _TILE_0 '='
-#define _TILE_3 _TILE_1
-#define _TILE_17 _TILE_2
+#define _TILE_0 '*'
+#define _TILE_1 '*'
 
-#define _TILE_6  'H'
-#define _TILE_9  'L'
+#define _TILE_2 '-'
+
+#define _TILE_3 '-'
+
+// Apple
+#define _TILE_4 'O'
+
+// Body
+#define _TILE_5 'O'
+
+#define _TILE_6 'H'
+
+#define _TILE_7 '='
+
+#define _TILE_8 'O'
+
+#define _TILE_9 'L'
+
+#define _TILE_10 '='
+
+#define _TILE_11 '|'
+#define _TILE_12 '|'
+
+#define _TILE_13 '-'
+
 #define _TILE_14 'S'
+
+#define _TILE_15 '|'
+
 #define _TILE_16 'C'
 
-#define _TILE_12 'I'
-
-#define _TILE_10 'O'
-
-
-#define _TILE_13 _TILE_10
+#define _TILE_17 'X'
 
 #define _TILE_18 '~'
-
-#define _TILE_8 '='
 
 #endif // _CHAR_TILES_H
 

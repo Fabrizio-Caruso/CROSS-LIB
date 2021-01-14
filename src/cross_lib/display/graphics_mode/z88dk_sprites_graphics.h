86e1882c4dbf4fb37613e5858a977de4af159afb
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-01-14 10:50:56 +0100

    NO_TRANSPARENCY

--- src/cross_lib/display/graphics_mode/z88dk_sprites_graphics.h
+++ src/cross_lib/display/graphics_mode/z88dk_sprites_graphics.h
@@ -10,20 +10,28 @@ extern uint8_t sprites[];
 
 #include "display_target_geometry.h"
 
+#define BLIT_OPERATION spr_or
+
+#define __DELETE(x,y) \
+    putsprite(spr_and,(x)*(SPRITE_X_STEP),(y)*(SPRITE_Y_STEP),sprites + ((_SPACE)*(2+SPRITE_Y_SIZE)))
+
 #if defined(NO_TRANSPARENCY)
-    #define BLIT_OPERATION 0
+    #define _XLIB_DRAW_TILE(x,y,tile,color) \
+        do \
+        { \
+            __DELETE(x,y); \
+            putsprite(BLIT_OPERATION,(x)*(SPRITE_X_STEP),(y)*(SPRITE_Y_STEP),sprites + ((tile)*(2+SPRITE_Y_SIZE))); \
+        } while(0)
 #else
-    #define BLIT_OPERATION spr_or
+    #define _XLIB_DRAW_TILE(x,y,tile,color) \
+        putsprite(BLIT_OPERATION,(x)*(SPRITE_X_STEP),(y)*(SPRITE_Y_STEP),sprites + ((tile)*(2+SPRITE_Y_SIZE)))
 #endif
 
-#define _XLIB_DRAW_TILE(x,y,tile,color) \
-	putsprite(BLIT_OPERATION,(x)*(SPRITE_X_STEP),(y)*(SPRITE_Y_STEP),sprites + ((tile)*(2+SPRITE_Y_SIZE)))
 
 #define __DRAW(x,y,image) \
 	putsprite(BLIT_OPERATION,(x)*(SPRITE_X_STEP),(y)*(SPRITE_Y_STEP),sprites + ((image)->_imageData*(2+SPRITE_Y_SIZE)))
 
-#define __DELETE(x,y) \
-	putsprite(spr_and,(x)*(SPRITE_X_STEP),(y)*(SPRITE_Y_STEP),sprites + ((_SPACE)*(2+SPRITE_Y_SIZE)))
+
 
 #endif // __Z88DK_SPRITES_GRAPHICS
 

4498c5980f631525e2852f7aa4f0747ca58bf553
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-01-14 22:20:16 +0100

    Fix Supervision text in XShoot

--- src/games/shoot/text_strings.h
+++ src/games/shoot/text_strings.h
@@ -42,7 +42,7 @@
     #define DESTROY_MISSILES_STRING "destroy" _XL_SPACE "the" _XL_SPACE "missiles"
     #define LURE_THE_ENEMIES_STRING "kill" _XL_SPACE "all" _XL_SPACE "enemies"
     #define INTO_THE_MINES_STRING ""
-    #define SCORE_STRING "score" _XL_SPACE
+    #define SCORE_STRING _XL_s _XL_c _XL_o _XL_r _XL_e _XL_SPACE _XL_SPACE
     #define LEVEL_STRING "level"
     #define GAME_COMPLETED_STRING "game" _XL_SPACE "completed"
     #define SECRET_LEVEL_FOUND_STRING "secret" _XL_SPACE "level" _XL_SPACE "found"
@@ -63,7 +63,7 @@
     #define DESTROY_MISSILES_STRING "DESTROY THE MISSILES"    
     #define LURE_THE_ENEMIES_STRING "KILL ALL ENEMIES"
     #define INTO_THE_MINES_STRING     ""
-    #define SCORE_STRING "SCORE "
+    #define SCORE_STRING _XL_S _XL_C _XL_O _XL_R _XL_E _XL_SPACE
     #define LEVEL_STRING "LEVEL"
     #define GAME_COMPLETED_STRING "GAME COMPLETED"
     #define SECRET_LEVEL_FOUND_STRING "SECRET LEVEL FOUND"
@@ -83,29 +83,29 @@
     #else
         #define CROSS_SHOOT_STRING "C R O S S  S H O O T"        
         #define CROSS_SHOOT_SHORT_STRING "CROSS SHOOT"
-        #define AUTHOR_STRING "by Fabrizio Caruso"
+        #define AUTHOR_STRING _XL_b _XL_y _XL_SPACE _XL_F _XL_a _XL_b _XL_r _XL_i _XL_z _XL_i _XL_o _XL_SPACE _XL_C _XL_a _XL_r _XL_u _XL_s _XL_o
     #endif
-    #define KILL_THE_SKULLS_STRING "Kill the skulls"
-    #define KILL_THE_BOSS "Kill the boss"
-    #define DESTROY_MISSILES_STRING "Destroy the missiles"    
-    #define LURE_THE_ENEMIES_STRING "Kill all enemies"
+    #define KILL_THE_SKULLS_STRING _XL_K _XL_i _XL_l _XL_l _XL_SPACE _XL_t _XL_h _XL_e _XL_SPACE _XL_s _XL_k _XL_u _XL_l _XL_l _XL_s
+    #define KILL_THE_BOSS _XL_K _XL_i _XL_l _XL_l _XL_SPACE _XL_t _XL_h _XL_e _XL_SPACE _XL_b _XL_o _XL_s _XL_s
+    #define DESTROY_MISSILES_STRING _XL_D _XL_e _XL_s _XL_t _XL_r _XL_o _XL_y _XL_SPACE _XL_t _XL_h _XL_e _XL_SPACE _XL_m _XL_i _XL_s _XL_s _XL_i _XL_l _XL_e _XL_s    
+    #define LURE_THE_ENEMIES_STRING _XL_K _XL_i _XL_l _XL_l _XL_SPACE _XL_a _XL_l _XL_l _XL_SPACE _XL_e _XL_n _XL_e _XL_m _XL_i _XL_e _XL_s
     #define INTO_THE_MINES_STRING ""
-    #define SCORE_STRING "SCORE "
-    #define LEVEL_STRING "LEVEL"    
+    #define SCORE_STRING _XL_S _XL_C _XL_O _XL_R _XL_E _XL_SPACE
+    #define LEVEL_STRING _XL_L _XL_E _XL_V _XL_E _XL_L
     #define GAME_COMPLETED_STRING "GAME COMPLETED"
     #define SECRET_LEVEL_FOUND_STRING "SECRET LEVEL FOUND"
     #define SECRET_FOUND_STRING "SECRET FOUND"
     #define EMPTY_STRING        "            "
     #define START_LEVEL_STRING "LEVEL"    
-    #define USE_THE_GUN_AGAINST_STRING "Shoot at"
-    #define THE_SKULL_AND_STRING "the skulls and"
-    #define MISSILE_BASES_STRING "missile bases"
-    #define FOR_POINTS_AND___STRING "for points"
-    #define EXTRA_POWERUPS__STRING "and items"    
+    #define USE_THE_GUN_AGAINST_STRING _XL_S _XL_o _XL_o _XL_t _XL_SPACE _XL_a _XL_t
+    #define THE_SKULL_AND_STRING _XL_t _XL_h _XL_e _XL_SPACE _XL_s _XL_k _XL_u _XL_l _XL_l _XL_s _XL_SPACE _XL_a _XL_n _XL_d
+    #define MISSILE_BASES_STRING _XL_m _XL_i _XL_s _XL_s _XL_i _XL_l _XL_e _XL_SPACE _XL_b _XL_a _XL_s _XL_e _XL_s
+    #define FOR_POINTS_AND___STRING _XL_f _XL_o _XL_r _XL_SPACE _XL_p _XL_o _XL_i _XL_n _XL_t _XL_s
+    #define EXTRA_POWERUPS__STRING _XL_a _XL_n _XL_d _XL_SPACE _XL_i _XL_t _XL_e _XL_m _XL_s    
 #endif
 
 #if defined(JOYSTICK_CONTROL) || defined(__MSX__) || defined(__CPC_JOYSTICK__)
-    #define USE_STRING "use" _XL_SPACE "the" _XL_SPACE "joystick"
+    #define USE_STRING _XL_U _XL_s _XL_e _XL_SPACE _XL_t _XL_h _XL_e _XL_SPACE _XL_j _XL_o _XL_y _XL_s _XL_t _XL_i _XL_c _XL_k
 #elif !defined(TINY_TEXT)        
     #if !defined(__PC6001__)
         #define USE_STRING "use i j k l space"
@@ -150,7 +150,7 @@
         #define YOU_MADE_IT_STRING "you" _XL_SPACE "made" _XL_SPACE "it"
     #else
         #if defined(JOYSTICK_CONTROL) || defined(__MSX__)
-            #define PRESS_STRING "PRESS FIRE"
+            #define PRESS_STRING _XL_P _XL_R _XL_E _XL_S _XL_S _XL_SPACE _XL_F _XL_I _XL_R _XL_E
         #else
             #define PRESS_STRING "PRESS A KEY"
         #endif
@@ -166,11 +166,11 @@
 
 #if defined(ONLY_SMALL_LETTERS)
     #define BONUS_STRING "bonus "
-    #define SCORE_STRING "score" _XL_SPACE
+    // #define SCORE_STRING _XL_S _XL_C _XL_O _XL_R _XL_E _XL_SPACE
     #define HIGH_SCORE_STRING "hiscore" _XL_SPACE
 #else
-    #define BONUS_STRING "BONUS "
-    #define SCORE_STRING "SCORE "
+    #define BONUS_STRING "BONUS" _XL_SPACE
+    #define SCORE_STRING _XL_S _XL_C _XL_O _XL_R _XL_E _XL_SPACE
     #define HIGH_SCORE_STRING "HISCORE "
 #endif
 

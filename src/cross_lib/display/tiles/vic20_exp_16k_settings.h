92f861466d433427aff8f5cf71ca1dbf3a14d9a6
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-04-29 22:14:24 +0200

    More 8 bytes saved

--- src/cross_lib/display/tiles/vic20_exp_16k_settings.h
+++ src/cross_lib/display/tiles/vic20_exp_16k_settings.h
@@ -1,40 +1,40 @@
 // BLUE
 
 #define _TILE_0 '\''
-#define _TILE_1 (0x70-12-4)
-#define _TILE_2 (0x74-13-4) 
-#define _TILE_3 (0x72-13-4)
+#define _TILE_1 ('>'+1)
+#define _TILE_2 (0x74-13-5) 
+#define _TILE_3 (0x72-13-5)
 
-#define _TILE_9 (0x7B-13-4)
+#define _TILE_9 (0x7B-13-5)
 
 // YELLOW
-#define _TILE_7 (0x77-13-4)
+#define _TILE_7 (0x77-13-5)
 
-#define _TILE_12 ('|'-13-4)
-#define _TILE_13 ('>'+1)
+#define _TILE_12 ('|'-13-5)
+#define _TILE_13 ('>'+5-5)
 #define _TILE_0_B _TILE_0
 #define _DOLLAR_TILE '$'
 
 // GREEN
-#define _TILE_8  (0x7A-13-4)
+#define _TILE_8  (0x7A-13-5)
 
 // RED
-#define _TILE_5 (0x78-13-4)
+#define _TILE_5 (0x78-13-5)
 //0x5E
 
 
 // CYAN
-#define _TILE_10 (0x73-13-4)
-#define _TILE_11 (0x7C-13-4)
+#define _TILE_10 (0x73-13-5)
+#define _TILE_11 (0x7C-13-5)
 
 // WHITE
-#define _TILE_4 (0x76-13-4)
+#define _TILE_4 (0x76-13-5)
 
 // TODO: this has to be different
 #define _TILE_6 (_TILE_16+1)
 
 
-#define _TILE_14 '>'
+#define _TILE_14 ('>'-1)
 
 #define _TILE_15 '<'
 

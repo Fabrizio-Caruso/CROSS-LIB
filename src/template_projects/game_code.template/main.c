5ac95075843dc6f31e54179696448cfafaab2d7b
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-09-13 09:02:22 +0200

    Game template

--- src/template_projects/game_code.template/main.c
+++ src/template_projects/game_code.template/main.c
@@ -31,279 +31,35 @@
 
 #include "images.h"
 
+#define _GAME_NAME__STRING _XL_M _XL_Y _XL_SPACE _XL_G _XL_A _XL_M _XL_E
+#define _BY__STRING _XL_B _XL_Y
+#define _AUTHOR__STRING _XL_A _XL_U _XL_T _XL_H _XL_O _XL_R 
 
-#if !defined(SLOW_DOWN)
-    #define SLOW_DOWN 0
-#endif
-
-#  if SLOW_DOWN<20
-    #define LEVEL_SPEED_UP 0
-#elif SLOW_DOWN<100
-    #define LEVEL_SPEED_UP 2
-#elif SLOW_DOWN<200
-    #define LEVEL_SPEED_UP 4
-#elif SLOW_DOWN<400
-    #define LEVEL_SPEED_UP 8
-#elif SLOW_DOWN<800
-    #define LEVEL_SPEED_UP 16
-#elif SLOW_DOWN<1600
-    #define LEVEL_SPEED_UP 32
-#elif SLOW_DOWN<3200
-    #define LEVEL_SPEED_UP 64
-#else
-    #define LEVEL_SPEED_UP 128
-#endif 
-
-#define MAX_Y ((YSize)+(Y_OFFSET))
-
-#define MIN_BUILDING_HEIGHT 2
-
-
-#if XSize>78
-    #define BUILDINGS_NUMBER (XSize-24)
-    #define FIRST_BULDING_X_POS 12
-#elif XSize>63
-    #define BUILDINGS_NUMBER (XSize-22)
-    #define FIRST_BULDING_X_POS 10
-#elif XSize>48
-    #define BUILDINGS_NUMBER (XSize-14)
-    #define FIRST_BULDING_X_POS 8
-#elif XSize>32
-    #define BUILDINGS_NUMBER (XSize-10)
-    #define FIRST_BULDING_X_POS 5
-#elif XSize>27
-    #define BUILDINGS_NUMBER (XSize-8)
-    #define FIRST_BULDING_X_POS 4
-#else
-    #define BUILDINGS_NUMBER (XSize-7)
-    #define FIRST_BULDING_X_POS 4
-#endif
-
-// #define BUILDINGS_NUMBER 1
-// #define FIRST_BULDING_X_POS 10
-
-
-// String definitions
-#define _CROSS_BOMBER__STRING _XL_C _XL_R _XL_O _XL_S _XL_S _XL_SPACE _XL_B _XL_O _XL_M _XL_B _XL_E _XL_R
-#define _BY_FABRIZIO_CARUSO__STRING _XL_B _XL_Y _XL_SPACE _XL_F _XL_A _XL_B _XL_R _XL_I _XL_Z _XL_I _XL_O _XL_SPACE _XL_C _XL_A _XL_R _XL_U _XL_S _XL_O
-#define _DESTROY_ALL__STRING _XL_D _XL_E _XL_S _XL_T _XL_R _XL_O _XL_Y _XL_SPACE _XL_A _XL_L _XL_L
-#define _BUILDINGS__STRING _XL_B _XL_U _XL_I _XL_L _XL_D _XL_I _XL_N _XL_G _XL_S
 #define _PRESS_FIRE__STRING _XL_P _XL_R _XL_E _XL_S _XL_S _XL_SPACE _XL_F _XL_I _XL_R _XL_E
+
 #define _LEVEL__STRING _XL_L _XL_E _XL_V _XL_E _XL_L
-#define _LEVEL_COMPLETED__STRING _XL_L _XL_E _XL_V _XL_E _XL_L _XL_SPACE _XL_C _XL_O _XL_M _XL_P _XL_L _XL_E _XL_T _XL_E _XL_D
-#define _BONUS__STRING _XL_B _XL_O _XL_N _XL_U _XL_S
+#define _LEVEL_COMPLETED__STRING _LEVEL__STRING _XL_SPACE _XL_C _XL_O _XL_M _XL_P _XL_L _XL_E _XL_T _XL_E _XL_D
+
+#define _LIVES__STRING _XL_L _XL_I _XL_V _XL_E _XL_S
+
+#define _YOU_DIED__STRING _XL_Y _XL_O _XL_U _XL_SPACE _XL_D _XL_I _XL_E _XL_D
+
 #define _GAME_COMPLETED__STRING _XL_G _XL_A _XL_M _XL_E _XL_SPACE _XL_C _XL_O _XL_M _XL_P _XL_E _XL_T _XL_E _XL_D
-#define _NEW_HISCORE__STRING _XL_N _XL_E _XL_W _XL_SPACE _XL_H _XL_I _XL_S _XL_C _XL_O _XL_R _XL_E
 #define _GAME_OVER__STRING _XL_G _XL_A _XL_M _XL_E _XL_SPACE _XL_O _XL_V _XL_E _XL_R
-
-
-#define drawPlane() \
-do { \
-    _XL_DRAW(x,y,PLANE_BACK_TILE, _PLANE_COLOR); \
-    _XL_DRAW(x+1,y,PLANE_FRONT_TILE, _PLANE_COLOR); \
-} while(0)
-
-
-
-#define drawPlaneBack() \
-    _XL_DRAW(x,y,PLANE_BACK_TILE,_PLANE_COLOR);
-
-#define deletePlaneBack() \
-    _XL_DELETE(x,y);
-
-#define deletePlaneFront() \
-    _XL_DELETE(x+1,y);
-
-
-
-#define deleteAnimatedPlaneBack() \
-    _XL_DELETE(x-1,y); 
-
-#define deleteAnimatedPlaneCenter() \
-    deletePlaneBack(); 
-
-
-#define deleteAnimatedBombUp() \
-    _XL_DELETE(bomb_x,bomb_y-1);
-
-#if !defined(NO_ANIMATION)
-    #define drawAnimatedPlane() \
-        do { \
-            _XL_DRAW(x-1,y,ANIMATED_PLANE_BACK_TILE,_PLANE_COLOR); \
-            _XL_DRAW(x,y,ANIMATED_PLANE_CENTER_TILE,_PLANE_COLOR); \
-            _XL_DRAW(x+1,y,ANIMATED_PLANE_FRONT_TILE, _PLANE_COLOR); \
-        } while(0)
-
-
-        
-    #define drawAnimatedBomb() \
-    do { \
-        _XL_DRAW(bomb_x,bomb_y,ANIMATED_BOMB_UP_TILE, _BOMB_COLOR); \
-        _XL_DRAW(bomb_x,bomb_y+1,ANIMATED_BOMB_DOWN_TILE,_BOMB_COLOR); \
-    } while(0)
-
-    #define deletePlane() \
-    do  {\
-        deletePlaneBack() ; \
-        deletePlaneFront() ; \
-    } while(0)  
-
-    #define deleteAnimatedBomb() \
-    do { \
-        _XL_DELETE(bomb_x,bomb_y-1); \
-        _XL_DELETE(bomb_x,bomb_y); \
-    } while(0)
-        
-#else 
-    #define drawAnimatedPlane() 
-
-        
-    #define drawAnimatedBomb() 
-
-    #define deletePlane() \
-        deletePlaneFront(); 
-        
-    #define deleteAnimatedBomb() \
-        _XL_DELETE(bomb_x,bomb_y-1);
-    
-#endif
-
-#define drawRoad() \
-    _XL_DRAW(x,MAX_Y-1,ROAD_TILE,_ROAD_COLOR);
-
-#define drawBuilding() \
-    _XL_DRAW(x,MAX_Y-1-y,buildingType,buildingColor);
-
-#define drawBomb() \
-    _XL_DRAW(bomb_x,bomb_y,BOMB_TILE,_BOMB_COLOR);
-
-
-#define drawExplosion() \
-    _XL_DRAW(bomb_x,bomb_y,EXPLOSION_TILE,_XL_RED);
-
-#define deleteExplosion() \
-    _XL_DELETE(bomb_x,bomb_y);
-
-#define displayScore() \
-do { \
-    _XL_SET_TEXT_COLOR(_XL_WHITE); \
-    _XL_PRINTD(2,0,5,score); \
-} while(0)
-
-#define displayRemainingBuilings() \
-do { \
-    _XL_SET_TEXT_COLOR(_XL_WHITE); \
-    _XL_PRINTD(9,0,2,remaining_buildings); \
-} while(0)
-
-#define displayHiScore() \
-do { \
-    _XL_SET_TEXT_COLOR(_XL_WHITE); \
-    _XL_PRINTD(XSize-5,0,5,hiscore); \
-} while(0)
-
-
-#define displayLevel() \
-do { \
-    _XL_SET_TEXT_COLOR(_XL_WHITE); \
-    _XL_PRINTD(XSize-8,0,1,level); \
-} while(0)
-
-#define displayLevelMessage() \
-do { \
-    _XL_SET_TEXT_COLOR(_XL_WHITE); \
-    _XL_PRINTD(XSize/2+2,4,2,level); \
-} while(0)
-
-#define displayNewHiScoreMessage() \
-do { \
-    _XL_SET_TEXT_COLOR(_XL_CYAN); \
-    _XL_PRINT(1,4,_NEW_HISCORE__STRING); \
-} while(0)
-
-#define displayGameOverMessage() \
-do { \
-    _XL_SET_TEXT_COLOR(_XL_RED); \
-    _XL_PRINT_CENTERED(_GAME_OVER__STRING); \
-} while(0)
-
-#define handle_hiscore() \
-do { \
-    if(score>hiscore) \
-    { \
-        hiscore = score; \
-        displayNewHiScoreMessage(); \
-        _XL_SLEEP(1); \
-    } \
-} while(0)
-
-#if MAX_Y<24
-    #define LEVEL_FACTOR_SPEED_UP 3
-#else
-    #define LEVEL_FACTOR_SPEED_UP 2
-#endif 
-
 
 #define INITIAL_LEVEL 1
 #define FINAL_LEVEL 8
 
-uint16_t building_height[XSize];
-
-
-uint8_t x;
-uint8_t y;
-
-uint8_t bombActive;
-uint8_t bomb_x;
-uint8_t bomb_y;
-
-uint8_t level;
-
-uint16_t score;
-uint16_t bonus;
-uint16_t bonus_ind;
 uint16_t hiscore;
-
-uint8_t remaining_buildings;
-
+uint16_t score;
+uint8_t level;
 uint8_t alive;
-uint8_t explosion;
-
-
-const uint8_t building_tiles[] = {
-    WALL_1_TILE, WALL_2_TILE, 
-    TWO_WINDOW_WALL_1_TILE, TWO_WINDOW_WALL_2_TILE, 
-    THREE_WINDOW_WALL_1_TILE, THREE_WINDOW_WALL_2_TILE, 
-    SMALL_TWO_WINDOW_WALL_1_TILE, SMALL_TWO_WINDOW_WALL_2_TILE
-    };
-
-const uint8_t building_colors[] = {
-    _WALL_1_COLOR, _WALL_2_COLOR, 
-    _TWO_WINDOW_WALL_1_COLOR, _TWO_WINDOW_WALL_2_COLOR, 
-    _THREE_WINDOW_WALL_1_COLOR, _THREE_WINDOW_WALL_2_COLOR, 
-    _SMALL_TWO_WINDOW_WALL_1_COLOR, _SMALL_TWO_WINDOW_WALL_2_COLOR
-    };
-
-#if defined(DEBUG)
-const uint8_t tiles[] = {
-		_TILE_0, _TILE_1, _TILE_2, _TILE_3, 
-		_TILE_4, _TILE_5, _TILE_6, _TILE_7, 
-		_TILE_8, _TILE_9, _TILE_10, _TILE_11, 
-		_TILE_12, _TILE_13, _TILE_14, _TILE_15, 
-		_TILE_16, _TILE_17, _TILE_18, _TILE_19,
-		_TILE_20, _TILE_21, _TILE_22, _TILE_23, 
-        _TILE_24, _TILE_25
-		};
-
-const static uint8_t tile_color[] = {_XL_WHITE, _XL_RED, _XL_CYAN, _XL_GREEN, _XL_YELLOW, _XL_BLUE};
-#endif
+uint8_t lives;
+uint8_t input;
+uint8_t level_completed;
 
 int main(void)
 {        
-
-    uint8_t buildingType;
-    uint8_t buildingColor;
-    uint8_t rnd;
 
     _XL_INIT_GRAPHICS();
 
@@ -313,267 +69,89 @@ int main(void)
     
     hiscore = 0;
 
-    #if defined(DEBUG)
-
-        #define COL_OFFSET ((XSize-16)/2-1)
-        #define ROW_OFFSET 3
-        
-        
-        for(buildingColor=0;buildingColor<6;++buildingColor)
-        {
-            _XL_CLEAR_SCREEN();
-
-            for(rnd=0;rnd<26;++rnd)
-            {
-                _XL_DRAW((rnd&7)*2+COL_OFFSET,(rnd/8)*2+ROW_OFFSET,tiles[rnd],tile_color[buildingColor]);
-                _XL_SLOW_DOWN(300);
-            }        
-        }
-        
-        
-    
-    #endif
 
     while(1)
     {
         alive = 1;
         score = 0;
         level = INITIAL_LEVEL;
-        explosion = 0;
+        lives = 3;
 
         _XL_CLEAR_SCREEN();
             
         _XL_SET_TEXT_COLOR(_XL_RED);
-        _XL_PRINT_CENTERED_ON_ROW(2, _CROSS_BOMBER__STRING);
+        _XL_PRINT_CENTERED_ON_ROW(2, _GAME_NAME__STRING);
         _XL_SET_TEXT_COLOR(_XL_CYAN);
-        _XL_PRINT_CENTERED_ON_ROW(4, _BY_FABRIZIO_CARUSO__STRING);
-        _XL_SET_TEXT_COLOR(_XL_YELLOW);
-        _XL_PRINT_CENTERED_ON_ROW(8, _DESTROY_ALL__STRING);
-        _XL_PRINT_CENTERED_ON_ROW(9, _BUILDINGS__STRING);
+        _XL_PRINT_CENTERED_ON_ROW(4, _BY__STRING _XL_SPACE _AUTHOR__STRING);
         _XL_SET_TEXT_COLOR(_XL_WHITE);
         _XL_PRINT_CENTERED_ON_ROW(14, _PRESS_FIRE__STRING);
         _XL_WAIT_FOR_INPUT();
-        while(alive && (level < FINAL_LEVEL+1))
+        
+        _XL_CLEAR_SCREEN();
+        while(lives && (level<FINAL_LEVEL+1))
         {
-            bombActive = 0;
-            bomb_x = 0;
-            bomb_y = MAX_Y-2;
-            bonus = 0;
-            remaining_buildings = BUILDINGS_NUMBER;
-            
+            // (RE-)START LEVEL
+            level_completed = 0;
             _XL_CLEAR_SCREEN();
-            _XL_PRINT(XSize/2-4, 4, _LEVEL__STRING);
-            displayLevelMessage();
-            _XL_SLEEP(1);
+            
+            _XL_PRINT(0,0,_LEVEL__STRING);
+            _XL_PRINTD(7,0,1,level);
+            
+            _XL_PRINT(0,2,_LIVES__STRING);
+            _XL_PRINTD(7,2,1,lives);       
+            
             _XL_WAIT_FOR_INPUT();
-            _XL_CLEAR_SCREEN();
             
-            for(x=0;x<XSize;++x)
+            // LEVEL LOOP
+            while(!level_completed && alive)
             {
-                drawRoad();
-            }
-            for(x=0;x<XSize-2;++x)
-            {
-                building_height[x] = 0;
-            }
-            for(x=FIRST_BULDING_X_POS;x<FIRST_BULDING_X_POS+BUILDINGS_NUMBER;++x)
-            {
-                building_height[x] = (uint8_t) MIN_BUILDING_HEIGHT+level/LEVEL_FACTOR_SPEED_UP+(_XL_RAND()&7);
-                rnd = ((uint8_t) _XL_RAND())&7;
-                buildingType=building_tiles[rnd];
-                buildingColor=building_colors[rnd];
-                
-                for(y=1;y<building_height[x];++y)
-                {
-                    drawBuilding();     
-                }
-                _XL_PING_SOUND();
-            }
+                uint8_t counter;
 
-            _XL_SLEEP(1);
-            y = 2;
-            x = 1;
-            
-            displayScore();
-            
-            
-            _XL_DRAW(0,0,SCORE_TEXT_LEFT_TILE, _XL_CYAN);
-            _XL_DRAW(1,0,SCORE_TEXT_RIGHT_TILE, _XL_CYAN);
-            
-            _XL_DRAW(XSize-6,0,HI_TEXT_TILE, _XL_RED);
-            #if XSize>=20
-                _XL_DRAW(XSize-9,0,LV_TEXT_TILE, _XL_GREEN);
-                displayLevel();
-            #endif
-            #if XSize>=20
-                _XL_DRAW(8,0,TWO_WINDOW_WALL_2_TILE, _XL_YELLOW);
-                displayRemainingBuilings();
-            #endif
-            
-            
-            displayHiScore();
-            while((y<MAX_Y-building_height[x+1]) && (y<MAX_Y-2 || x<XSize-3))
-            {
-                // Land safely
-                if(!remaining_buildings && (y<MAX_Y-2) && (x<XSize-3) )
-                {
-                    deleteAnimatedPlaneBack();
-                    deleteAnimatedPlaneCenter();
-                    ++y;
-                }
-                #if defined(DEBUG_GHOST_DISPLAY)
-                    UNSET_DEBUG_BORDER();
-                #endif
-                drawAnimatedPlane();
-                _XL_SLOW_DOWN(SLOW_DOWN/2-level*LEVEL_SPEED_UP);
+                _XL_PRINTD(0,4,3,input);
+                _XL_PRINTD(0,6,3,counter++);
+                input = _XL_INPUT();
+                _XL_PRINTD(0,6,3,counter++);    
                 
-
-                if(!bombActive )
-                {   
-                    if(explosion)
+                if(_XL_DOWN(input))
+                {
+                    alive=0;
+                    _XL_EXPLOSION_SOUND();
+                }
+                else if(_XL_FIRE(input))
+                {
+                    _XL_TOCK_SOUND();
+                    ++score;
+                    if(score>=level*10)
                     {
-                        deleteExplosion();
-                        explosion = 0;
-                    }
-                    
-                    if((y<MAX_Y-2) && _XL_KEY_PRESSED())
-                    {
-                        _XL_SHOOT_SOUND();
-                        ++bombActive;
-                        bomb_x = x;
-                        bomb_y = y;
-                        if(building_height[x])
-                        {
-                            building_height[x] = 0;
-                            score+=10;
-                            --remaining_buildings;
-                            if(!remaining_buildings)
-                            {
-                                bonus = 10*(MAX_Y-y)+level*20;
-                            }
-                            displayScore();
-                        }
+                        level_completed = 1;
                     }
                 }
-                else
-                {
-                    // Draw animated bomb
-                    drawAnimatedBomb();
-                    
-                    ++bomb_y;
-                
-                    if(bomb_y>MAX_Y-3) // Bomb reaches the ground
-                    {
-                        drawBomb();
-                        bombActive = 0;
-                        explosion = 1;
-                        
-                        
-                        #if XSize>=20
-                            displayRemainingBuilings();
-                        #endif
-                        // Delete animated bomb
-                        deleteAnimatedBombUp();
-                        drawExplosion();
-                        _XL_SLOW_DOWN(5+SLOW_DOWN/4);
-                    }
-
-                }
-                _XL_WAIT_VSYNC();
-                #if defined(DEBUG_GHOST_DISPLAY)
-                    SET_DEBUG_BORDER();
-                #endif
-                deleteAnimatedPlaneBack();
-                drawPlane();
-                #if defined(DEBUG_GHOST_DISPLAY)
-                    UNSET_DEBUG_BORDER();
-                #endif
-                
-                #if !defined(NO_ANIMATION)
-                    _XL_SLOW_DOWN(SLOW_DOWN/2-level*LEVEL_SPEED_UP);
-                #endif
-                
-                if(bombActive)
-                {
-                    drawBomb();
-                    deleteAnimatedBombUp();
-                }
-
-                
-                if(x<XSize-3)
-                {
-                    ++x;
-                }
-                else if(y<MAX_Y-2)
-                {
-                    #if defined(NO_ANIMATION) // Required because deletePlane() in this case only deletes the front
-                        deletePlaneBack();
-                    #endif
-                    deletePlane();
-                    x=1;
-                    ++y;
-                }
-                
-                _XL_WAIT_VSYNC();
-                #if defined(DEBUG_GHOST_DISPLAY)
-                    SET_DEBUG_BORDER();
-                #endif
-                deleteAnimatedPlaneBack();
-            } // while flying
-            #if defined(DEBUG_GHOST_DISPLAY)
-                UNSET_DEBUG_BORDER();
-            #endif
-            if(!remaining_buildings)
+            }
+            
+            
+            // ALIVE OR LEVEL_COMPLETED?
+            if(alive)
             {
-                drawPlane();
-                _XL_SET_TEXT_COLOR(_XL_YELLOW);
-                _XL_PRINT(1,2,_LEVEL_COMPLETED__STRING);
-                _XL_SLEEP(1);
                 ++level;
-                score+=bonus;
-                _XL_SET_TEXT_COLOR(_XL_WHITE);
-                _XL_PRINT(1,4,_BONUS__STRING);
-                for(bonus_ind=10;bonus_ind<=bonus;bonus_ind+=10)
-                {
-                    _XL_PRINTD(7,4,5,bonus_ind);
-                    _XL_SHOOT_SOUND();
-                    _XL_SLOW_DOWN(SLOW_DOWN);
-                    _XL_SLOW_DOWN(SLOW_DOWN);
-                }
-                displayScore();
-                _XL_SLEEP(1);
             }
             else
             {
-                drawPlaneBack();
-                _XL_EXPLOSION_SOUND();
-                displayGameOverMessage();
-                _XL_SLEEP(2);
-                alive = 0;
-                handle_hiscore();
+                --lives;
+                _XL_PRINT_CENTERED(_YOU_DIED__STRING);
+                _XL_SLEEP(1);
             }
-            _XL_WAIT_FOR_INPUT();
-        } // while(alive || (level>FINAL})) -> restart level
-        if(alive) // Game completed
+        } // (lives && (level<FINAL_LEVEL+1)) 
+        if(lives) // Game completed
         {
-            _XL_SET_TEXT_COLOR(_XL_YELLOW);
+            // GAME COMPLETED
+            _XL_CLEAR_SCREEN();
             _XL_PRINT_CENTERED(_GAME_COMPLETED__STRING);
             _XL_SLEEP(1);
             _XL_WAIT_FOR_INPUT();
-            for(y=2;y<MAX_Y-1;++y)
-            {
-                for(x=1;x<XSize-2;x+=2)
-                {
-                    drawPlane();
-                }
-            }
-            _XL_SLEEP(1);
-            displayGameOverMessage();
-            _XL_SLEEP(2);
-            handle_hiscore();
-            _XL_SLEEP(1);
-            _XL_WAIT_FOR_INPUT();
         }
+        _XL_CLEAR_SCREEN();
+        _XL_PRINT_CENTERED(_GAME_OVER__STRING);
+        _XL_WAIT_FOR_INPUT();
     } // while(1) -> restart from level 1
 
     return EXIT_SUCCESS;
--- src/template_projects/helloworld_code.template/main.c
+++ src/template_projects/helloworld_code.template/main.c
@@ -31,106 +31,17 @@
 
 #include "images.h"
 
-// TODO: REMOVE THIS
-#define MAX_INIT_Y_POS ((YSize)+(Y_OFFSET)-19)
-
-#if ((YSize)+(Y_OFFSET)-1)>19
-    #define MAX_TILES 19
-#else
-    #define MAX_TILES ((YSize)+(Y_OFFSET)-1)
-#endif
-
-
- 
-const uint8_t tiles[] = {
-		_TILE_0, _TILE_1, _TILE_2, _TILE_3, 
-		_TILE_4, _TILE_5, _TILE_6, _TILE_7, 
-		_TILE_8, _TILE_9, _TILE_10, _TILE_11, 
-		_TILE_12, _TILE_13, _TILE_14, _TILE_15, 
-		_TILE_16, _TILE_17, _TILE_18, _TILE_19,
-		_TILE_20, _TILE_21, _TILE_22, _TILE_23, 
-        _TILE_24, _TILE_25
-		};
-
-#define NUMBER_OF_COLORS 6
-#define MAX_STRING_SIZE 10
-static const uint8_t tile_color[NUMBER_OF_COLORS] = {_XL_WHITE, _XL_RED, _XL_CYAN, _XL_GREEN, _XL_YELLOW, _XL_BLUE};
-
-const char strings[NUMBER_OF_COLORS][MAX_STRING_SIZE] = 
-{
-    _XL_M _XL_i _XL_n _XL_e,
-    _XL_E _XL_n _XL_e _XL_r _XL_g _XL_y,
-    _XL_B _XL_o _XL_n _XL_u _XL_s,
-    _XL_P _XL_o _XL_i _XL_n _XL_t _XL_s,
-    _XL_A _XL_p _XL_p _XL_l _XL_e,
-    _XL_A _XL_p _XL_p _XL_l _XL_e,
-};
-
-const char color_name[NUMBER_OF_COLORS][MAX_STRING_SIZE] = { 
-                                _XL_W _XL_H _XL_I _XL_T _XL_E, 
-                                _XL_R _XL_E _XL_D, 
-                                _XL_C _XL_Y _XL_A _XL_N, 
-                                _XL_G _XL_R _XL_E _XL_E _XL_N, 
-                                _XL_Y _XL_E _XL_L _XL_L _XL_O _XL_W, 
-                                _XL_B _XL_L _XL_U _XL_E, 
-                                };
-
-
-#define COL_OFFSET ((XSize-16)/2-1)
-#define ROW_OFFSET 3
-
 
 int main(void)
 {        
 
-    uint8_t i;
-    uint8_t j;
-    uint8_t k;
     _XL_INIT_GRAPHICS();
 
     _XL_INIT_INPUT();
 
+    _XL_CLEAR_SCREEN();
 
-    for(k=0;k<3;++k)
-    {
-        for(j=0;j<6;++j)
-        {
-            _XL_CLEAR_SCREEN();
-            
-            _XL_SET_TEXT_COLOR(_XL_WHITE);
-
-            
-            _XL_PRINT(COL_OFFSET,   4,_XL_P _XL_R _XL_I _XL_N _XL_T _XL_D);
-            _XL_PRINTD(COL_OFFSET  ,5,5, 1234U);
-            _XL_PRINTD(COL_OFFSET+5,5,5,56789U);
-
-            _XL_SET_TEXT_COLOR(tile_color[j]);
-            
-            _XL_PRINT(COL_OFFSET, 7, (char *) color_name[j]);
-            
-            _XL_PRINT(COL_OFFSET, 9, _XL_a _XL_b _XL_c _XL_d _XL_e _XL_f _XL_g _XL_h _XL_i _XL_j);
-            _XL_PRINT(COL_OFFSET,10, _XL_k _XL_l _XL_m _XL_n _XL_o _XL_p _XL_q _XL_r _XL_s _XL_t);
-            _XL_PRINT(COL_OFFSET,11, _XL_u _XL_v _XL_w _XL_x _XL_y _XL_z);
-            _XL_PRINT(COL_OFFSET,13, _XL_A _XL_B _XL_C _XL_D _XL_E _XL_F _XL_G _XL_H _XL_I _XL_J);
-            _XL_PRINT(COL_OFFSET,14, _XL_K _XL_L _XL_M _XL_N _XL_O _XL_P _XL_Q _XL_R _XL_S _XL_T);
-            _XL_PRINT(COL_OFFSET,15, _XL_U _XL_V _XL_W _XL_X _XL_Y _XL_Z);
-            
-            _XL_WAIT_FOR_INPUT();
-
-            _XL_CLEAR_SCREEN();
-
-            for(i=0;i<NUM_OF_TILES;++i)
-            {
-                _XL_DRAW((i&7)*2+COL_OFFSET,(i/8)*2+ROW_OFFSET,tiles[i],tile_color[j]);
-                _XL_SLOW_DOWN(300);
-            }
-            
-            _XL_SET_TEXT_COLOR(_XL_WHITE);
-            _XL_PRINT(COL_OFFSET,YSize-5, _XL_P _XL_R _XL_E _XL_S _XL_S _XL_SPACE _XL_F _XL_I _XL_R _XL_E);
-            _XL_WAIT_FOR_INPUT();
-        }
-    }
-    _XL_PRINT(COL_OFFSET,YSize-5, _XL_E _XL_N _XL_D _XL_SPACE _XL_O _XL_F _XL_SPACE _XL_D _XL_E _XL_M _XL_O);
+    _XL_PRINT_CENTERED(_XL_H _XL_E _XL_L _XL_L _XL_O _XL_SPACE _XL_W _XL_O _XL_R _XL_L _XL_D);
 
     while(1){};
     

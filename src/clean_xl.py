1880f85b6d1089d239fe175b9cf16b223a92ea56
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-09-22 19:43:59 +0200

    clean_xl script

---
+++ src/clean_xl.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+import shutil
+
+import os,sys
+
+
+if len(sys.argv)<2:
+    game_dir = "helloworld"
+else:
+    game_dir = sys.argv[1]
+
+if(game_dir in ["tiles","sounds","matrix","invaders","zombies"]):
+    project_type = "demo"
+else:
+    project_type = "game" 
+
+parent_dir = project_type + "s"
+
+print("Project name: " + game_dir)
+
+
+parent_and_game_dir = parent_dir + "/" + game_dir
+
+if not os.path.exists(parent_and_game_dir):
+    print("Project not found!")
+    exit();
+
+make_command = "make clean -f " + parent_dir+"/"+game_dir+"/Makefile."+game_dir;
+
+print("run command : " + make_command)
+print("\n")
+
+os.system(make_command)

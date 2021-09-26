de397bc7c18fc7ca164395d83fe60931408f2a3c
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-09-26 23:34:19 +0200

    Experiments with bg color

--- src/debug/helloworld_background_color.c
+++ src/debug/helloworld_background_color.c
@@ -1,9 +1,16 @@
 #include<conio.h>
+#include <sys/ioctl.h>
 
 int main()
 {
-    bgcolor(COLOR_BLACK);
-    textcolor(COLOR_BLACK);
+    int mode = 0;
+    console_ioctl(IOCTL_GENCON_SET_MODE, &mode);
+
+    textcolor(WHITE);    
+    bordercolor(YELLOW);
+    textbackground(RED); \
+    clrscr();
+
     gotoxy(2,2);
     cprintf("hello world\n");
     while(1){};

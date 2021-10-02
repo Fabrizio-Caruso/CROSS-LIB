69b760db9ba0bc5e5eaa224d9e7f9e51f6e31f81
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-10-02 15:09:16 +0200

    Remove border hack for mo5

--- src/cross_lib/sound/generic/dac_sounds.c
+++ src/cross_lib/sound/generic/dac_sounds.c
@@ -38,11 +38,11 @@ void _XL_INIT_SOUND(void)
         andcc #$af
     }
     // TODO: Remove this HACK to reset border color by fixing the Assembly code above
-    #if defined(_BACKGROUND_COLOR) && _BACKGROUND_COLOR==_XL_WHITE
-        PUTCH(0x1B);PUTCH(103);
-    #else
-        PUTCH(0x1B);PUTCH(96);
-    #endif
+    // #if defined(_BACKGROUND_COLOR) && _BACKGROUND_COLOR==_XL_WHITE
+        // PUTCH(0x1B);PUTCH(103);
+    // #else
+        // PUTCH(0x1B);PUTCH(96);
+    // #endif
 }
 
 #elif defined(__TO7__)

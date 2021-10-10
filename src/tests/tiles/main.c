7bf61e823dc66bd2290a50adf1e6905290eebe28
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-10-10 23:11:25 +0200

    More tests use API_VERSION 2

--- src/tests/tiles/main.c
+++ src/tests/tiles/main.c
@@ -31,21 +31,6 @@
 
 #include "images.h"
 
-// TODO: REMOVE THIS
-#define MAX_INIT_Y_POS ((YSize)+(Y_OFFSET)-19)
-
-#if ((YSize)+(Y_OFFSET)-1)>19
-    #define MAX_TILES 19
-#else
-    #define MAX_TILES ((YSize)+(Y_OFFSET)-1)
-#endif
-
-
-
-// Global initialization
-//<global>
-
-      
 const uint8_t tiles[] = {
 		_TILE_0, _TILE_1, _TILE_2, _TILE_3, 
 		_TILE_4, _TILE_5, _TILE_6, _TILE_7, 
@@ -62,7 +47,6 @@ const uint8_t tiles[] = {
 static const uint8_t tile_color[NUMBER_OF_COLORS] = {_XL_WHITE, _XL_RED, _XL_CYAN, _XL_GREEN, _XL_YELLOW, _XL_BLUE};
 #endif
 
-// const char strings[NUMBER_OF_STRINGS][MAX_STRING_SIZE] = 
 const char strings[NUMBER_OF_COLORS][MAX_STRING_SIZE] = 
 {
     _XL_M _XL_i _XL_n _XL_e,
@@ -81,8 +65,6 @@ const char color_name[NUMBER_OF_COLORS][MAX_STRING_SIZE] = {
                                 _XL_Y _XL_E _XL_L _XL_L _XL_O _XL_W, 
                                 _XL_B _XL_L _XL_U _XL_E, 
                                 };
-
-//</global>
 
 
 #define COL_OFFSET ((XSize-16)/2-1)
@@ -110,17 +92,12 @@ int main(void)
             _XL_CLEAR_SCREEN();
             
             _XL_SET_TEXT_COLOR(tile_color[j]);
-            // _XL_PRINT(COL_OFFSET, 1, 32+64);
-            //_XL_PRINT(COL_OFFSET,   1,_XL_P _XL_R _XL_I _XL_N _XL_T);
-            // _XL_PRINT(COL_OFFSET,   2,"0123456789");
-            
+
             _XL_PRINT(XSize-12,0,"01234567890");
             
-            _XL_PRINT(COL_OFFSET,   1,_XL_P _XL_R _XL_I _XL_N _XL_T _XL_D);
+            _XL_PRINT(COL_OFFSET,   1,"PRINTD");
             _XL_PRINTD(COL_OFFSET  ,2,5, 1234U);
             _XL_PRINTD(COL_OFFSET+5,2,5,56789U);
-
-            // _XL_SET_TEXT_COLOR(tile_color[j]);
             
             _XL_PRINT(COL_OFFSET, 4, (char *) color_name[j]);
             
@@ -131,12 +108,8 @@ int main(void)
             _XL_PRINT(COL_OFFSET,11, _XL_K _XL_L _XL_M _XL_N _XL_O _XL_P _XL_Q _XL_R _XL_S _XL_T);
             _XL_PRINT(COL_OFFSET,12, _XL_U _XL_V _XL_W _XL_X _XL_Y _XL_Z);
 
-
-
-            #if defined(_API_VERSION) && _API_VERSION>=2
             _XL_PRINT(COL_OFFSET,14, " ABCDEFG HIJKLM");
             _XL_PRINT(COL_OFFSET,15, " NOPQRST UVWXYZ");
-            #endif
             
             _XL_WAIT_FOR_INPUT();
 
@@ -149,11 +122,11 @@ int main(void)
             }
             
             _XL_SET_TEXT_COLOR(_XL_WHITE);
-            _XL_PRINT(COL_OFFSET,YSize-5, _XL_P _XL_R _XL_E _XL_S _XL_S _XL_SPACE _XL_F _XL_I _XL_R _XL_E);
+            _XL_PRINT(COL_OFFSET,YSize-5, "PRESS FIRE");
             _XL_WAIT_FOR_INPUT();
         }
     }
-    _XL_PRINT(COL_OFFSET,YSize-5, _XL_E _XL_N _XL_D _XL_SPACE _XL_O _XL_F _XL_SPACE _XL_D _XL_E _XL_M _XL_O);
+    _XL_PRINT(COL_OFFSET,YSize-5, "END OF DEMO");
 
     while(1){};
     

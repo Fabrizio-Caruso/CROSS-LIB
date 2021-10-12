6285c11681c6b8631a44542e1945fdfea191be1a
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-10-12 20:54:46 +0200

    Examples instead of tests

---
+++ src/examples/helloworld/main.c
@@ -0,0 +1,52 @@
+/* --------------------------------------------------------------------------------------- */ 
+// 
+// CROSS SHOOT by Fabrizio Caruso
+//
+// Fabrizio_Caruso@hotmail.com
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from
+// the use of this software.
+
+// Permission is granted to anyone to use this software for non-commercial applications, 
+// subject to the following restrictions:
+
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software in
+// a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+
+// 2. Altered source versions must be plainly marked as such, and must not
+// be misrepresented as being the original software.
+
+// 3. This notice may not be removed or altered from any source distribution.
+/* --------------------------------------------------------------------------------------- */ 
+
+
+#if !defined EXIT_SUCCESS
+    #define EXIT_SUCCESS 0
+#endif
+
+#include "cross_lib.h"
+
+#include "images.h"
+
+
+int main(void)
+{        
+
+    _XL_INIT_GRAPHICS();
+
+    _XL_INIT_INPUT();
+
+    _XL_CLEAR_SCREEN();
+
+    _XL_SET_TEXT_COLOR(_XL_WHITE);
+
+    _XL_PRINT_CENTERED("HELLO WORLD");
+
+    while(1){};
+    
+    return EXIT_SUCCESS;
+}
+

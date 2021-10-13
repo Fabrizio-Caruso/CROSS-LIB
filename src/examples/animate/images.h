5eb8fbdb6df8e5fd84c4c1fef1e5a82488a6637c
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-10-14 01:47:31 +0200

    Animate example

---
+++ src/examples/animate/images.h
@@ -0,0 +1,52 @@
+#ifndef _IMAGES_H
+#define _IMAGES_H
+
+
+#include "cross_lib.h"
+
+
+    #define PLAYER_DOWN_TILE _TILE_0
+    #define PLAYER_UP_TILE _TILE_1
+    #define PLAYER_RIGHT_TILE _TILE_2
+    #define PLAYER_LEFT_TILE _TILE_0
+
+    #define GHOST_TILE _TILE_4
+    #define BOMB_TILE _TILE_5
+
+    #define DEAD_GHOST_TILE _TILE_6
+    #define SKULL_TILE _TILE_7
+    #define CALM_DOWN_TILE _TILE_8
+    #define FIRE_POWER_TILE _TILE_9
+    #define INVINCIBILITY_TILE _TILE_10
+
+    #define BULLET_TILE _TILE_11
+
+    #define VERTICAL_BRICK_TILE _TILE_12
+    #define HORIZONTAL_BRICK_TILE _TILE_13
+
+
+    #define LEFT_HORIZONTAL_MISSILE_TILE _TILE_14
+    #define RIGHT_HORIZONTAL_MISSILE_TILE _TILE_15
+
+    #define ROCKET_TILE _TILE_16
+
+    #define FREEZE_TILE	 _TILE_17
+    #define SUPER_TILE _TILE_18
+
+    #define EXTRA1_TILE _TILE_19
+    #define EXTRA2_TILE _TILE_20
+    #define EXTRA3_TILE _TILE_21
+    #define EXTRA4_TILE _TILE_22
+    #define EXTRA5_TILE _TILE_23
+    #define EXTRA6_TILE _TILE_24
+    
+    // #define _DOLLAR_TILE _TILE_25
+    
+    #define EXTRA_POINTS_TILE _TILE_25
+
+
+    #define _PLAYER_COLOR _XL_CYAN
+
+
+#endif // _IMAGES_H
+
---
+++ src/examples/animate/main.c
@@ -0,0 +1,105 @@
+/* --------------------------------------------------------------------------------------- */ 
+// 
+// CROSS SHOOT by Fabrizio Caruso
+//
+// Fabrizio_Caruso@hotmail.com
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from
+// the use of this software.
+
+// Permission is granted to anyone to use this software for non-commercial applications, 
+// subject to the following restrictions:
+
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software in
+// a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+
+// 2. Altered source versions must be plainly marked as such, and must not
+// be misrepresented as being the original software.
+
+// 3. This notice may not be removed or altered from any source distribution.
+/* --------------------------------------------------------------------------------------- */ 
+
+
+#if !defined EXIT_SUCCESS
+    #define EXIT_SUCCESS 0
+#endif
+
+#include "cross_lib.h"
+
+#include "images.h"
+
+#define MIN_X 1
+#define MAX_X (XSize-2)
+#define MIN_Y 1
+#define MAX_Y (YSize-2)
+
+int main(void)
+{   
+
+    uint8_t x;
+    uint8_t y;
+    uint8_t input;
+
+    _XL_INIT_GRAPHICS();
+
+    _XL_INIT_INPUT();
+
+    _XL_CLEAR_SCREEN();
+
+    x = XSize/2;
+    y = YSize/2;
+
+    while(1)
+    {
+        _XL_SET_TEXT_COLOR(_XL_WHITE);
+        _XL_PRINTD(0,0,3,x);
+        _XL_PRINTD(5,0,3,y);
+        
+        _XL_DRAW(x,y,_TILE_0,_XL_WHITE);
+        
+        
+        input = _XL_INPUT();
+        if(_XL_UP(input))
+        {
+            _XL_DELETE(x,y);
+            --y;
+        }
+        else if (_XL_DOWN(input))
+        {
+            _XL_DELETE(x,y);
+            ++y;
+        }
+        else if (_XL_LEFT(input))
+        {
+            _XL_DELETE(x,y);
+            --x;
+        }
+        else if(_XL_RIGHT(input))
+        {
+            _XL_DELETE(x,y);
+            ++x;
+        }
+        
+        
+        if((y>MIN_Y)&&(y<MAX_Y)&&(x>MIN_X)&&(x<MAX_X))
+        {
+            _XL_DRAW(x,y,_TILE_0,_XL_WHITE);
+        }
+        else
+        {
+            _XL_DELETE(x,y);
+            x = XSize/2;
+            y = YSize/2;
+        }
+        
+        _XL_SLOW_DOWN(10*_XL_SLOW_DOWN_FACTOR);
+        
+
+    }
+    
+    return EXIT_SUCCESS;
+}
+

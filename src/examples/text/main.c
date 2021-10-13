f55f5f729e56f2aec082df845744d6fe96ed4ff1
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-10-13 21:01:15 +0200

    example characters vs text

--- src/examples/text/main.c
+++ src/examples/text/main.c
@@ -33,26 +33,25 @@
 
 #define NUMBER_OF_COLORS 6
 #define MAX_STRING_SIZE 10
+
 #if !defined(_XL_NO_COLOR)
 static const uint8_t text_color[NUMBER_OF_COLORS] = {_XL_WHITE, _XL_RED, _XL_CYAN, _XL_GREEN, _XL_YELLOW, _XL_BLUE};
 #endif
 
 const char color_name[NUMBER_OF_COLORS][MAX_STRING_SIZE] = { 
-                                _XL_W _XL_H _XL_I _XL_T _XL_E, 
-                                _XL_R _XL_E _XL_D, 
-                                _XL_C _XL_Y _XL_A _XL_N, 
-                                _XL_G _XL_R _XL_E _XL_E _XL_N, 
-                                _XL_Y _XL_E _XL_L _XL_L _XL_O _XL_W, 
-                                _XL_B _XL_L _XL_U _XL_E, 
+                                "WHITE", 
+                                "RED", 
+                                "CYAN", 
+                                "GREEN", 
+                                "YELLOW", 
+                                "BLUE", 
                                 };
 
-
-#define COL_OFFSET ((XSize-16)/2-1)
-#define ROW_OFFSET 3
 
 
 int main(void)
 {        
+    uint8_t i;
     uint8_t j;
     uint8_t k;
     
@@ -70,30 +69,65 @@ int main(void)
             _XL_CLEAR_SCREEN();
             
             _XL_SET_TEXT_COLOR(text_color[j]);
-
-            _XL_PRINT(XSize-12,0,"01234567890");
+            _XL_PRINT(0,0, (char *) color_name[j]);
             
-            _XL_PRINT(COL_OFFSET,   1,"PRINTD");
-            _XL_PRINTD(COL_OFFSET  ,2,5, 1234U);
-            _XL_PRINTD(COL_OFFSET+5,2,5,56789U);
+            #if defined(_XL_NO_TEXT_COLOR)
+                _XL_PRINT(10,0,"NO TEXT COLOR");
+            #else
+                _XL_PRINT(10,0,"TEXT COLOR");
+            #endif
             
-            _XL_PRINT(COL_OFFSET, 4, (char *) color_name[j]);
             
-            _XL_PRINT(COL_OFFSET, 6, _XL_a _XL_b _XL_c _XL_d _XL_e _XL_f _XL_g _XL_h _XL_i _XL_j);
-            _XL_PRINT(COL_OFFSET, 7, _XL_k _XL_l _XL_m _XL_n _XL_o _XL_p _XL_q _XL_r _XL_s _XL_t);
-            _XL_PRINT(COL_OFFSET, 8, _XL_u _XL_v _XL_w _XL_x _XL_y _XL_z);
-            _XL_PRINT(COL_OFFSET,10, _XL_A _XL_B _XL_C _XL_D _XL_E _XL_F _XL_G _XL_H _XL_I _XL_J);
-            _XL_PRINT(COL_OFFSET,11, _XL_K _XL_L _XL_M _XL_N _XL_O _XL_P _XL_Q _XL_R _XL_S _XL_T);
-            _XL_PRINT(COL_OFFSET,12, _XL_U _XL_V _XL_W _XL_X _XL_Y _XL_Z);
-
-            _XL_PRINT(COL_OFFSET,14, " ABCDEFG HIJKLM");
-            _XL_PRINT(COL_OFFSET,15, " NOPQRST UVWXYZ");
+            _XL_PRINT_CENTERED_ON_ROW(5,"SCREEN CENTERED");
             
             _XL_WAIT_FOR_INPUT();
 
+            _XL_PRINT_CENTERED("0123456789");
+            
+            _XL_WAIT_FOR_INPUT();
+            
+            _XL_PRINT_CENTERED("SOME TEXT ");
+            
+            _XL_WAIT_FOR_INPUT();
+            
+            _XL_CLEAR_SCREEN();
+            
+
+            for(i=YSize/6+3;i<YSize-2;i+=3)
+            {
+                
+                _XL_SET_TEXT_COLOR(text_color[j]);
+                _XL_PRINT(0,0, (char *) color_name[j]);
+            
+                _XL_PRINT(0,2,"ON ROW");
+
+                _XL_PRINTD(7,2,2,i);
+                
+                _XL_WAIT_FOR_INPUT();
+
+                _XL_PRINT_CENTERED_ON_ROW(i,"0123456789");
+                
+                _XL_WAIT_FOR_INPUT();
+                
+                _XL_PRINT_CENTERED_ON_ROW(i-1,"ABCDEFGHIJ");
+                _XL_PRINT_CENTERED_ON_ROW(i,  "KLMNOPQRST");
+                _XL_PRINT_CENTERED_ON_ROW(i+1,"UVWXYZ    ");                
+                _XL_WAIT_FOR_INPUT();
+
+                _XL_PRINT_CENTERED_ON_ROW(i-1,_XL_a _XL_b _XL_c _XL_d _XL_e _XL_f _XL_g _XL_h _XL_i _XL_j);
+                _XL_PRINT_CENTERED_ON_ROW(i,  _XL_k _XL_l _XL_m _XL_n _XL_o _XL_p _XL_q _XL_r _XL_s _XL_t);
+                _XL_PRINT_CENTERED_ON_ROW(i+1,_XL_u _XL_v _XL_w _XL_x _XL_y _XL_z _XL_SPACE _XL_SPACE _XL_SPACE _XL_SPACE);                
+                _XL_WAIT_FOR_INPUT();
+                
+                _XL_CLEAR_SCREEN();
+                
+                
+            }            
+            
+
         }
     }
-    _XL_PRINT(COL_OFFSET,YSize-5, "END OF DEMO");
+    _XL_PRINT_CENTERED_ON_ROW(YSize-5, "END OF DEMO");
 
     while(1){};
     

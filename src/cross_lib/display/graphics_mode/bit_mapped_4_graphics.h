a20ae353be6d9c193c43bfd78e74ca8396f5387b
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-11-12 01:41:46 +0100

    Optimize memory for Supervision

--- src/cross_lib/display/graphics_mode/bit_mapped_4_graphics.h
+++ src/cross_lib/display/graphics_mode/bit_mapped_4_graphics.h
@@ -16,30 +16,17 @@ extern uint8_t udgs[];
 uint8_t left_map_one_to_two(uint8_t n);
 uint8_t right_map_one_to_two(uint8_t n);
 
+void supervision_draw(uint8_t x, uint8_t y, uint8_t tile, uint8_t color);
+
+void supervision_delete(uint8_t x, uint8_t y);
+
+
 #define _XL_DRAW(x,y,tile,color) \
-    { \
-        uint8_t k; \
-        uint16_t offset = (8*(uint8_t)(tile)) ; \
-        \
-        for(k=0;k<8;++k) \
-        { \
-            SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)]    = left_map_one_to_two(udgs[offset+k]); \
-            SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)+1]  = right_map_one_to_two(udgs[offset+k]); \
-        } \
-    }
+    supervision_draw(x,y,tile,color)
 
+#define _XL_DELETE(x,y) \
+    supervision_delete(x,y)
 
-#if defined(_XL_NO_COLOR)
-    #define _XL_DELETE(x,y) \
-    { \
-        uint16_t k; \
-        for(k=0;k<8;++k) \
-        { \
-            SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)]=0; \
-            SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)+1]=0; \
-        } \
-    }
-#endif
 
 #endif // _BIT_MAPPED_4_GRAPHICS
 
--- src/cross_lib/display/init_graphics/cc65/supervision/supervision_init_graphics.c
+++ src/cross_lib/display/init_graphics/cc65/supervision/supervision_init_graphics.c
@@ -1,6 +1,32 @@
 #include <supervision.h>
 #include "display_macros.h"
 
+
+void supervision_draw(uint8_t x, uint8_t y, uint8_t tile, uint8_t color)
+    {
+        uint8_t k;
+        uint16_t offset = (8*(uint8_t)(tile)) ;
+        
+        for(k=0;k<8;++k)
+        {
+            SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)]    = left_map_one_to_two(udgs[offset+k]);
+            SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)+1]  = right_map_one_to_two(udgs[offset+k]);
+        }
+    }
+
+
+
+void supervision_delete(uint8_t x, uint8_t y)
+{
+    uint16_t k;
+    for(k=0;k<8;++k)
+    {
+        SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)]=0;
+        SV_VIDEO[2*(x)+BYTES_PER_LINE*k+BYTES_PER_LINE*8*(y)+1]=0;
+    }
+}
+
+
 void _XL_INIT_GRAPHICS(void)
 {
     SV_LCD.height = 160;

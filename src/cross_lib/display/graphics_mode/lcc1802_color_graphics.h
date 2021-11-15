4b05957add6fcad450ca19173e6e42a62b006af8
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-11-15 23:57:58 +0100

    Support for X_OFFSET and Y_OFFSET on lcc1802 graphics

--- src/cross_lib/display/graphics_mode/lcc1802_color_graphics.h
+++ src/cross_lib/display/graphics_mode/lcc1802_color_graphics.h
@@ -3,10 +3,15 @@
 
 #define BASE_ADDR 0xF800
 
-#define _XL_DRAW(x,y,tile,color) vidcharxy(x,y,(tile)+(color))
+#if !defined(_XL_NO_COLOR)
+    #define _XL_DRAW(x,y,tile,color) vidcharxy(((x)+X_OFFSET),((y)+Y_OFFSET),(tile)+(color))
+#else
+    #define _XL_DRAW(x,y,tile,color) vidcharxy(((x)+X_OFFSET),((y)+Y_OFFSET),(tile))
+#endif
 
-#define _XL_DELETE(x,y) vidcharxy(x,y,' ')
+#define _XL_DELETE(x,y) vidcharxy(((x)+X_OFFSET),((y)+Y_OFFSET),' ')
 
+    
 
 #endif // _LCC1802_COLOR_GRAPHICS
 
--- src/cross_lib/display/graphics_mode/lcc1802_graphics.h
+++ src/cross_lib/display/graphics_mode/lcc1802_graphics.h
@@ -3,9 +3,9 @@
 
 #define BASE_ADDR 0xF800
 
-#define _XL_DRAW(x,y,tile,color) vidcharxy(x,y,tile)
+#define _XL_DRAW(x,y,tile,color) vidcharxy(((x)+X_OFFSET),((y)+Y_OFFSET),tile)
 
-#define _XL_DELETE(x,y) vidcharxy(x,y,' ')
+#define _XL_DELETE(x,y) vidcharxy(((x)+X_OFFSET),((y)+Y_OFFSET),' ')
 
 
 #endif // _LCC1802_GRAPHICS

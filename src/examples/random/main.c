3a78ff478af35599141ddac74b05120d5abe2026
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-11-16 19:07:57 +0100

    Improve random example with random colors

--- src/examples/random/main.c
+++ src/examples/random/main.c
@@ -24,35 +24,31 @@
 
 #include "cross_lib.h"
 
+uint8_t colors[] = { _XL_WHITE, _XL_RED, _XL_GREEN, _XL_YELLOW };
+
 int main(void)
 {        
     uint8_t i;
     uint8_t j;
     
     _XL_INIT_GRAPHICS();
-    
     _XL_INIT_INPUT();
-
-    _XL_SET_TEXT_COLOR(_XL_WHITE);
 
     for(j=0;j<9;++j)
     {
-        _XL_CLEAR_SCREEN();
-        
+        _XL_CLEAR_SCREEN();        
         for(i=1;i<YSize-2;i+=2)
         {
+            _XL_SET_TEXT_COLOR(colors[_XL_RAND()&3]);
             _XL_PRINTD(XSize/2-3,i,5,_XL_RAND());
         }
-        
         _XL_WAIT_FOR_INPUT();
     }
     
-    _XL_CLEAR_SCREEN();
-    
+    _XL_CLEAR_SCREEN(); 
     _XL_PRINT_CENTERED("END OF DEMO");
 
     while(1){};
-    
     return EXIT_SUCCESS;
 }
 

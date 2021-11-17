a3348cd269a8cca0e9bc1a9b5a941c144a06c770
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-11-17 02:00:23 +0100

    Improve animate example

--- src/examples/animate/main.c
+++ src/examples/animate/main.c
@@ -30,71 +30,83 @@
 #define MIN_Y 1
 #define MAX_Y (YSize-2)
 
+#define DOWN_TILE  _TILE_0
+#define UP_TILE    _TILE_1 
+#define RIGHT_TILE _TILE_2
+#define LEFT_TILE  _TILE_3 
+#define FIRE_TILE  _TILE_4
+
 int main(void)
 {   
 
     uint8_t x;
     uint8_t y;
     uint8_t input;
+    uint8_t tile;
 
     _XL_INIT_GRAPHICS();
-
+    _XL_INIT_SOUND();
     _XL_INIT_INPUT();
 
     _XL_CLEAR_SCREEN();
 
     x = XSize/2;
     y = YSize/2;
-
+    
+    tile = DOWN_TILE;
+    
     while(1)
     {
         _XL_SET_TEXT_COLOR(_XL_WHITE);
         _XL_PRINTD(0,0,3,x);
         _XL_PRINTD(5,0,3,y);
         
-        _XL_DRAW(x,y,_TILE_0,_XL_WHITE);
-        
-        
         input = _XL_INPUT();
         if(_XL_UP(input))
         {
             _XL_DELETE(x,y);
+            tile = UP_TILE;
             --y;
         }
         else if (_XL_DOWN(input))
         {
             _XL_DELETE(x,y);
+            tile = DOWN_TILE;
             ++y;
         }
         else if (_XL_LEFT(input))
         {
             _XL_DELETE(x,y);
+            tile = LEFT_TILE;
             --x;
         }
         else if(_XL_RIGHT(input))
         {
             _XL_DELETE(x,y);
+            tile = RIGHT_TILE;
             ++x;
         }
         else if(_XL_FIRE(input))
         {
-            _XL_DRAW(x,y,_TILE_1,_XL_WHITE);
-            _XL_SLEEP(1);
+            _XL_DRAW(x,y,FIRE_TILE,_XL_WHITE);
+            _XL_EXPLOSION_SOUND();
+            _XL_SLOW_DOWN(16*_XL_SLOW_DOWN_FACTOR);  
         }
         
-        
-        if((y>MIN_Y)&&(y<MAX_Y)&&(x>MIN_X)&&(x<MAX_X))
+        if((y>=MIN_Y)&&(y<=MAX_Y)&&(x>=MIN_X)&&(x<=MAX_X))
         {
-            _XL_DRAW(x,y,_TILE_0,_XL_WHITE);
+            _XL_DRAW(x,y,tile,_XL_WHITE);
         }
         else
         {
             _XL_DELETE(x,y);
+            _XL_ZAP_SOUND();
+            _XL_SLOW_DOWN(16*_XL_SLOW_DOWN_FACTOR);  
             x = XSize/2;
             y = YSize/2;
         }
         
-        _XL_SLOW_DOWN(8*_XL_SLOW_DOWN_FACTOR);
+        _XL_SLOW_DOWN(4*_XL_SLOW_DOWN_FACTOR);
         
     }
     

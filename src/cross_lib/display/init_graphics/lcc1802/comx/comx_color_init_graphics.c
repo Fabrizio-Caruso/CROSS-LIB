09d05a0b28afe756b6c4a53de353bbec8201bfc7
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-11-18 23:09:37 +0100

    Avoid udg for non-graphics Cidelsa

--- src/cross_lib/display/init_graphics/lcc1802/comx/comx_color_init_graphics.c
+++ src/cross_lib/display/init_graphics/lcc1802/comx/comx_color_init_graphics.c
@@ -34,75 +34,75 @@
 #endif
 
 #if defined(__COMX__) || defined(__PECOM__) || defined(__MICRO__) || defined(REDEFINED_CHARS)
-    void redefine_char(uint8_t ch, const uint8_t * shapelocation)
-    {
-        uint8_t colored_shape[NUMBER_OF_LINES];
-        uint8_t i;
-        uint8_t color;
-        
-        for(color=0;color<3;++color)
+        void redefine_char(uint8_t ch, const uint8_t * shapelocation)
         {
-            for(i=0;i<NUMBER_OF_LINES;++i)
+            uint8_t colored_shape[NUMBER_OF_LINES];
+            uint8_t i;
+            uint8_t color;
+            
+            for(color=0;color<3;++color)
             {
-               colored_shape[i]=shapelocation[i]+COLOR_OFFSET+color*64U;
+                for(i=0;i<NUMBER_OF_LINES;++i)
+                {
+                   colored_shape[i]=shapelocation[i]+COLOR_OFFSET+color*64U;
+                }
+                shapechar(colored_shape, ch+color, 1);
             }
-            shapechar(colored_shape, ch+color, 1);
         }
-    }
 
-    const struct redefine_struct
-    {
-       const uint8_t _ascii;
-       const uint8_t _bitmap[9];
-    } ;
+        const struct redefine_struct
+        {
+           const uint8_t _ascii;
+           const uint8_t _bitmap[9];
+        } ;
 
+    #if defined(__COMX__) || defined(__PECOM__) || defined(__MICRO__)
+        const struct redefine_struct redefine_map[] =
+        {
+            {_TILE_0, _TILE_0_UDG},
+            {_TILE_1, _TILE_1_UDG},
+            {_TILE_2, _TILE_2_UDG},
+            {_TILE_3, _TILE_3_UDG},
+            {_TILE_4, _TILE_4_UDG},
+            {_TILE_5, _TILE_5_UDG},
+            {_TILE_6, _TILE_6_UDG},
+            {_TILE_7, _TILE_7_UDG},
+            {_TILE_8, _TILE_8_UDG},
+            {_TILE_9, _TILE_9_UDG},
+            {_TILE_10, _TILE_10_UDG},
+            {_TILE_11, _TILE_11_UDG},
+            {_TILE_12, _TILE_12_UDG},
+            {_TILE_13, _TILE_13_UDG},
+            {_TILE_14, _TILE_14_UDG},
+            {_TILE_15, _TILE_15_UDG},
+            {_TILE_16, _TILE_16_UDG},
+            {_TILE_17, _TILE_17_UDG},
+            {_TILE_18, _TILE_18_UDG},
+            // #if _XL_NUMBER_OF_TILES>=20
+            {_TILE_19, _TILE_19_UDG},
+            // #if _XL_NUMBER_OF_TILES>=21
+            {_TILE_20, _TILE_20_UDG},
+            // #if _XL_NUMBER_OF_TILES>=22
+            {_TILE_21, _TILE_21_UDG},
+            // #if _XL_NUMBER_OF_TILES>=23
+            {_TILE_22, _TILE_22_UDG},
+            // #if _XL_NUMBER_OF_TILES>=24
+            {_TILE_23, _TILE_23_UDG},
+            // #if _XL_NUMBER_OF_TILES>=25
+            {_TILE_24, _TILE_24_UDG},
+            // #if _XL_NUMBER_OF_TILES>=26
+            {_TILE_25, _TILE_25_UDG},
+            // #endif
+            // #endif
+            // #endif
+            // #endif
+            // #endif
+            // #endif
+            // #endif
+            
+        };
 
-const struct redefine_struct redefine_map[] =
-{
-    {_TILE_0, _TILE_0_UDG},
-    {_TILE_1, _TILE_1_UDG},
-    {_TILE_2, _TILE_2_UDG},
-    {_TILE_3, _TILE_3_UDG},
-    {_TILE_4, _TILE_4_UDG},
-    {_TILE_5, _TILE_5_UDG},
-    {_TILE_6, _TILE_6_UDG},
-    {_TILE_7, _TILE_7_UDG},
-    {_TILE_8, _TILE_8_UDG},
-    {_TILE_9, _TILE_9_UDG},
-    {_TILE_10, _TILE_10_UDG},
-    {_TILE_11, _TILE_11_UDG},
-    {_TILE_12, _TILE_12_UDG},
-    {_TILE_13, _TILE_13_UDG},
-    {_TILE_14, _TILE_14_UDG},
-    {_TILE_15, _TILE_15_UDG},
-    {_TILE_16, _TILE_16_UDG},
-    {_TILE_17, _TILE_17_UDG},
-    {_TILE_18, _TILE_18_UDG},
-    // #if _XL_NUMBER_OF_TILES>=20
-    {_TILE_19, _TILE_19_UDG},
-    // #if _XL_NUMBER_OF_TILES>=21
-    {_TILE_20, _TILE_20_UDG},
-    // #if _XL_NUMBER_OF_TILES>=22
-    {_TILE_21, _TILE_21_UDG},
-    // #if _XL_NUMBER_OF_TILES>=23
-    {_TILE_22, _TILE_22_UDG},
-    // #if _XL_NUMBER_OF_TILES>=24
-    {_TILE_23, _TILE_23_UDG},
-    // #if _XL_NUMBER_OF_TILES>=25
-    {_TILE_24, _TILE_24_UDG},
-    // #if _XL_NUMBER_OF_TILES>=26
-    {_TILE_25, _TILE_25_UDG},
-    // #endif
-    // #endif
-    // #endif
-    // #endif
-    // #endif
-    // #endif
-    // #endif
-    
-};
-
-
+    #endif
 
 #endif
 

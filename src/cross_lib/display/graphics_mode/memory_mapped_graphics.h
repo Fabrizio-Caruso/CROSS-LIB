938f82127c9dcd1fdea39bfedead5dd24a91ff17
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2021-11-19 09:06:53 +0100

    clone in command list

--- src/cross_lib/display/graphics_mode/memory_mapped_graphics.h
+++ src/cross_lib/display/graphics_mode/memory_mapped_graphics.h
@@ -90,23 +90,13 @@
 
 #if !defined(_XL_NO_COLOR)
 
-    #if defined(__TMC600__)
-        #define _XL_DRAW(x,y,tile,color) \
-        do \
-        { \
-            DISPLAY_POKE(loc(x,y), (tile)); \
-            COLOR_POKE((uint16_t) ((uint16_t) (COLOR_ADDR+(x)) +(uint16_t)(y)*(XSize)),(color)); \
-        } \
-        while(0)
-    #else
-        #define _XL_DRAW(x,y,tile,color) \
-        do \
-        { \
-            COLOR_POKE((uint16_t) ((uint16_t) (COLOR_ADDR+(x)) +(uint16_t)(y)*(XSize)),(color)); \
-            DISPLAY_POKE(loc(x,y), (tile)); \
-        } \
-        while(0)
-    #endif
+    #define _XL_DRAW(x,y,tile,color) \
+    do \
+    { \
+        COLOR_POKE((uint16_t) ((uint16_t) (COLOR_ADDR+(x)) +(uint16_t)(y)*(XSize)),(color)); \
+        DISPLAY_POKE(loc(x,y), (tile)); \
+    } \
+    while(0)
         
 #else
 	#define _XL_DRAW(x,y,tile,color) \

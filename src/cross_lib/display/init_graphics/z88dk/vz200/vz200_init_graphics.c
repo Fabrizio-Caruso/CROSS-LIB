7a93428fea53c6e0389ac47f3e0033dadb5bfff2
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2022-03-26 20:17:39 +0100

    Document vz200 init to fix loading by some emulators

--- src/cross_lib/display/init_graphics/z88dk/vz200/vz200_init_graphics.c
+++ src/cross_lib/display/init_graphics/z88dk/vz200/vz200_init_graphics.c
@@ -2,5 +2,6 @@
 
 void _XL_INIT_GRAPHICS(void)
 {
+    // Necessary to have .vz file correctly started on several emulators that do not load .vz as they should
     *((unsigned char *)0x7839) |= 1; 
 }

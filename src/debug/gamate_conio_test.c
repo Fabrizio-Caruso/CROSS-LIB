a0671c93783a05bc47a326e2da8b2c158303a694
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2022-04-02 15:28:15 +0200

    Improve the gamate conio test

--- src/debug/gamate_conio_test.c
+++ src/debug/gamate_conio_test.c
@@ -13,7 +13,22 @@ int main(void)
     textcolor(COLOR_WHITE);
     gotoxy(2,2);
     cprintf("ciao!");
-    
+    sleep(1);
+    gotoxy(2,2);
+    textcolor(1);
+    cprintf("hello");
+    sleep(1);
+    textcolor(2);
+    gotoxy(2,2);
+    cprintf("ciao!");
+    sleep(1);
+    gotoxy(2,2);
+    textcolor(COLOR_BLACK);
+    cprintf("hello");
+    sleep(1);
+    textcolor(COLOR_WHITE);
+    gotoxy(2,2);
+    cprintf("ciao!");    
     while(1){};
     return 0;
 }

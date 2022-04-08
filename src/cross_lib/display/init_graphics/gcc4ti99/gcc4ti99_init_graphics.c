a9f9ee99a6976b0cedfedcd6f545cdc76c75b7b0
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2022-04-08 22:50:28 +0200

    Text works on the TI99

--- src/cross_lib/display/init_graphics/gcc4ti99/gcc4ti99_init_graphics.c
+++ src/cross_lib/display/init_graphics/gcc4ti99/gcc4ti99_init_graphics.c
@@ -7,12 +7,17 @@
 
 void _XL_INIT_GRAPHICS(void)
 {
-    // set_text();
-    charsetlc();
-    cursor(0);
-    textcolor(COLOR_WHITE);
-    // bgcolor(COLOR_DKBLUE);
-    bgcolor(COLOR_BLACK);
+    set_graphics(VDP_SPR_16x16);                        // set video mode
+    charsetlc();                                        // load character set with lowercase
+    vdpmemset(gImage, 32, 768);                         // clear screen
+    VDP_SET_REGISTER(VDP_REG_COL, COLOR_MEDGREEN);      // set screen color
+    vdpmemset(gColor, 0x10, 32);                        // set colors to black on transparent
+    
+    
+    // charsetlc();
+    // cursor(0);
+    // textcolor(COLOR_WHITE);
+    // bgcolor(COLOR_BLACK);
 
     setScreenColors();
 }
--- src/debug/ti99_test.c
+++ src/debug/ti99_test.c
@@ -1,14 +1,20 @@
 
 #include <conio.h>
-
+#include <vdp.h>
 
 int main(void)
 {
-        gotoxy(2,2);
-        
-        cprintf("Hello");
+    set_graphics(VDP_SPR_16x16);                        // set video mode
+    charsetlc();                                        // load character set with lowercase
+    vdpmemset(gImage, 32, 768);                         // clear screen
+    VDP_SET_REGISTER(VDP_REG_COL, COLOR_MEDGREEN);      // set screen color
+    vdpmemset(gColor, 0x10, 32);                        // set colors to black on transparent
+    
+    gotoxy(2,2);
+    
+    cprintf("Hello");
 
-        while(1){};
-        return 0;
+    while(1){};
+    return 0;
 }
 

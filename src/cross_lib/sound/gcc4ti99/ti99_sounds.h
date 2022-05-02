5d895291380d62bc604ed872083ed0d4ba2cb158
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2022-05-02 22:54:55 +0200

    Improve sounds in Ti99

--- src/cross_lib/sound/gcc4ti99/ti99_sounds.h
+++ src/cross_lib/sound/gcc4ti99/ti99_sounds.h
@@ -5,19 +5,19 @@
 
 void _XL_ZAP_SOUND();
 
-void NOISE(uint16_t duration, uint8_t period);
+void NOISE(uint16_t duration, uint8_t mode);
 
-void CLICK(uint16_t duration, uint8_t period);
+void CLICK(uint16_t duration, uint16_t period);
 
 #define _XL_EXPLOSION_SOUND() NOISE(1000,4)
 
-#define _XL_SHOOT_SOUND() NOISE(400,6)
+#define _XL_SHOOT_SOUND() NOISE(200,5)
 
-#define _XL_PING_SOUND() CLICK(550,5)
+#define _XL_PING_SOUND() CLICK(550,900)
 
-#define _XL_TICK_SOUND() CLICK(450,4)
+#define _XL_TICK_SOUND() CLICK(450,700)
 
-#define _XL_TOCK_SOUND() CLICK(350,3)
+#define _XL_TOCK_SOUND() CLICK(350,300)
 
 #endif // __TI99_SOUNDS
 

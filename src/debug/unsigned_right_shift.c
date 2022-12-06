bfb794291fb639f78f01030c2f777152bcbc272b
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2022-12-07 00:50:01 +0100

    Minimal test case for a right shift CC65 bug

--- src/debug/unsigned_right_shift.c
+++ src/debug/unsigned_right_shift.c
@@ -1,39 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint8_t level;
-
-#define WORD_SIZE 5
-#define INITIAL_ROWS 3
-
-#if WORD_SIZE>20
-    #define SMALL_SCREEN_REDUCTION 1
-#else
-    #define SMALL_SCREEN_REDUCTION 0
-#endif
-
-#define MIN_INITIAL_DROP ((((WORD_SIZE)*INITIAL_ROWS))+SMALL_SCREEN_REDUCTION)
-
-void initial_letter_drop(void)
-{
-    uint8_t i;
-   
-    for(i=0;i<(uint8_t) MIN_INITIAL_DROP + (level>>0);++i)
-    {
-        // drop_letter();
-        // short_pause();
-    } 
-}
+uint8_t foo;
+uint8_t bar; // Without this there is no bug
 
 int main(void)
-{
+{            
+    uint8_t i; 
 
-    level = 1;
+    foo = 1U;
+    bar = 3U;  // Without this there is no bug
 
-    initial_letter_drop();
-    
-    printf("end\n");
-    while(1){};
-    
+    for(i=0;i<foo>>0;++i)
+    {
+        printf("foo\n");
+    }
+
+    printf("OK");
+
     return 0;
 }
+

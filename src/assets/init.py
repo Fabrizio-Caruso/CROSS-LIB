9d910be7c71d59b098c04a4877439486df864483
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2023-01-29 13:17:20 +0100

    Initial support for a config.ini file

--- src/assets/init.py
+++ src/assets/init.py
@@ -1,5 +1,11 @@
 from os import walk
 
+import sys
+
+DEFAULT_THREADS = 8
+
+python_version = sys.version_info[0]
+python_subversion = sys.version_info[1]
 
 def dirs_in_path(mypath):
     dirs = []

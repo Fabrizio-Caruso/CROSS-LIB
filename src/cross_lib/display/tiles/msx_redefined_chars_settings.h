8f2b3b5b57262b75142e0bd873f14cde9dabbccc
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2023-02-09 09:35:17 +0100

    Fix xl test z88dk_quick

--- src/cross_lib/display/tiles/msx_redefined_chars_settings.h
+++ src/cross_lib/display/tiles/msx_redefined_chars_settings.h
@@ -21,7 +21,6 @@
     #define _TILE_12 0x26
     #define _TILE_13 0x22
     #define _TILE_25 0x24
-    #define _TILE_4_B _TILE_4
     #define _TILE_10 0x23
 
     // GREEN
@@ -33,10 +32,6 @@
     #define _TILE_14 ((uint8_t)0x7B)
     #define _TILE_15 ((uint8_t)0x7D)
     #define _TILE_16 ((uint8_t)0x60)
-
-    // TODO: Change it to something else
-    // #define _TILE_5_B _TILE_5
-    #define _TILE_5_B 'X'
 
     #define _TILE_19 0x80
     #define _TILE_20 0x81
@@ -51,8 +46,9 @@
     #define _TILE_6 0xEB
     #define _TILE_18 0xEC
 
-    #define _TILE_7_B _TILE_7
-    #define _TILE_0_B _TILE_0
+	// TODO: Check this
+    #define _TILE_26 0x86
+
 
 #else
     // CYAN
@@ -68,9 +64,7 @@
     #define _TILE_7 0x27
     #define _TILE_12 0x26
     #define _TILE_13 0x22
-    #define _DOLLAR_TILE 0x24
     #define _TILE_10 0x23
-    #define _TILE_4_B _TILE_4
 
     // GREEN
     #define _TILE_0  0x2C
@@ -81,14 +75,9 @@
     // RED
     #define _TILE_5 0x86
     #define _TILE_6 0x87
-    #define _TILE_7_B _TILE_7
-    #define _TILE_0_B _TILE_0
 
     // WHITE
     #define _TILE_8 0xA9
-
-    // TODO: Change it to something else
-    #define _TILE_5_B 'X'
 
     #define _TILE_19 0x80
     #define _TILE_20 0x81
@@ -96,6 +85,9 @@
     #define _TILE_22 0x83
     #define _TILE_23 0x84
     #define _TILE_24 0x85
+	
+	// TODO: check this
+	#define _TILE_26 0xB0
 
 #endif
 

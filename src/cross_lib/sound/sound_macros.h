e37ca10fed23757780bc0bcb3a0f53e1f4255482
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2023-04-18 18:25:24 +0200

    Initial removal of -DSOUNDS

--- src/cross_lib/sound/sound_macros.h
+++ src/cross_lib/sound/sound_macros.h
@@ -24,87 +24,93 @@
 
 #ifndef _SOUNDS_MACROS
 #define _SOUNDS_MACROS
-	#  if defined(FORCE_BIT_BANG)
-		#include "z88dk_bit_bang_sounds.h"
-	#elif defined(FORCE_PSG)
-		#include "psg_sounds.h"
-	#elif defined(ASCII_SOUNDS)
-		#define _XL_INIT_SOUND()
-		#include "ascii_sounds.h"
-    #elif defined(__TI99__) && defined(SOUNDS)
-        #include "ti99_sounds.h"
-	#elif defined(__ACE__) && defined(SOUNDS)
-		#include "z88dk_bit_bang_sounds.h"
-	#elif (defined(__ATARI__) && defined(SOUNDS))
-		#include "pokey_sounds.h"
-	#elif defined(__ATARI5200__) && defined(SOUNDS)
-		#include "pokey_sounds.h"
-	#elif defined(__ATMOS__) && defined(SOUNDS)
-		#include "atmos_sounds.h"
-	#elif defined(__AQUARIUS__) && defined(SOUNDS)
-		#include "z88dk_bit_bang_sounds.h"
-	#elif defined(__BEE__) && defined(SOUNDS)
-		#include "z88dk_bit_bang_sounds.h"
-	#elif defined(__C16__) && defined(SOUNDS)
-		#include "c264_sounds.h"
-	#elif defined(__C64__) && defined(SOUNDS)
-		#include "sid_sounds.h"
-	#elif defined(__C128__) && defined(SOUNDS)
-		#include "sid_sounds.h"
-	#elif defined(__CBM510__) && defined(SOUNDS)
-		#include "sid_sounds.h"
-	#elif defined(__COLECO__) && defined(SOUNDS)
-		#include "psg_sounds.h"
-	#elif defined(__COLECO_ADAM__) && defined(SOUNDS)
-		#include "psg_sounds.h"
-	#elif (defined(__COMX__) || defined(__PECOM__) || defined(__TMC600__) || defined(__CIDELSA__)) && defined(SOUNDS)
-		#include "comx_sounds.h"
-	#elif defined(__CPC__) && defined(SOUNDS)
-		#include "psg_sounds.h"
-	#elif defined(__MC1000__) && defined(SOUNDS)
-		#include "z88dk_bit_bang_sounds.h"
-	#elif defined(__MSX__) && defined(SOUNDS)
-		#include "psg_sounds.h"
-	#elif defined(__RX78__) && defined(SOUNDS)
-		#include "psg_sounds.h"
-	#elif defined(__SPECTRUM__) && defined(SOUNDS)
-		#include "z88dk_bit_bang_sounds.h"
-	#elif defined(__SV8000__) && defined(SOUNDS)
-		#include "psg_sounds.h"
-	#elif defined(__SVI__) && defined(SOUNDS)
-		#include "psg_sounds.h"
-	#elif defined(__VG5K__) && defined(SOUNDS)
-		#include "z88dk_bit_bang_sounds.h"
-	#elif defined(__VIC20__) && !defined(VIC20_UNEXPANDED) && defined(SOUNDS)
-		#include "vic20_sounds.h"
-	#elif defined(__VIC20__) && defined(VIC20_UNEXPANDED) && defined(SOUNDS)
-		#include "vic20_explosion_sound.h"
-	#elif defined(__VZ__) && defined(SOUNDS)
-		#include "z88dk_bit_bang_sounds.h"
-	#elif defined(__WINCMOC__) && defined(SOUNDS)
-		#include "wincmoc_sounds.h"
-	#elif (defined(__COCO__) || defined(__DRAGON__)) && defined(SOUNDS) 
-		#include "dac_sounds.h"
-	#elif (defined(__MO5__) || defined(__TO7__)) && defined(SOUNDS)
-		#include "dac_sounds.h"
-    #elif defined(__GAMATE__) && defined(SOUNDS)
-        #include "gamate_sounds.h"
-    #elif defined(__CREATIVISION__) && defined(SOUNDS)
-        #include "creativision_sounds.h"
-    #elif defined(__GB__) && defined(SOUNDS)
-        #include "gb_sounds.h"
-    #elif (defined(__APPLE2__) || defined(__APPLE2ENH__)) && defined(SOUNDS)
-        #include "bit_bang_sounds.h"
-    #elif defined(__PET__) && defined(SOUNDS)
-        #include "pet_sounds.h"
-	#elif defined(__ATARI_LYNX__)
-        #include "atari_lynx_sounds.h"
-    #elif defined(__MC10__) && defined(SOUNDS)
-        #include "bit_bang_sounds.h"
-    #elif defined(__SUPERVISION__) && defined(SOUNDS)
-        #include "supervision_sounds.h"
-    #else
-        #define _XL_NO_SOUND
+    #if !defined(_XL_NO_SOUND)
+        #  if defined(FORCE_BIT_BANG)
+            #include "z88dk_bit_bang_sounds.h"
+        #elif defined(FORCE_PSG)
+            #include "psg_sounds.h"
+        #elif defined(ASCII_SOUNDS)
+            #define _XL_INIT_SOUND()
+            #include "ascii_sounds.h"
+        #elif defined(__TI99__) 
+            #include "ti99_sounds.h"
+        #elif defined(__ACE__) 
+            #include "z88dk_bit_bang_sounds.h"
+        #elif (defined(__ATARI__) )
+            #include "pokey_sounds.h"
+        #elif defined(__ATARI5200__) 
+            #include "pokey_sounds.h"
+        #elif defined(__ATMOS__) 
+            #include "atmos_sounds.h"
+        #elif defined(__AQUARIUS__) 
+            #include "z88dk_bit_bang_sounds.h"
+        #elif defined(__BEE__) 
+            #include "z88dk_bit_bang_sounds.h"
+        #elif defined(__C16__) 
+            #include "c264_sounds.h"
+        #elif defined(__C64__) 
+            #include "sid_sounds.h"
+        #elif defined(__C128__) 
+            #include "sid_sounds.h"
+        #elif defined(__CBM510__) 
+            #include "sid_sounds.h"
+        // #elif defined(__COLECO__) 
+            // #include "psg_sounds.h"
+        // #elif defined(__COLECO_ADAM__) 
+            // #include "psg_sounds.h"
+        #elif (defined(__COMX__) || defined(__PECOM__) || defined(__TMC600__) || defined(__CIDELSA__)) 
+            #include "comx_sounds.h"
+        #elif defined(__CPC__) 
+            #include "psg_sounds.h"
+        #elif defined(__MC1000__) 
+            #include "z88dk_bit_bang_sounds.h"
+        #elif defined(__MSX__) 
+            #include "psg_sounds.h"
+        #elif defined(__RX78__) 
+            #include "psg_sounds.h"
+        #elif defined(__SPECTRUM__) 
+            #include "z88dk_bit_bang_sounds.h"
+        #elif defined(__SV8000__) 
+            #include "psg_sounds.h"
+        #elif defined(__SVI__) 
+            #include "psg_sounds.h"
+        #elif defined(__VG5K__) 
+            #include "z88dk_bit_bang_sounds.h"
+        #elif defined(__VIC20__) && !defined(VIC20_UNEXPANDED) 
+            #include "vic20_sounds.h"
+        #elif defined(__VIC20__) && defined(VIC20_UNEXPANDED) 
+            #include "vic20_explosion_sound.h"
+        #elif defined(__VZ__) 
+            #include "z88dk_bit_bang_sounds.h"
+        #elif defined(__WINCMOC__) 
+            #include "wincmoc_sounds.h"
+        #elif (defined(__COCO__) || defined(__DRAGON__))  
+            #include "dac_sounds.h"
+        #elif (defined(__MO5__) || defined(__TO7__)) 
+            #include "dac_sounds.h"
+        #elif defined(__GAMATE__) 
+            #include "gamate_sounds.h"
+        #elif defined(__CREATIVISION__) 
+            #include "creativision_sounds.h"
+        #elif defined(__GB__) 
+            #include "gb_sounds.h"
+        #elif (defined(__APPLE2__) || defined(__APPLE2ENH__)) 
+            #include "bit_bang_sounds.h"
+        #elif defined(__PET__) 
+            #include "pet_sounds.h"
+        #elif defined(__ATARI_LYNX__)
+            #include "atari_lynx_sounds.h"
+        #elif defined(__MC10__) 
+            #include "bit_bang_sounds.h"
+        // #elif defined(__SUPERVISION__) 
+            // #include "supervision_sounds.h"
+        #else
+            #define _XL_NO_SOUND
+        #endif
+    #endif
+    
+    
+    #if defined(_XL_NO_SOUND)
 		#define _XL_EXPLOSION_SOUND() 
 		#define _XL_PING_SOUND() 
 		#define _XL_SHOOT_SOUND() 
@@ -113,8 +119,8 @@
 		#define _XL_ZAP_SOUND() 
 	#endif
     
-    #if defined(SOUNDS) && (defined(FORCE_PSG) || defined(__GB__) || defined(__MO5__) || defined(__TO7__) || defined(__COCO__) || defined(__DRAGON__) || defined(__MSX__))
-    // _XL_INIT_SOUND() needs to be defined
+    #if (defined(FORCE_PSG) || defined(__GB__) || defined(__MO5__) || defined(__TO7__) || defined(__COCO__) || defined(__DRAGON__) || defined(__MSX__))
+     // _XL_INIT_SOUND() needs to be defined
     #else
         #define _XL_INIT_SOUND()
     #endif

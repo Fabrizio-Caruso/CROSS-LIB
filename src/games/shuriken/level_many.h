df1ec8af70adca8f3a23dfa48ad03aefd9eab5d4
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2023-07-01 18:52:38 +0200

    more adaptations

--- src/games/shuriken/level_many.h
+++ src/games/shuriken/level_many.h
@@ -37,32 +37,56 @@
 #define LV_MANY_OBJECTS_SIZE LV_OBJECTS_SIZE(LV_MANY_NO_OF_OBJECTS)
 
 
+#if XSize>=20
+    #define LV_MANY_SHURIKENS \
+        8, 4, 4, \
+        \
+        3,1+3, \
+        3,1+5, \
+        XSize-4,1+6, \
+        3,YSize-1-6, \
+        3,YSize-1-4, \
+        XSize-4,1+4, \
+        XSize-4,YSize-1-5, \
+        XSize-4,YSize-1-3, \
+        \
+        2,4, \
+        XSize-1-2,4, \
+        4,4, \
+        XSize-1-4,5, \
+        \
+        2, \
+        XSize-1-2, \
+        3, \
+        XSize-1-3
 
-#define LV_MANY_SHURIKENS \
-    8, 4, 4, \
-	\
-    3,1+3, \
-    3,1+5, \
-    XSize-4,1+6, \
-    3,YSize-1-6, \
-    3,YSize-1-4, \
-    XSize-4,1+4, \
-    XSize-4,YSize-1-5, \
-    XSize-4,YSize-1-3, \
-    \
-    2,4, \
-    XSize-1-2,4, \
-    4,4, \
-    XSize-1-4,5, \
-    \
-    2, \
-    XSize-1-2, \
-    3, \
-    XSize-1-3
+
+    #define LV_MANY_SHURIKENS_SIZE LV_SHURIKENS_SIZE(8,4,4)
+#else
+    #define LV_MANY_SHURIKENS \
+        8, 2, 4, \
+        \
+        3,1+3, \
+        3,1+5, \
+        XSize-4,1+6, \
+        3,YSize-1-6, \
+        3,YSize-1-4, \
+        XSize-4,1+4, \
+        XSize-4,YSize-1-5, \
+        XSize-4,YSize-1-3, \
+        \
+        2,4, \
+        XSize-1-2,4, \
+        \
+        2, \
+        XSize-1-2, \
+        3, \
+        XSize-1-3
 
 
-#define LV_MANY_SHURIKENS_SIZE LV_SHURIKENS_SIZE(10,4,4)
+    #define LV_MANY_SHURIKENS_SIZE LV_SHURIKENS_SIZE(8,2,4)
 
+#endif
 
 // 
 

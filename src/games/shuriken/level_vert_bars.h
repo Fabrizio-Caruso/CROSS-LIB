e2075aaa26c4d5d5b33a94ea46b29e1ea8f59465
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2023-07-01 09:59:04 +0200

    Adapt to smaller screens

--- src/games/shuriken/level_vert_bars.h
+++ src/games/shuriken/level_vert_bars.h
@@ -11,28 +11,52 @@
     #define LV_VERT_BARS_WIDTH (XSize/4-1)
 #endif
 
-#define LV_VERT_BARS_NO_OF_OBJECTS 14
 
-#define LV_VERT_BARS_OBJECTS \
-    LV_VERT_BARS_NO_OF_OBJECTS, \
-	\
-    5+LV_VERT_BARS_WIDTH,4,1,YSize-7,WALL, \
-    XSize-6-LV_VERT_BARS_WIDTH,4,1,YSize-7,WALL, \
-    \
-    3,4,1,YSize-7,WALL, \
-    XSize-4,4,1,YSize-7,WALL, \
-	\
-	XSize-3,4,1,1,RING, \
-	XSize-3,YSize-4,1,1,FREEZE, \
-	2,4,1,1,RING, \
-	2,YSize-4,1,1,FREEZE, \
-    \
-    2,5,1,YSize-9,DIAMOND, \
-    4,5,1,YSize-9,DIAMOND, \
-    6+LV_VERT_BARS_WIDTH,7,1,YSize-7-6,DIAMOND, \
-    XSize-7-LV_VERT_BARS_WIDTH,7,1,YSize-7-6,DIAMOND, \
-    XSize-5,5,1,YSize-9,DIAMOND,  \
-    XSize-3,5,1,YSize-9,DIAMOND
+#if XSize>=20
+    #define LV_VERT_BARS_NO_OF_OBJECTS 14
+
+    #define LV_VERT_BARS_OBJECTS \
+        LV_VERT_BARS_NO_OF_OBJECTS, \
+        \
+        5+LV_VERT_BARS_WIDTH,4,1,YSize-7,WALL, \
+        XSize-6-LV_VERT_BARS_WIDTH,4,1,YSize-7,WALL, \
+        \
+        3,4,1,YSize-7,WALL, \
+        XSize-4,4,1,YSize-7,WALL, \
+        \
+        XSize-3,4,1,1,RING, \
+        XSize-3,YSize-4,1,1,FREEZE, \
+        2,4,1,1,RING, \
+        2,YSize-4,1,1,FREEZE, \
+        \
+        2,5,1,YSize-9,DIAMOND, \
+        4,5,1,YSize-9,DIAMOND, \
+        6+LV_VERT_BARS_WIDTH,7,1,YSize-7-6,DIAMOND, \
+        XSize-7-LV_VERT_BARS_WIDTH,7,1,YSize-7-6,DIAMOND, \
+        XSize-5,5,1,YSize-9,DIAMOND,  \
+        XSize-3,5,1,YSize-9,DIAMOND
+
+#else
+    #define LV_VERT_BARS_NO_OF_OBJECTS 10
+
+    #define LV_VERT_BARS_OBJECTS \
+        LV_VERT_BARS_NO_OF_OBJECTS, \
+        \
+        3,4,1,YSize-7,WALL, \
+        XSize-4,4,1,YSize-7,WALL, \
+        \
+        XSize-3,4,1,1,RING, \
+        XSize-3,YSize-4,1,1,FREEZE, \
+        2,4,1,1,RING, \
+        2,YSize-4,1,1,FREEZE, \
+        \
+        2,5,1,YSize-9,DIAMOND, \
+        4,5,1,YSize-9,DIAMOND, \
+        \
+        XSize-5,5,1,YSize-9,DIAMOND,  \
+        XSize-3,5,1,YSize-9,DIAMOND    
+
+#endif
 
 #define LV_VERT_BARS_OBJECTS_SIZE LV_OBJECTS_SIZE(LV_VERT_BARS_NO_OF_OBJECTS)
 
@@ -60,15 +84,26 @@
 
 #define LV_VERT_BARS_NO_OF_WALLS 2
 
+#if XSize>=20
+    #define LV_VERT_BARS_WALLS \
+        LV_VERT_BARS_NO_OF_WALLS, \
+        \
+        6+LV_VERT_BARS_WIDTH,5, \
+        XSize-12-2*LV_VERT_BARS_WIDTH, 1, \
+        \
+        6+LV_VERT_BARS_WIDTH,YSize-5, \
+        XSize-12-2*LV_VERT_BARS_WIDTH,1
 
-#define LV_VERT_BARS_WALLS \
-    LV_VERT_BARS_NO_OF_WALLS, \
-	\
-    6+LV_VERT_BARS_WIDTH,5, \
-    XSize-12-2*LV_VERT_BARS_WIDTH, 1, \
-    \
-    6+LV_VERT_BARS_WIDTH,YSize-5, \
-    XSize-12-2*LV_VERT_BARS_WIDTH,1 \
+#else
+    #define LV_VERT_BARS_WALLS \
+        LV_VERT_BARS_NO_OF_WALLS, \
+        \
+        5,5, \
+        XSize-8-2*1, 1, \
+        \
+        5,YSize-5, \
+        XSize-8-2*1,1
+#endif
 
 #define LV_VERT_BARS_WALLS_SIZE LC_WALLS_SIZE(LV_VERT_BARS_NO_OF_WALLS)
 

f963fc286a7c8ab66da55f9e4d43be585317def3
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2023-07-05 22:40:09 +0200

    Fix color setting for sc3000

--- src/cross_lib/display/init_graphics/z88dk/udg/udg_init_graphics.c
+++ src/cross_lib/display/init_graphics/z88dk/udg/udg_init_graphics.c
@@ -13,25 +13,18 @@ void _XL_INIT_GRAPHICS(void)
 	void *param = &udgs;
 	console_ioctl(IOCTL_GENCON_SET_FONT32, &param);
 	
-	#if defined(__MC1000__) || defined(__PC6001__) || defined(__CPC__) \
-    || defined(__MULTI8__) || defined(__SPC1000__) || defined(__GB__) || defined(__SMS__) \
-    || defined(__GAL_PLUS__) || defined(__GAME_GEAR__) || defined(__MTX__) || defined(__MSX__) || defined(__SAM__) 
-	{
-		#if !defined(__SCREEN_MODE)
-			#define __SCREEN_MODE 2
-		#endif
-        
-        #if defined(__MC1000__) && defined(_BACKGROUND_COLOR) && _BACKGROUND_COLOR==_XL_WHITE
-            int mode = __SCREEN_MODE + 32;
-        #else
-            int mode = __SCREEN_MODE;
-        #endif
-            
-		console_ioctl(IOCTL_GENCON_SET_MODE, &mode);
-        
-	}
-    #endif
-    
+
+	#if !defined(__SCREEN_MODE)
+		#define __SCREEN_MODE 2
+	#endif
+	
+	#if defined(__MC1000__) && defined(_BACKGROUND_COLOR) && _BACKGROUND_COLOR==_XL_WHITE
+		int mode = __SCREEN_MODE + 32;
+	#else
+		int mode = __SCREEN_MODE;
+	#endif
+		
+	console_ioctl(IOCTL_GENCON_SET_MODE, &mode); 
     
 	#if defined(__SPECTRUM__)
         printf("\x01\x20");

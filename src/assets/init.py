38b1e22d457e46eeb7d7f9d94b0d781099156123
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2023-07-21 10:58:33 +0200

    color class in init.py

--- src/assets/init.py
+++ src/assets/init.py
@@ -18,4 +18,14 @@ game_projects = dirs_in_path("./games")
 example_projects = dirs_in_path("./examples")
 
     
-        
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'

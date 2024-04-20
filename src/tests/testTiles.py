63f26f93fa02a4ecedd1ca23634bb745badf07ec
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2024-04-20 23:31:57 +0200

    Initial uni-tests for tile auxiliary routines

---
+++ src/tests/testTiles.py
@@ -0,0 +1,104 @@
+import unittest
+import sys
+
+
+
+sys.path.append("assets")
+
+# from generate_assets import *
+
+from tiles import *
+
+
+
+class testTiles(unittest.TestCase):
+
+
+    # detect_xsize
+    def test_detect_xsize_1(self):
+        res = detect_xsize("vic20")
+        self.assertEqual(res,8)
+
+    def test_detect_xsize_2(self):
+        res = detect_xsize("oric")
+        self.assertEqual(res,6)
+
+    def test_detect_xsize_3(self):
+        res = detect_xsize("pecom")
+        self.assertEqual(res,6)
+
+    def test_detect_xsize_4(self):
+        res = detect_xsize("comx")
+        self.assertEqual(res,6)
+
+    def test_detect_xsize_5(self):
+        res = detect_xsize("micro")
+        self.assertEqual(res,6)
+
+    def test_detect_xsize_6(self):
+        res = detect_xsize("atari_lynx")
+        self.assertEqual(res,8)
+
+    def test_detect_xsize_7(self):
+        res = detect_xsize("apple2")
+        self.assertEqual(res,7)
+
+    def test_detect_xsize_8(self):
+        res = detect_xsize("apple2enh")
+        self.assertEqual(res,7)
+        
+    def test_detect_xsize_9(self):
+        res = detect_xsize("comx_ntsc")
+        self.assertEqual(res,6)
+
+    def test_detect_xsize_10(self):
+        res = detect_xsize("micro_ntsc")
+        self.assertEqual(res,6)
+
+
+    # detect_ysize
+    def test_detect_ysize_1(self):
+        res = detect_ysize("vic20")
+        self.assertEqual(res,8)
+
+    def test_detect_ysize_2(self):
+        res = detect_ysize("oric")
+        self.assertEqual(res,8)
+
+    def test_detect_ysize_3(self):
+        res = detect_ysize("pecom")
+        self.assertEqual(res,9)
+
+    def test_detect_ysize_4(self):
+        res = detect_ysize("comx")
+        self.assertEqual(res,9)
+
+    def test_detect_ysize_5(self):
+        res = detect_ysize("micro")
+        self.assertEqual(res,9)
+
+    def test_detect_ysize_6(self):
+        res = detect_ysize("atari_lynx")
+        self.assertEqual(res,6)
+
+    def test_detect_ysize_7(self):
+        res = detect_ysize("apple2")
+        self.assertEqual(res,8)
+
+    def test_detect_ysize_8(self):
+        res = detect_ysize("apple2enh")
+        self.assertEqual(res,8)
+        
+    def test_detect_ysize_9(self):
+        res = detect_ysize("comx_ntsc")
+        self.assertEqual(res,8)
+
+    def test_detect_ysize_10(self):
+        res = detect_ysize("micro_ntsc")
+        self.assertEqual(res,8)
+
+
+if __name__ == '__main__':
+    # global_vars.verbose = 0
+    # global_vars.test = 1
+    unittest.main()

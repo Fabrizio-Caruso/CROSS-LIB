d76aa4e21480a94f0806c01d2dd87314e4a104e2
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2024-10-31 18:57:26 +0100

    Wait for input on the TI99

--- src/cross_lib/input/input_macros.c
+++ src/cross_lib/input/input_macros.c
@@ -45,39 +45,39 @@
     #include "kscan.h"
 
     uint8_t __joystfast(int unit) {
-        unsigned int result;
+    unsigned int result;
 
-        // read the joystick lines (column 6 or 7, (5 added to unit))
-        __asm__("li r12,>0024\n\tai %1,5\n\tswpb %1\n\tldcr %1,3\n\tsrc r12,7\n\tli r12,>0006\n\tclr %0\n\tstcr %0,8" : "=r"(result) : "r"(unit) : "r12");
+    // read the joystick lines (column 6 or 7, (5 added to unit))
+    __asm__("li r12,>0024\n\tai %1,5\n\tswpb %1\n\tldcr %1,3\n\tsrc r12,7\n\tli r12,>0006\n\tclr %0\n\tstcr %0,8" : "=r"(result) : "r"(unit) : "r12");
 
-        KSCAN_JOYY = 0;
-        KSCAN_JOYX = 0;
+    KSCAN_JOYY = 0;
+    KSCAN_JOYX = 0;
 
-        if ((result & 0x0200) == 0) 
-        {
-            KSCAN_JOYX = JOY_LEFT;
-            return JOY_LEFT;
-        }
-        if ((result & 0x0400) == 0) 
-        {
-            KSCAN_JOYX = JOY_RIGHT;
-            return JOY_RIGHT;
-        }
-        if ((result & 0x0800) == 0) 
-        {
-            KSCAN_JOYY = JOY_DOWN;
-            return JOY_DOWN;
-        }
-        if ((result & 0x1000) == 0) 
-        {
-            KSCAN_JOYY = JOY_UP;
-            return JOY_UP;
-        }
-        kscan(1);
-        if(result==18)
-        {
-            return 18;
-        }
+    if ((result & 0x0200) == 0) 
+    {
+        KSCAN_JOYX = JOY_LEFT;
+        return JOY_LEFT;
+    }
+    if ((result & 0x0400) == 0) 
+    {
+        KSCAN_JOYX = JOY_RIGHT;
+        return JOY_RIGHT;
+    }
+    if ((result & 0x0800) == 0) 
+    {
+        KSCAN_JOYY = JOY_DOWN;
+        return JOY_DOWN;
+    }
+    if ((result & 0x1000) == 0) 
+    {
+        KSCAN_JOYY = JOY_UP;
+        return JOY_UP;
+    }
+    kscan(1);
+    if(result==18)
+    {
+        return 18;
+    }
     }
     
     uint8_t JOY_INPUT(void)
@@ -469,8 +469,15 @@ out         stb res
         }
     #elif defined(__TI99__)
         // TODO: Implement this
+        #include "kscan.h"
+        
         void _XL_WAIT_FOR_INPUT(void)
         {
+            do
+            {
+                kscan(1);
+            } while(KSCAN_KEY!=18);
+            
         }
     #else
         #include<joystick.h>

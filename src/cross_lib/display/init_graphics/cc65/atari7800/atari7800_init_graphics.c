5cbad565ca8ceeb0f396bb4ff0f9165dbd0f43e3
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2024-11-07 21:34:42 +0100

    __ALTERNATE_COLORS for Atari 7800

--- src/cross_lib/display/init_graphics/cc65/atari7800/atari7800_init_graphics.c
+++ src/cross_lib/display/init_graphics/cc65/atari7800/atari7800_init_graphics.c
@@ -9,5 +9,18 @@ extern char screen[];
 void _XL_INIT_GRAPHICS(void)
 {
 
+    #if defined(__ALTERNATE_COLORS)
+    
+        // TODO: Implement palette switch
+        // MARIA.bkgrnd = 0x17;
+        // MARIA.p0c1 = 0x00;
+        // MARIA.p0c2 = 0xb2;
+        // MARIA.p0c3 = 0x05;
+        MARIA.bkgrnd = 0x00;
+        // MARIA.p0c1 = 0x00;
+        MARIA.p0c2 = 0xb2;
+        MARIA.p0c3 = 0x05;
+    #endif
+
 }
 

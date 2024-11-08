f6985ba323b4d9a8526f61bf54567a17c70d10a1
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2024-11-08 22:34:28 +0100

    noise sounds

--- src/cross_lib/sound/vbcc/bbc/bbc_sounds.c
+++ src/cross_lib/sound/vbcc/bbc/bbc_sounds.c
@@ -1,7 +1,9 @@
 #include "bbc.h"
 
-bbc_sound ping_snd = {1,-15,200,2};
+bbc_sound ping_snd =      {1,-15,200,2};
 
-bbc_sound tick_snd = {1,-15,100,1};
-bbc_sound tock_snd = {1,-15,50,1};
+bbc_sound tick_snd =      {1,-15,100,1};
+bbc_sound tock_snd =      {1,-15,50,1};
 
+bbc_sound shoot_snd =     {0,-15,80,1};
+bbc_sound explosion_snd = {0,-15,30,2};
--- src/cross_lib/sound/vbcc/bbc/bbc_sounds.h
+++ src/cross_lib/sound/vbcc/bbc/bbc_sounds.h
@@ -12,16 +12,16 @@
 extern bbc_sound ping_snd;
 extern bbc_sound tick_snd;
 extern bbc_sound tock_snd;
-// extern bbc_sound snd;
-// extern bbc_sound snd;
+extern bbc_sound explosion_snd;
+extern bbc_sound shoot_snd;
 
 
 
-#define _XL_EXPLOSION_SOUND() OSSOUND(&tock_snd)
+#define _XL_EXPLOSION_SOUND() OSSOUND(&explosion_snd)
 
 #define _XL_PING_SOUND() OSSOUND(&ping_snd)
 
-#define _XL_SHOOT_SOUND() OSSOUND(&tock_snd)
+#define _XL_SHOOT_SOUND() OSSOUND(&shoot_snd)
 
 #define _XL_TICK_SOUND() OSSOUND(&tick_snd)
 

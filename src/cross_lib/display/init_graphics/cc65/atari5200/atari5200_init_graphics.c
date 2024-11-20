3c2faf3bf8704fd6b97c2191073a9c2a6ffc1a41
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2024-11-20 22:56:27 +0100

    Fix green in atari and atari 5200

--- src/cross_lib/display/init_graphics/cc65/atari5200/atari5200_init_graphics.c
+++ src/cross_lib/display/init_graphics/cc65/atari5200/atari5200_init_graphics.c
@@ -62,27 +62,27 @@ void set_udg(void)
 void _XL_INIT_GRAPHICS(void)
 {
     // NTSC (A5200 is only NTSC)
-    #if !defined(__USE_GREEN)
+    // #if !defined(__USE_GREEN)
         POKE(COLOR0,_gtia_mkcolor(HUE_REDORANGE,2));
         POKE(COLOR1,GTIA_COLOR_WHITE);
         POKE(COLOR2,GTIA_COLOR_CYAN);
-        POKE(COLOR3,_gtia_mkcolor(HUE_GOLDORANGE  ,4));
+        POKE(COLOR3,_gtia_mkcolor(HUE_GREEN  ,4));
         #if defined(__BACKGROUND_COLOR) && __BACKGROUND_COLOR==1
             POKE(COLOR4,GTIA_COLOR_WHITE);
         #else
             POKE(COLOR4,GTIA_COLOR_BLACK);
         #endif
-    #else
-        SETCOLOR_LOW(0,_gtia_mkcolor(HUE_REDORANGE,2));
-        SETCOLOR_LOW(1,GTIA_COLOR_WHITE);
-        SETCOLOR_LOW(2,_gtia_mkcolor(HUE_GREEN,3));
-        SETCOLOR_LOW(3,_gtia_mkcolor(HUE_GOLDORANGE  ,4));
-        #if defined(__BACKGROUND_COLOR) && __BACKGROUND_COLOR==1
-            SETCOLOR_LOW(4,GTIA_COLOR_WHITE);
-        #else
-            SETCOLOR_LOW(4,GTIA_COLOR_BLACK);
-        #endif
-    #endif
+    // #else
+        // SETCOLOR_LOW(0,_gtia_mkcolor(HUE_REDORANGE,2));
+        // SETCOLOR_LOW(1,GTIA_COLOR_WHITE);
+        // SETCOLOR_LOW(2,_gtia_mkcolor(HUE_GREEN,3));
+        // SETCOLOR_LOW(3,_gtia_mkcolor(HUE_GOLDORANGE  ,4));
+        // #if defined(__BACKGROUND_COLOR) && __BACKGROUND_COLOR==1
+            // SETCOLOR_LOW(4,GTIA_COLOR_WHITE);
+        // #else
+            // SETCOLOR_LOW(4,GTIA_COLOR_BLACK);
+        // #endif
+    // #endif
     set_udg();
 
     _setScreenColors();

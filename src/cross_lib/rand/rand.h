0c209af7022549daade500e046d77f6b5c8b6287
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2024-11-28 09:17:47 +0100

    Fix rand for gcc targets

--- src/cross_lib/rand/rand.h
+++ src/cross_lib/rand/rand.h
@@ -2,7 +2,7 @@
 #define _RAND_H
 
 // Normalized 15-bit pseudo-random number generator
-#if defined(__NORMALIZED_RAND) || defined(__NCURSES__)
+#if defined(__NORMALIZED_RAND) || defined(__NCURSES__) || defined(__TERMINAL__)
     #define _XL_RAND() (rand()&0x7FFF)
 #else
     #define _XL_RAND() (rand())

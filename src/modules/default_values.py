4145bcf101287381ed1d9bdcd66147bb97529fe6
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2025-08-07 22:45:57 +0200

    default_values in a separate module

---
+++ src/modules/default_values.py
@@ -0,0 +1,94 @@
+def insert_default_sizes(params):
+
+    if len(params)==4 and params[2].startswith('terminal'):
+        target = params[2]
+        size_target = params[3]
+
+        if size_target == "atari_lynx" or "8x6" in size_target:
+            target = "terminal8x6"
+        elif size_target in ["apple2", "apple2enh"]:
+            target = "terminal7x8"
+        elif size_target in ["oric1", "comx_ntsc"]:
+            target = "terminal6x8"
+        elif size_target in ['comx', 'percom', 'micro']:
+            target = "terminal6x9"
+        else:
+            target = "terminal8x8"
+
+        if size_target == "atari_lynx":
+            xsize = '20'
+            ysize = '17'
+        elif size_target == "pc6001":
+            xsize = '24'
+            ysize = '16'
+        elif size_target in ['ti82', 'ti83', 'ti8x']:
+            xsize = '12'
+            ysize = '8'
+        elif size_target in ['ti85', 'ti86','z88']:
+            xsize = '16'
+            ysize = '8'
+        elif size_target == "oric1":
+            xsize = '38'
+            ysize = '28'
+        elif size_target == "supervision":
+            xsize = '20'
+            ysize = '20'
+        elif size_target == "gamate":
+            xsize = '20'
+            ysize = '19'
+        elif size_target in ["apple2", "apple2enh"]:
+            xsize = '20'
+            ysize = '24'
+        elif size_target in ['comx', 'pecom', 'micro']:
+            xsize = '40'
+            ysize = '24'
+        elif size_target == "comx_ntsc":
+            xsize = '40'
+            ysize = '24'
+        elif size_target in ["atari", "atari5200"]:
+            xsize = '20'
+            ysize = '24'
+        elif size_target in ["c64", "c16", "c128"]:
+            xsize = '40'
+            ysize = '25'
+        elif size_target == "vic20":
+            xsize = '22'
+            ysize = '23'
+        elif size_target in ["mo5","to7"]:
+            xsize = '39'
+            ysize = '25'
+        elif size_target in ["coco","dragon"]:
+            xsize = '16'
+            ysize = '24'
+        elif size_target in ["sms", "spectrum", "creativision", "msx", "mtx", \
+                             "mtx512", "mtx500", "svi", "sg1000", "sc3000", "einstein"]:
+            xsize = '32'
+            ysize = '24'
+        elif size_target == "cpc":
+            xsize = '20'
+            ysize = '25'
+        elif size_target in ["gb", "gg", "gamegear"]:
+            xsize = '20'
+            ysize = '18'
+        elif size_target == "atari7800":
+            xsize = '20'
+            ysize = '28'
+        else:
+            print("Default geometry for this target is not supported. Using default sizes.")
+            xsize = '20'
+            ysize = '17'
+        return [params[0], params[1], target, xsize, ysize]
+
+    if len(params)==3 and params[2].startswith('terminal'):
+        target = params[2]
+        if target in ("terminal", "terminal8x8", "terminal8x6"):
+            xsize = '20'
+            ysize = '17'
+        elif target == "terminal7x8":
+            xsize = '20'
+            ysize = '24'
+        elif target in ("terminal6x9", "terminal6x8"):
+            xsize = '40'
+            ysize = '24'
+        return [params[0], params[1], target, xsize, ysize]
+    return params

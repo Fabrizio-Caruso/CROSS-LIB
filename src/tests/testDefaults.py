39e6d07bebfd2209d12540ec067bd36ad47b099d
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2025-08-08 09:19:46 +0200

    More unit-tests for defaults

--- src/tests/testDefaults.py
+++ src/tests/testDefaults.py
@@ -42,6 +42,67 @@ class testDefaults(unittest.TestCase):
         
         self.assertEqual(result,['build', 'helloworld', 'terminal8x8', '20', '24'])
 
+    def test_terminal_4(self):
+        params = ("build", "helloworld","terminal6x8")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal6x8', '40', '24'])
+
+    def test_terminal_5(self):
+        params = ("build", "helloworld","terminal8x6")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal8x6', '20', '17'])
+
+    def test_terminal_6(self):
+        params = ("build", "helloworld","terminal7x8")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal7x8', '20', '24'])
+
+    def test_terminal_7(self):
+        params = ("build", "helloworld","terminal6x9")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal6x9', '40', '24'])
+
+    def test_terminal_8(self):
+        params = ("build", "helloworld","terminal8x8")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal8x8', '20', '17'])
+
+    def test_terminal_9(self):
+        params = ("build", "helloworld","terminal8x6", "atari_lynx")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal8x6', '20', '17'])
+
+    def test_terminal_10(self):
+        params = ("build", "helloworld", "terminal", "c64")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal8x8', '40', '25'])
+
+    def test_terminal_11(self):
+        params = ("build", "helloworld","terminal", "msx")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal8x8', '32', '24'])
+
+    def test_terminal_12(self):
+        params = ("build", "helloworld","terminal", "cpc")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal8x8', '20', '25'])
+
+    def test_terminal_13(self):
+        params = ("build", "helloworld","terminal", "spectrum")
+        result = insert_default_sizes(params)
+        
+        self.assertEqual(result,['build', 'helloworld', 'terminal8x8', '32', '24'])
+
+
 if __name__ == '__main__':
     # global_vars.verbose = 0
     # global_vars.test = 1

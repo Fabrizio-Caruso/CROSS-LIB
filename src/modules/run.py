4a1d49463d46ce001a3b794617c3e86c41a5babf
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2025-08-20 00:36:41 +0200

    Tune terminal verbosity within and outside of test

--- src/modules/run.py
+++ src/modules/run.py
@@ -6,13 +6,13 @@ logger = LoggerSingleton.initLogger('xl', '../logs')
 
 def run_command(option_config, command_string):
     logger.info("Running command: %s", command_string)
-    test = option_config.terminal_config.test
+    test = option_config.terminal_config.test           # run instde test_self makes test      = False
     verbose = option_config.terminal_config.verbose
-    show_fail = option_config.terminal_config.show_fail
+    show_fail = option_config.terminal_config.show_fail # run inside test_self makes show_fail = False
     
     if verbose:
         printc(option_config, bcolors.BOLD, command_string+"\n")
-    elif test:
+    if show_fail and (test or not verbose):
         command_string+= " > /dev/null 2>&1"
 
     res = os.system(command_string)

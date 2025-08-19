daceaf900ec8855f6b9d450bf60541ccbb2a8e57
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2025-08-19 09:33:37 +0200

    Show fail return codes

--- src/modules/run.py
+++ src/modules/run.py
@@ -7,10 +7,14 @@ logger = LoggerSingleton.initLogger('xl', '../logs')
 def run_command(option_config, command_string):
     logger.info("Running command: %s", command_string)
     test = option_config.terminal_config.test
+    show_fail = option_config.terminal_config.show_fail
     if not test:
         print("Going to run: ")
         printc(option_config, bcolors.BOLD, command_string+"\n")
     else:
         command_string+= " > /dev/null 2>&1"
 
-    os.system(command_string)
+    res = os.system(command_string)
+
+    if not test and show_fail and res:
+        printc(option_config, bcolors.FAIL, "return code: " + str(res)+"\n")

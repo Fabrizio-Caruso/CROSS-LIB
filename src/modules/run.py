bb725138ecd1fc43d05dcfd0057e4c918ec13d9d
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2025-08-20 09:14:20 +0200

    Rename show_flag to native_console

--- src/modules/run.py
+++ src/modules/run.py
@@ -8,14 +8,14 @@ def run_command(option_config, command_string):
     logger.info("Running command: %s", command_string)
     test = option_config.terminal_config.test           # run instde test_self makes test      = False
     verbose = option_config.terminal_config.verbose
-    show_fail = option_config.terminal_config.show_fail # run inside test_self makes show_fail = False
+    native_console = option_config.terminal_config.native_console # run inside test_self makes native_console = False
     
     if verbose:
         printc(option_config, bcolors.BOLD, command_string+"\n")
-    if show_fail and (test or not verbose):
+    if native_console and (test or not verbose):
         command_string+= " > /dev/null 2>&1"
 
     res = os.system(command_string)
 
-    if show_fail and res:
+    if native_console and res:
         printc(option_config, bcolors.FAIL, "return code: " + str(res)+"\n")

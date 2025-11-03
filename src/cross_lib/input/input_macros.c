c771a3e36eb68b04a2a69740d4d71826651e7e42
U-DESKTOP-IP83304\User <Fabrizio_Caruso@hotmail.com>
2025-11-04 00:40:55 +0100

    Agat works

--- src/cross_lib/input/input_macros.c
+++ src/cross_lib/input/input_macros.c
@@ -140,7 +140,7 @@
         return 0;
 
     #elif defined(__VIC20__) || defined(__SUPERVISION__) || defined(__CREATIVISION__) || defined(__OSIC1P__) \
-    || defined(__APPLE2__) || defined(__APPLE2ENH__) || defined(__CBM610__) || defined(__C16__) || defined(__CX16__)
+    || defined(__APPLE2__) || defined(__APPLE2ENH__) || defined(__CBM610__) || defined(__C16__) || defined(__CX16__) || defined(__AGAT__)
         if(kbhit())
             return cgetc();
         else
